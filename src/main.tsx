1e0d9c07d7d11a912dddb2ca242aaf27e50cacb7
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-10-18 12:41:16 +0200

    react router v1

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
