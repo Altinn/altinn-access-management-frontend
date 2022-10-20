a8f16d089054ea7667710927c97f7badbd0c49db
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-10-20 16:46:24 +0200

    fix import errors

--- src/components/ExamplePage/ExamplePage.tsx
+++ src/components/ExamplePage/ExamplePage.tsx
@@ -1,5 +1,5 @@
-import { Page, PageContent, PageHeader } from '@altinn/altinn-design-system';
 import { useTranslation } from 'react-i18next';
+import { Page, PageContent, PageHeader } from '@altinn/altinn-design-system';
 
 export const ExamplePage = () => {
   const { t } = useTranslation('common');
