b3c901a80d0e1e328606ba70ef017d55e91c9180
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-10-24 14:22:56 +0200

    fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { React } from 'react';
+import { ReactDOM } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import { initReactI18next } from 'react-i18next';
+import i18next from 'i18next';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { ErrorPage } from './resources/ErrorPage/ErrorPage';
 import { ApiDelegation } from './routes/ApiDelegation';
-import { initReactI18next } from 'react-i18next';
 import LoadLocalizations from './resources/LoadLocalizations';
-import i18next from 'i18next';
 import { getConfig } from './config/config';
 import BaseLocalizations from './resources/BaseLocalizations/BaseLocalizations.json';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 /**
  * Special behaviour for react-query in dev environment
  */
 const queryClientDevDefaults = {
   queries: {
-    retry: false // Don't retry failed requests
-  }
+    retry: false, // Don't retry failed requests
+  },
 };
 
 const router = createBrowserRouter([
   {
     path: '/',
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
     path: 'api-delegation/1',
-    element: <ApiDelegation />
-  }
+    element: <ApiDelegation />,
+  },
 ]);
 
 // Initialise i18next; start application when ready
@@ -37,13 +37,13 @@ i18next.use(initReactI18next).init(
     lng: getConfig('defaultLocale'),
     ns: ['common', 'basic'],
     defaultNS: 'common',
-    resources: BaseLocalizations
+    resources: BaseLocalizations,
   },
 
   () => {
     // Configure react-query
     const queryClient = new QueryClient({
-      defaultOptions: import.meta.env.DEV ? queryClientDevDefaults : undefined
+      defaultOptions: import.meta.env.DEV ? queryClientDevDefaults : undefined,
     });
 
     ReactDOM.createRoot(document.getElementById('root')).render(
@@ -53,7 +53,7 @@ i18next.use(initReactI18next).init(
             <RouterProvider router={router} />
           </LoadLocalizations>
         </QueryClientProvider>
-      </React.StrictMode>
+      </React.StrictMode>,
     );
-  }
+  },
 );
