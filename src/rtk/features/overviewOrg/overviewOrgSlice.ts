43887119c88bacb2a9348342fc90f419766afb0e
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2022-11-10 13:03:07 +0100

    rename slice

--- src/rtk/features/overviewOrg/overviewOrgSlice.ts
+++ src/rtk/features/overviewOrg/overviewOrgSlice.ts
@@ -59,15 +59,11 @@ const overviewOrgSlice = createSlice({
       state.overviewOrgs.push(action.payload);
 
       const { softDeletedItems: modifiedOverviewOrgs } = state;
-      state.softDeletedItems = modifiedOverviewOrgs.filter(
-        (delegableApi) => delegableApi.id !== action.payload.id,
-      );
+      state.softDeletedItems = modifiedOverviewOrgs.filter((org) => org.id !== action.payload.id);
     },
     softDelete: (state, action) => {
       const { overviewOrgs } = state;
-      state.overviewOrgs = overviewOrgs.filter(
-        (delegableApi) => delegableApi.id !== action.payload.id,
-      );
+      state.overviewOrgs = overviewOrgs.filter((org) => org.id !== action.payload.id);
 
       state.softDeletedItems.push(action.payload);
     },
