a1aac2b529c6260fcf97398991af13f50cd683b4
Torgeir Holen <torgeir333@gmail.com>
2023-01-04 11:35:03 +0100

    ErrorPage get Page format and error.status, error.errorText, error.data

--- src/resources/ErrorPage/ErrorPage.tsx
+++ src/resources/ErrorPage/ErrorPage.tsx
@@ -1,15 +1,37 @@
 import { useRouteError } from 'react-router-dom';
+import {
+  Page,
+  PageContent,
+  PageHeader,
+  SearchField,
+  Select,
+  Button,
+  ButtonVariant,
+  ButtonColor,
+  ButtonSize,
+} from '@altinn/altinn-design-system';
 
 export const ErrorPage = () => {
   // use when completing this page
-  const error = useRouteError();
+  const error = useRouteError(); // NB error har ikke Type ennå
 
   return (
     <div>
-      <h1>Det skjedde en feil</h1>
-      <p>
-        <i>Vennligst gå tilbake i nettleseren eller refresh siden</i>
-      </p>
+      <Page>
+        <PageHeader>Det skjedde en feil</PageHeader>
+        <PageContent>
+          <p>
+            <i>Vennligst gå tilbake i nettleseren eller refresh siden</i>
+            <br />
+            <br />
+            Error status : {error.status}
+            <br />
+            Error statusText : {error.statusText}
+            <br />
+            Error data : {error.data}
+          </p>
+        </PageContent>
+      </Page>
     </div>
   );
 };
