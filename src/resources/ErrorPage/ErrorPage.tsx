60613fec91ac34c7f7d3e300da64b7033c72ee23
Torgeir Holen <torgeir333@gmail.com>
2023-01-05 14:22:20 +0100

    add css to ErrorPage

--- src/resources/ErrorPage/ErrorPage.tsx
+++ src/resources/ErrorPage/ErrorPage.tsx
@@ -14,30 +14,34 @@ import { useTranslation } from 'react-i18next';
 
 import { ReactComponent as ApiIcon } from '../../assets/Cancel.svg';
 
+import classes from './ErrorPage.module.css'; // fixme: trim superflous css in file
+
 export const ErrorPage = () => {
   // use when completing this page
   const error = useRouteError(); // NB error har ikke Type ennå
-
-  // import api_delegation.error_page_header from no_nb.json
   const { t } = useTranslation('common');
 
   return (
     <div>
-      <Page>
-        <PageHeader icon={<ApiIcon />}> {t('api_delegation.error_page_header')} </PageHeader>
-        <PageContent>
-          <p>
-            <i>Vennligst gå tilbake i nettleseren eller refresh siden</i>
-            <br />
-            <br />
-            Error status : {error.status}
-            <br />
-            Error statusText : {error.statusText}
-            <br />
-            Error data : {error.data}
-          </p>
-        </PageContent>
-      </Page>
+      <div className={classes.page}>
+        <Page>
+          <PageHeader icon={<ApiIcon />}> {t('api_delegation.error_page_header')} </PageHeader>
+          <PageContent>
+            <div className={classes.pageContent}>
+              <p>
+                <i>Vennligst gå tilbake i nettleseren eller refresh siden</i>
+                <br />
+                <br />
+                Error status : {error.status}
+                <br />
+                Error statusText : {error.statusText}
+                <br />
+                Error data : {error.data}
+              </p>
+            </div>
+          </PageContent>
+        </Page>
+      </div>
     </div>
   );
 };
