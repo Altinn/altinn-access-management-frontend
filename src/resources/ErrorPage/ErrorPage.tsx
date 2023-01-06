c6938f37fa063ce0bf4a5354a7374694ce5d904b
Torgeir Holen <torgeir333@gmail.com>
2023-01-06 10:20:04 +0100

    add HOME function to upper left icon

--- src/resources/ErrorPage/ErrorPage.tsx
+++ src/resources/ErrorPage/ErrorPage.tsx
@@ -1,15 +1,5 @@
-import { useRouteError } from 'react-router-dom';
-import {
-  Page,
-  PageContent,
-  PageHeader,
-  SearchField,
-  Select,
-  Button,
-  ButtonVariant,
-  ButtonColor,
-  ButtonSize,
-} from '@altinn/altinn-design-system';
+import { useRouteError, useNavigate } from 'react-router-dom';
+import { Page, PageContent, PageHeader } from '@altinn/altinn-design-system';
 import { useTranslation } from 'react-i18next';
 
 import { ReactComponent as ApiIcon } from '../../assets/Cancel.svg';
@@ -17,15 +7,18 @@ import { ReactComponent as ApiIcon } from '../../assets/Cancel.svg';
 import classes from './ErrorPage.module.css'; // fixme: trim superflous css in file
 
 export const ErrorPage = () => {
-  // use when completing this page
-  const error = useRouteError(); // NB error har ikke Type ennå
-  const { t } = useTranslation('common');
+  // error Docs: https://reactrouter.com/en/main/hooks/use-route-error
+  const error = useRouteError(); // fixme: error har ikke Type ennå
+  const { t } = useTranslation('common'); // "t" is the (ugly) convention of "translate"
+  const navigate = useNavigate(); // use upper left icon as HOME button
 
   return (
     <div>
       <div className={classes.page}>
         <Page>
-          <PageHeader icon={<ApiIcon />}> {t('api_delegation.error_page_header')} </PageHeader>
+          <PageHeader icon={<ApiIcon onClick={() => navigate('/api-delegations')} />}>
+            {t('api_delegation.error_page_header')}
+          </PageHeader>
           <PageContent>
             <div className={classes.pageContent}>
               <p>
