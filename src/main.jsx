abc6efd7d94c4907adbebf3d9f8563ffb8daccf2
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-01-24 12:33:10 +0100

    Implement refresh token

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { use } from 'i18next';
@@ -12,7 +12,6 @@ import { getConfig } from '../config';
 
 import LoadLocalizations from './resources/LoadLocalizations';
 import store from './rtk/app/store';
-import { App } from './App';
 
 /**
  * Special behaviour for react-query in dev environment
@@ -56,7 +55,9 @@ use(initReactI18next).init(
         <Provider store={store}>
           <QueryClientProvider client={queryClient}>
             <LoadLocalizations>
-              <BrowserRouter basename='/accessmanagement/ui'></BrowserRouter>
+              <BrowserRouter basename='accessmanagement/ui'>
+                <Router></Router>
+              </BrowserRouter>
             </LoadLocalizations>
           </QueryClientProvider>
         </Provider>
--- src/routes/Router/Router.tsx
+++ src/routes/Router/Router.tsx
@@ -1,5 +1,8 @@
-import { createBrowserRouter, createRoutesFromElements, Route, Routes } from 'react-router-dom';
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Route, Routes } from 'react-router-dom';
 import * as React from 'react';
+import axios from 'axios';
 
 import { ErrorPage } from '@/resources/ErrorPage/ErrorPage';
 import { NewApiDelegationPage } from '@/components/NewApiDelegationPage';
@@ -7,7 +10,6 @@ import { OverviewPage as GivenOverviewPage } from '@/components/apiDelegation/gi
 import { OverviewPage as ReceivedOverviewPage } from '@/components/apiDelegation/received/OverviewPage';
 import { NewOrgDelegationPage } from '@/components/NewOrgDelegationPage';
 import { ApiDelegationConfirmationPage } from '@/components/ApiDelegationConfirmationPage';
-import { App } from '@/App';
 
 export enum RouterPath {
   GivenApiDelegations = 'given-api-delegations',
@@ -22,38 +24,68 @@ export enum RouterPath {
   StartPage = 'www.altinn.no',
 }
 
-export const Router = (
-  <Routes>
-    createRoutesFromElements(
-    <Route
-      path='/'
-      errorElement={<ErrorPage />}
-    >
+export const Router = () => {
+  const lastRefreshTokenTimestamp = React.useRef(0);
+  const TEN_MINUTES_IN_MILLISECONDS = 600000;
+
+  async function refreshJwtToken() {
+    const timeNow = Date.now();
+    if (timeNow - lastRefreshTokenTimestamp.current > TEN_MINUTES_IN_MILLISECONDS) {
+      lastRefreshTokenTimestamp.current = timeNow;
+      return await axios
+        // TODO: This may fail in AT if axios doesn't automatically change the base url
+        .get('accessmanagement/api/v1/authentication/refresh')
+        .then((response) => response.data)
+        .catch((error) => {
+          window.location.pathname = '/';
+          console.error(error);
+        });
+    }
+  }
+
+  React.useEffect(() => {
+    const setUpEventListeners = () => {
+      window.addEventListener('mousemove', refreshJwtToken);
+      window.addEventListener('scroll', refreshJwtToken);
+      window.addEventListener('onfocus', refreshJwtToken);
+      window.addEventListener('keydown', refreshJwtToken);
+    };
+    refreshJwtToken();
+    setUpEventListeners();
+  });
+  return (
+    <Routes>
+      createRoutesFromElements(
       <Route
-        path={RouterPath.GivenApiDelegations + '/' + RouterPath.GivenApiDelegationOverview}
-        element={<GivenOverviewPage />}
-      />
-      <Route
-        path={RouterPath.GivenApiDelegations + '/' + RouterPath.NewGivenOrgDelegation}
-        element={<NewOrgDelegationPage />}
+        path='/'
         errorElement={<ErrorPage />}
-      />
-      <Route
-        path={RouterPath.GivenApiDelegations + '/' + RouterPath.NewGivenApiDelegation}
-        element={<NewApiDelegationPage />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path={RouterPath.GivenApiDelegations + '/' + RouterPath.GivenApiDelegationConfirmation}
-        element={<ApiDelegationConfirmationPage />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path={RouterPath.ReceivedApiDelegations + '/' + RouterPath.GivenApiDelegationOverview}
-        element={<ReceivedOverviewPage />}
-        errorElement={<ErrorPage />}
-      />
-    </Route>
-    , ),
-  </Routes>
-);
+      >
+        <Route
+          path={RouterPath.GivenApiDelegations + '/' + RouterPath.GivenApiDelegationOverview}
+          element={<GivenOverviewPage />}
+        />
+        <Route
+          path={RouterPath.GivenApiDelegations + '/' + RouterPath.NewGivenOrgDelegation}
+          element={<NewOrgDelegationPage />}
+          errorElement={<ErrorPage />}
+        />
+        <Route
+          path={RouterPath.GivenApiDelegations + '/' + RouterPath.NewGivenApiDelegation}
+          element={<NewApiDelegationPage />}
+          errorElement={<ErrorPage />}
+        />
+        <Route
+          path={RouterPath.GivenApiDelegations + '/' + RouterPath.GivenApiDelegationConfirmation}
+          element={<ApiDelegationConfirmationPage />}
+          errorElement={<ErrorPage />}
+        />
+        <Route
+          path={RouterPath.ReceivedApiDelegations + '/' + RouterPath.GivenApiDelegationOverview}
+          element={<ReceivedOverviewPage />}
+          errorElement={<ErrorPage />}
+        />
+      </Route>
+      , ),
+    </Routes>
+  );
+};
