0b34211b47477d055438c058c8cdca210bae02ad
Torgeir Holen <torgeir333@gmail.com>
2023-02-02 13:18:26 +0100

    wrap PageHeader for css

--- src/components/apiDelegation/given/OverviewPage/OverviewPage.tsx
+++ src/components/apiDelegation/given/OverviewPage/OverviewPage.tsx
@@ -8,13 +8,17 @@ import { OverviewPageContent } from '../../reusables/OverviewPageContent';
 import { LayoutState } from '../../reusables/LayoutState';
 import { PageContainer } from '../../../reusables/PageContainer';
 
+import classes from './OverviewPage.module.css';
+
 export const OverviewPage = () => {
   const { t } = useTranslation('common');
 
   return (
     <PageContainer>
       <Page>
-        <PageHeader icon={<ApiIcon />}>{t('api_delegation.api_delegations')}</PageHeader>
+        <div className={classes.pageHeaderWrapper}>
+          <PageHeader icon={<ApiIcon />}>{t('api_delegation.api_delegations')}</PageHeader>
+        </div>
         <PageContent>
           <OverviewPageContent layout={LayoutState.Given} />
         </PageContent>
