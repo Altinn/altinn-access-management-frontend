4e44e6257ca20ec65ed9e29e0db99ceb9158a124
Torgeir Holen <torgeir333@gmail.com>
2023-02-13 13:30:36 +0100

    ErrorPage issue25 third iteration per 130223

--- src/resources/ErrorPage/ErrorPage.tsx
+++ src/resources/ErrorPage/ErrorPage.tsx
@@ -1,15 +1,69 @@
-// import { useRouteError } from 'react-router-dom';
+import { useRouteError, useNavigate } from 'react-router-dom';
+import { Page, PageContent, PageHeader } from '@altinn/altinn-design-system';
+import { useTranslation } from 'react-i18next';
+
+import { PageContainer } from '@/components/reusables/PageContainer';
+
+import { ReactComponent as MaakeIcon } from '../../assets/Maake.svg';
+
+import classes from './ErrorPage.module.css';
 
 export const ErrorPage = () => {
-  // use line below when completing this page
-  /* const error = useRouteError(); */
+  // error Docs: https://reactrouter.com/en/main/hooks/use-route-error
+  // const error = useRouteError(); // fixme: error har ikke Type ennå
+  // FixMe: Hook broken: "history.ts:480 Uncaught Error:
+  // useRouteError must be used within a data router.
+  // See https://reactrouter.com/routers/picking-a-router."
+
+  const { t } = useTranslation('common'); // "t" is the (ugly) convention of "translate"
 
   return (
-    <div>
-      <h1>Det skjedde en feil</h1>
-      <p>
-        <i>Vennligst gå tilbake i nettleseren eller refresh siden</i>
-      </p>
-    </div>
+    <PageContainer>
+      <Page>
+        <PageContent>
+          <div className={classes.errorPageWrapper}>
+            <table>
+              <tbody>
+                <tr>
+                  <td className={classes.tabellKolonne1}>
+                    <p className={classes.errorParagraph}>
+                      {t('api_delegation.error_page404_type')}
+                    </p>
+
+                    <h1>{t('api_delegation.error_page404_header')}</h1>
+
+                    <p>{t('api_delegation.error_page404_upper_text')}</p>
+
+                    <p>
+                      <a href='https://www.altinn.no/ui/MessageBox'>
+                        {t('api_delegation.error_page404_link1_messages')}
+                      </a>
+                    </p>
+
+                    <p>
+                      <a href='https://www.altinn.no/ui/Profile'>
+                        {t('api_delegation.error_page404_link2_profile')}
+                      </a>
+                    </p>
+
+                    <p>
+                      <a href='https://www.altinn.no/skjemaoversikt/'>
+                        {t('api_delegation.error_page404_link3_forms')}
+                      </a>
+                    </p>
+
+                    <p>{t('api_delegation.error_page404_lower_text')}</p>
+                  </td>
+
+                  <td className={classes.tabellKolonne2}>
+                    <MaakeIcon />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </PageContent>
+      </Page>
+    </PageContainer>
   );
 };
