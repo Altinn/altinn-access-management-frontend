e6896796bb0d186d3a4bcec0ed713e9fdb4fde57
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-02-15 16:08:59 +0100

    Bug/fix error element handling (#145)
    * redirect to ui/Profile
    
    * Fix errorelement not working
    
    * code cleanup
    
    * merge
    
    * code cleanup
    
    * code cleanup

---
+++ src/resources/Token/RefreshToken.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import axios from 'axios';
+import * as React from 'react';
+
+export const RefreshToken = () => {
+  const lastRefreshTokenTimestamp = React.useRef(0);
+  const TEN_MINUTES_IN_MILLISECONDS = 600000;
+
+  async function refreshJwtToken() {
+    const timeNow = Date.now();
+
+    if (timeNow - lastRefreshTokenTimestamp.current > TEN_MINUTES_IN_MILLISECONDS) {
+      lastRefreshTokenTimestamp.current = timeNow;
+      return await axios
+        // TODO: This may fail in AT if axios doesn't automatically change the base url
+        .get('accessmanagement/api/v1/authentication/refresh')
+        .then((response) => response.data)
+        .catch((error) => {
+          !import.meta.env.DEV && (window.location.pathname = '/');
+          console.error(error);
+        });
+    }
+  }
+
+  React.useEffect(() => {
+    const setUpEventListeners = () => {
+      window.addEventListener('mousemove', refreshJwtToken);
+      window.addEventListener('scroll', refreshJwtToken);
+      window.addEventListener('onfocus', refreshJwtToken);
+      window.addEventListener('keydown', refreshJwtToken);
+    };
+    refreshJwtToken();
+    setUpEventListeners();
+  });
+};
