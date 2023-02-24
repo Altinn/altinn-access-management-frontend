5a0110b1f786387cf676efe1664b658692a497e1
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-02-24 10:06:21 +0100

    remove mocked orgs

--- src/rtk/features/apiDelegation/overviewOrg/overviewOrgSlice.ts
+++ src/rtk/features/apiDelegation/overviewOrg/overviewOrgSlice.ts
@@ -58,56 +58,7 @@ export interface DeletionRequest {
 
 const initialState: SliceState = {
   loading: true,
-  overviewOrgs: [
-    {
-      id: '1',
-      orgName: 'Skatteetaten',
-      orgNr: '123456789',
-      isAllSoftDeleted: false,
-      apiList: [
-        {
-          id: '1',
-          apiName: 'Delegert API A',
-          isSoftDelete: false,
-          owner: 'Brønnøysundregisterene',
-          description:
-            'kan du registrere og endre opplysninger på bedrift, finne bedriftsinformasjon og kunngjøringer, sjekke heftelser i bil og stoppe telefonsalg.',
-        },
-        {
-          id: '2',
-          apiName: 'Delegert API B',
-          isSoftDelete: false,
-          owner: 'Accenture',
-          description:
-            'API for forvaltningsorgan og kompetansesenter som skal styrke kommunenes, sektormyndighetenes og andre samarbeidspartneres kompetanse på integrering og',
-        },
-      ],
-    },
-    {
-      id: '2',
-      orgName: 'Brønnøysundregistrene',
-      orgNr: '950124321',
-      isAllSoftDeleted: false,
-      apiList: [
-        {
-          id: '1',
-          apiName: 'Delegert API A',
-          isSoftDelete: false,
-          owner: 'Avanade',
-          description:
-            'kan du registrere og endre opplysninger på bedrift, finne bedriftsinformasjon og kunngjøringer, sjekke heftelser i bil og stoppe telefonsalg.',
-        },
-        {
-          id: '2',
-          apiName: 'Delegert API B',
-          isSoftDelete: false,
-          owner: 'Accenture',
-          description:
-            'Accenture er et forvaltningsorgan og kompetansesenter som skal styrke kommunenes, sektormyndighetenes og andre samarbeidspartneres kompetanse på integrering og',
-        },
-      ],
-    },
-  ],
+  overviewOrgs: [],
   error: '',
 };
 
