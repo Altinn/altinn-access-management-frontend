3ed7ae31dec79e056cc39eb2c42a45542b82b145
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-03-21 16:44:20 +0100

    Feat/223 new blue screen bug fix (#236)
    * Revert changes
    
    * Try new blue screen solution
    
    * Try new fix

--- src/resources/LoadLocalizations/LoadLocalizations.tsx
+++ src/resources/LoadLocalizations/LoadLocalizations.tsx
@@ -4,24 +4,20 @@ import i18next from 'i18next';
 import axios from 'axios';
 import * as React from 'react';
 
+import { getCookie } from '../Cookie/CookieMethods';
+
 interface Props {
   children: React.ReactNode;
 }
 
 const LoadLocalizations = ({ children }: Props) => {
-  const { i18n } = useTranslation('common');
-  const envUrl = import.meta.env.DEV ? 'src/' : 'accessmanagement/';
-  const baseUrl = import.meta.env.BASE_URL + envUrl;
-  const localizationsFilePath = `${baseUrl}localizations/${i18n.language}.json`;
-  const localizationsFileUrl = new URL(localizationsFilePath, import.meta.url).href;
-
   const initLanguage = (lang: string) => {
     if (lang === 'en') {
       return 'en';
     } else if (lang === 'nn') {
       return 'no_nn';
     } else {
-      return 'no_nn';
+      return 'no_nb';
     }
   };
 
@@ -40,12 +36,18 @@ const LoadLocalizations = ({ children }: Props) => {
 
   void setLanguage();
 
+  const backupLang = getCookie('i18next') ? getCookie('i18next') : 'no_nb';
+  const envUrl = import.meta.env.DEV ? 'src/' : 'accessmanagement/';
+  const baseUrl = import.meta.env.BASE_URL + envUrl;
+  const localizationsFilePath = `${baseUrl}localizations/${backupLang}.json`;
+  const localizationsFileUrl = new URL(localizationsFilePath, import.meta.url).href;
+
   useQuery(
     'Localizations',
     async () => {
       const data = await fetch(localizationsFileUrl);
       const resource = await data.json();
-      i18next.addResourceBundle(i18n.language, 'common', resource);
+      i18next.addResourceBundle(backupLang, 'common', resource);
     },
     {
       staleTime: Infinity,
