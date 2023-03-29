5e05e6ae8bf1b8fe75d4cee9e9c54cfd4aebab5d
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-03-29 09:45:38 +0200

    Modularize delegation requests

---
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Models/Delegation/DelegationRequest.cs
@@ -0,0 +1,24 @@
+/*using System.ComponentModel.DataAnnotations;
+
+namespace Altinn.AccessManagement.UI.Core.Models.Delegation
+
+{
+    /// <summary>
+    /// Model for performing an API-delegation or deleting an API-delegation
+    /// </summary>
+    public record DelegationRequest
+    {
+        /// <summary>
+        /// Organization number of the organization getting the api access delegated or deleted
+        /// </summary>
+        [Required]
+        public string OrganizationNumber { get; init; }
+
+        /// <summary>
+        /// The id to the API that's being delegated or deleted
+        /// </summary>
+        [Required]
+        public string ApiIdentifier { get; init; }
+    }
+}
+*/
