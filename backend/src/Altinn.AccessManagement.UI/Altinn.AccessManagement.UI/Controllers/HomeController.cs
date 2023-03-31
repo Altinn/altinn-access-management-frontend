e79f691b6e56e2359e5c4542fb948a96e6242394
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-03-31 09:28:43 +0200

    fixed build error

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/HomeController.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/HomeController.cs
@@ -104,7 +104,7 @@ namespace Altinn.AccessManagement
         {
             if (User.Identity.IsAuthenticated)
             {
-                await SetLanguageCookie();
+                SetLanguageCookie();
                 return true;
             }
 
