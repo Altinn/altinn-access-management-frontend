97848743e487602c54cfc2474be55d3f799f1d87
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-04-19 15:58:36 +0200

    Feat/fix refresh token url (#289)
    * Fix i18next cookie
    
    * Open url in new tab
    
    * Add fix for refresh token call
    
    * add LanguageDetector

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { use } from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import { RefreshToken } from '@/resources/Token/RefreshToken';
 import { Router } from '@/routes/Router';
@@ -25,31 +26,33 @@ const queryClientDevDefaults = {
 };
 
 // Initialise i18next; start application when ready
-use(initReactI18next).init(
-  {
-    fallbackLng: getConfig('defaultLocale'),
-    ns: ['common'],
-    defaultNS: 'common',
-    returnNull: false,
-  },
+use(LanguageDetector)
+  .use(initReactI18next)
+  .init(
+    {
+      fallbackLng: getConfig('defaultLocale'),
+      ns: ['common'],
+      defaultNS: 'common',
+      returnNull: false,
+    },
 
-  () => {
-    // Configure react-query
-    const queryClient = new QueryClient({
-      defaultOptions: import.meta.env.DEV ? queryClientDevDefaults : undefined,
-    });
+    () => {
+      // Configure react-query
+      const queryClient = new QueryClient({
+        defaultOptions: import.meta.env.DEV ? queryClientDevDefaults : undefined,
+      });
 
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <QueryClientProvider client={queryClient}>
-            <LoadLocalizations>
-              <RefreshToken />
-              <RouterProvider router={Router}></RouterProvider>
-            </LoadLocalizations>
-          </QueryClientProvider>
-        </Provider>
-      </React.StrictMode>,
-    );
-  },
-);
+      ReactDOM.createRoot(document.getElementById('root')).render(
+        <React.StrictMode>
+          <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+              <LoadLocalizations>
+                <RefreshToken />
+                <RouterProvider router={Router}></RouterProvider>
+              </LoadLocalizations>
+            </QueryClientProvider>
+          </Provider>
+        </React.StrictMode>,
+      );
+    },
+  );
