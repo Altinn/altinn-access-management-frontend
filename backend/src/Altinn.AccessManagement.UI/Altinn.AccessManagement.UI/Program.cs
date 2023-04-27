fe6dfc45c1046ea5c81cf6d4824bb93d4fac1739
jonkjetiloye <40996230+jonkjetiloye@users.noreply.github.com>
2023-04-27 07:46:29 +0200

    Remove extra information level logging for debugging (#323)
    Co-authored-by: Jon Kjetil Øye <acn-joye@ai-dev.no>

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Program.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Program.cs
@@ -114,7 +114,7 @@ void ConfigureLogging(ILoggingBuilder logging)
         // Optional: Apply filters to control what logs are sent to Application Insights.
         // The following configures LogLevel Information or above to be sent to
         // Application Insights for all categories.
-        logging.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+        logging.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Warning);
 
         // Adding the filter below to ensure logs of all severity from Program.cs
         // is sent to ApplicationInsights.
