b423d158f48a9b03e8a09a3253d1faabc32ac4b4
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-05-11 13:26:29 +0200

    revert code

--- src/rtk/features/apiDelegation/overviewOrg/overviewOrgSlice.ts
+++ src/rtk/features/apiDelegation/overviewOrg/overviewOrgSlice.ts
@@ -45,56 +45,7 @@ export interface DeletionRequest {
 
 const initialState: SliceState = {
   loading: true,
-  overviewOrgs: [
-    {
-      id: '1',
-      orgName: 'Skatteetaten',
-      orgNr: '123456789',
-      isAllSoftDeleted: false,
-      apiList: [
-        {
-          id: '1',
-          apiName: 'Delegert API A',
-          isSoftDelete: false,
-          owner: 'Brønnøysundregisterene',
-          description:
-            'kan du registrere og endre opplysninger på bedrift, finne bedriftsinformasjon og kunngjøringer, sjekke heftelser i bil og stoppe telefonsalg.',
-        },
-        {
-          id: '2',
-          apiName: 'Delegert API B',
-          isSoftDelete: false,
-          owner: 'Accenture',
-          description:
-            'API for forvaltningsorgan og kompetansesenter som skal styrke kommunenes, sektormyndighetenes og andre samarbeidspartneres kompetanse på integrering og',
-        },
-      ],
-    },
-    {
-      id: '2',
-      orgName: 'Brønnøysundregistrene',
-      orgNr: '950124321',
-      isAllSoftDeleted: false,
-      apiList: [
-        {
-          id: '1',
-          apiName: 'Delegert API A',
-          isSoftDelete: false,
-          owner: 'Avanade',
-          description:
-            'kan du registrere og endre opplysninger på bedrift, finne bedriftsinformasjon og kunngjøringer, sjekke heftelser i bil og stoppe telefonsalg.',
-        },
-        {
-          id: '2',
-          apiName: 'Delegert API B',
-          isSoftDelete: false,
-          owner: 'Accenture',
-          description:
-            'Accenture er et forvaltningsorgan og kompetansesenter som skal styrke kommunenes, sektormyndighetenes og andre samarbeidspartneres kompetanse på integrering og',
-        },
-      ],
-    },
-  ],
+  overviewOrgs: [],
   error: '',
 };
 
@@ -177,11 +128,11 @@ export const fetchOverviewOrgsOffered = createAsyncThunk(
       throw new Error(String('Could not get AltinnPartyId cookie value'));
     }
     return await axios
-      .get(`/accessmanagement/api/v1/${altinnPartyId}/delegations/maskinportenschema/offered`)
+      .get(`/accessmanagement/api/v1/${altinnPartyId}/maskinportenschema/offered`)
       .then((response) => response.data)
       .catch((error) => {
         console.error(error);
-        // throw new Error(String(error.response.status));
+        throw new Error(String(error.response.status));
       });
   },
 );
@@ -364,8 +315,8 @@ const overviewOrgSlice = createSlice({
       .addCase(fetchOverviewOrgsOffered.fulfilled, (state, action) => {
         state.loading = true;
         const dataArray = action.payload;
-        // const responseList: OverviewOrg[] = mapToOverviewOrgList(dataArray, LayoutState.Offered);
-        // state.overviewOrgs = responseList;
+        const responseList: OverviewOrg[] = mapToOverviewOrgList(dataArray, LayoutState.Offered);
+        state.overviewOrgs = responseList;
         state.loading = false;
       })
       .addCase(fetchOverviewOrgsOffered.rejected, (state, action) => {
