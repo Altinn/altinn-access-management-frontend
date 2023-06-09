dd5b19cb680592ca131e8d21c9b22c66e75ddebe
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-06-09 20:51:02 +0200

    Redesign close button (#410)

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,8 +1,7 @@
-import { Button, ButtonColor, ButtonSize, ButtonVariant } from '@digdir/design-system-react';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { ReactComponent as ExitIcon } from '@/assets/Error.svg';
+import { ReactComponent as CloseIcon } from '@/assets/RedClose.svg';
 import { RouterPath } from '@/routes/Router';
 
 import { UserInfoBar } from '../UserInfoBar/UserInfoBar';
@@ -25,15 +24,17 @@ export const PageContainer = ({ children }: PageContainerProps) => {
     <div className={classes.pageMargin}>
       <div className={classes.pageContainer}>
         <UserInfoBar />
-        <div className={classes.exitButton}>
-          <Button
-            variant={ButtonVariant.Quiet}
-            color={ButtonColor.Secondary}
-            size={ButtonSize.Medium}
-            icon={<ExitIcon />}
+        <div className={classes.closeButtonContainer}>
+          <button
+            className={classes.closeButton}
             aria-label={String(t('common.close'))}
             onClick={redirectToProfile}
-          ></Button>
+          >
+            <CloseIcon
+              width={36}
+              height={30}
+            />
+          </button>
         </div>
         <div>{children}</div>
       </div>
