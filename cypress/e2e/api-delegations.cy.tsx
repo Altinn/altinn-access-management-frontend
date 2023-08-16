f23384ff48e68e5036253894dd6cc302df2138eb
Albertlarsen <31471142+Albertlarsen@users.noreply.github.com>
2023-08-16 13:05:39 +0200

    Chore/fix lint staged (#489)
    * fix lint stage
    
    * test lint stage
    
    * run yarn format
    
    * update package

--- cypress/e2e/api-delegations.cy.tsx
+++
@@ -1,9 +0,0 @@
-import { ApiDelegationPath } from '@/routes/paths';
-
-describe(ApiDelegationPath.OfferedApiDelegations + '/' + ApiDelegationPath.Overview, () => {
-  it('renders correctly', () => {
-    cy.visit(ApiDelegationPath.OfferedApiDelegations + '/' + ApiDelegationPath.Overview);
-  });
-});
-
-export {};
