f08fc960d41ab2207d3728bd14ec9a9a320932fe
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-08-18 09:37:43 +0200

    Initial commit

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+import classes from './Modal.module.css';
+
+export const Modal = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
