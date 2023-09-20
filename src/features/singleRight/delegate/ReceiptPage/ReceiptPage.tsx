90bde5bc1154b9e40552dce72504e372e7eec9ea
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2023-09-20 15:59:50 +0200

    Initial commit

--- src/features/singleRight/delegate/ReceiptPage/ReceiptPage.tsx
+++ src/features/singleRight/delegate/ReceiptPage/ReceiptPage.tsx
@@ -8,8 +8,11 @@ import { useEffect } from 'react';
 import { SingleRightPath } from '@/routes/paths';
 import { Page, PageContainer, PageContent, PageHeader } from '@/components';
 import { useMediaQuery } from '@/resources/hooks';
-import { useAppDispatch } from '@/rtk/app/hooks';
+import { useAppDispatch, useAppSelector } from '@/rtk/app/hooks';
 import { resetServicesWithStatus } from '@/rtk/features/singleRights/singleRightsSlice';
+import { ReduxStatusResponse } from '@/dataObjects/dtos/singleRights/DelegationInputDto';
+
+import { ResourceActionBar } from '../ChooseServicePage/ResourceActionBar/ResourceActionBar';
 
 import classes from './ReceiptPage.module.css';
 
@@ -18,10 +21,48 @@ export const ReceiptPage = () => {
   const navigate = useNavigate();
   const isSm = useMediaQuery('(max-width: 768px)');
   const dispatch = useAppDispatch();
+  const failedDelegations = useAppSelector((state) =>
+    state.singleRightsSlice.processedDelegations.filter(
+      (s) => s.status !== ReduxStatusResponse.Fulfilled,
+    ),
+  );
+  const succesfulDelegations = useAppSelector((state) =>
+    state.singleRightsSlice.processedDelegations.filter(
+      (s) => s.status !== ReduxStatusResponse.Rejected,
+    ),
+  );
 
   useEffect(() => {
     void dispatch(resetServicesWithStatus());
   }, []);
+
+  const failedActionBars = failedDelegations.map((resource) => {
+    return (
+      <ResourceActionBar
+        key={resource.To[0].id}
+        title={resource.serviceDto.serviceTitle}
+        subtitle={resource.serviceDto.serviceOwner}
+        status={'NotDelegable'}
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        compact={isSm}
+      ></ResourceActionBar>
+    );
+  });
+
+  const successfulActionBars = succesfulDelegations.map((resource) => {
+    return (
+      <ResourceActionBar
+        key={resource.To[0].id}
+        title={resource.serviceDto.serviceTitle}
+        subtitle={resource.serviceDto.serviceOwner}
+        status={'Delegable'}
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        compact={isSm}
+      ></ResourceActionBar>
+    );
+  });
+
+  console.log();
 
   return (
     <PageContainer>
@@ -35,6 +76,8 @@ export const ReceiptPage = () => {
             {t('single_rights.has_received_these_rights', { name: 'ANNEMA FIGMA' })}
           </Ingress>
           <Paragraph spacing>{t('single_rights.rights_are_valid_until_deletion')}</Paragraph>
+          <div className={classes.actionBars}>{failedActionBars}</div>
+          <div className={classes.actionBars}>{successfulActionBars}</div>
           <div className={classes.successButtonContainer}>
             <Button
               onClick={() => {
---
+++ src/features/singleRight/delegate/ReceiptPage/ResourceActionBar/ResourceActionBar.tsx
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import * as React from 'react';
+import { useMemo, useState } from 'react';
+import { Button, Paragraph } from '@digdir/design-system-react';
+import { PlusCircleIcon, ExclamationmarkTriangleIcon, ArrowUndoIcon } from '@navikt/aksel-icons';
+import { useTranslation } from 'react-i18next';
+
+import { ActionBar, type ActionBarProps } from '@/components';
+import { useUpdate } from '@/resources/hooks/useUpdate';
+import { usePrevious } from '@/resources/hooks';
+
+import classes from './ResourceActionBar.module.css';
+
+export interface ResourceActionBarProps
+  extends Pick<ActionBarProps, 'subtitle' | 'title' | 'children' | 'color'> {
+  /** Shows details about why the service isn't delegable */
+  errorText?: string | undefined;
+
+  /** When true saves as much space as possible. Usually true for smaller screens */
+  compact?: boolean;
+}
+
+export const ResourceActionBar = ({
+  subtitle,
+  title,
+  children,
+  status = 'Unchecked',
+  errorText = undefined,
+  onAddClick,
+  onRemoveClick,
+  compact = false,
+}: ResourceActionBarProps) => {
+  const { t } = useTranslation('common');
+  const [open, setOpen] = useState(false);
+
+  const previousStatus = usePrevious(status);
+
+  useUpdate(() => {
+    if (status === 'NotDelegable' && previousStatus !== undefined) {
+      setOpen(true);
+    }
+  }, [status]);
+
+  const color = useMemo(() => {
+    switch (status) {
+      case 'Delegable':
+      case 'PartiallyDelegable':
+        return 'success';
+      case 'NotDelegable':
+        return 'danger';
+      default:
+        return 'neutral';
+    }
+  }, [status]);
+
+  const addButton = (
+    <Button
+      variant='quiet'
+      icon={<PlusCircleIcon title='add' />}
+      size={compact ? 'large' : 'medium'}
+      onClick={onAddClick}
+      iconPlacement='right'
+    >
+      {!compact && t('common.add')}
+    </Button>
+  );
+
+  const undoButton = (
+    <Button
+      variant='quiet'
+      icon={<ArrowUndoIcon title={t('common.undo')} />}
+      size={compact ? 'large' : 'medium'}
+      onClick={onRemoveClick}
+      iconPlacement='right'
+    >
+      {!compact && t('common.undo')}
+    </Button>
+  );
+
+  const notDelegableLabel = (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div
+      className={classes.notDelegableContainer}
+      onClick={() => {
+        setOpen(!open);
+      }}
+    >
+      {!compact && <Paragraph>{errorText}</Paragraph>}
+      <ExclamationmarkTriangleIcon
+        className={classes.notDelegableIcon}
+        fontSize='1.5rem'
+      />
+    </div>
+  );
+
+  const action = () => {
+    switch (status) {
+      case 'Delegable':
+      case 'PartiallyDelegable':
+        return undoButton;
+      case 'NotDelegable':
+        return notDelegableLabel;
+      default:
+        return addButton;
+    }
+  };
+
+  return (
+    <ActionBar
+      subtitle={subtitle}
+      title={title}
+      color={color}
+      actions={action()}
+      open={open}
+      onClick={() => {
+        setOpen(!open);
+      }}
+    >
+      <div className={classes.content}>
+        <div>{children}</div>
+      </div>
+    </ActionBar>
+  );
+};
