5fca846a033c8b118ed3349bfa5c5db4e89a7785
Alexandra Vedeler <arnex04@gmail.com>
2023-09-21 17:26:22 +0200

    Add serialization to enum resourcePartyType (#556)
    * frontend now handles resources with missing resourceReferences
    
    * added enum serialization to resourcePartyType

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Enums/ResourcePartyType.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Enums/ResourcePartyType.cs
@@ -1,10 +1,12 @@
 ﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Altinn.AccessManagement.UI.Core.Enums
 {
     /// <summary>
     /// Defines the type of party that a resource is targeting
     /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ResourcePartyType
     {
         [EnumMember(Value = "PrivatePerson")]
