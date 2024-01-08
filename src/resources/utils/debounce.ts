d83f575f26b4a3595854d4ee031f94d4f29761de
Allinox <arnex04@gmail.com>
2024-01-08 13:40:16 +0100

    fixes from QA

--- src/resources/utils/debounce.ts
+++ src/resources/utils/debounce.ts
@@ -1,9 +1,16 @@
-// Delays calling of func by timeout ms
+/**
+ * Delays the execution of a function by a specified amount of time.
+ *
+ * @param {(...args: any[]) => void} func - The function to delay.
+ * @param {number} [timeout=300] - The amount of time in milliseconds to delay the function call.
+ * @returns {Function} A new function that delays the invocation of the input function.
+ *
+ * @example
+ * debounce(() => console.log('Hello, world!'), 500);
+ */
 export function debounce(func: (...args: any[]) => void, timeout = 300) {
-  console.log('ready to bounce');
   let timer: NodeJS.Timeout;
   return (...args: any[]) => {
-    console.log('time is out!');
     clearTimeout(timer);
     timer = setTimeout(() => {
       func(...args);
