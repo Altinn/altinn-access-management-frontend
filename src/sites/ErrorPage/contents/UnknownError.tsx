2e43ad575714da9ff923b78948b7fb6fc87d4fe2
Allinox <arnex04@gmail.com>
2024-01-12 13:41:41 +0100

    remade error page into case based version

---
+++ src/sites/ErrorPage/contents/UnknownError.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useTranslation } from 'react-i18next';
+import * as React from 'react';
+import { Paragraph, Heading } from '@digdir/design-system-react';
+
+import SeagullIcon from '@/assets/Seagull.svg?react';
+
+import classes from '../ErrorPage.module.css';
+
+export const UnknownError = () => {
+  const { t } = useTranslation('common');
+
+  return (
+    <div>
+      <Heading
+        size='xlarge'
+        spacing
+        className={classes.header}
+      >
+        {t('error_page.unknown_error_header')}
+      </Heading>
+      <div className={classes.flexContainer}>
+        <div className={classes.leftContainer}>
+          <Paragraph
+            size='large'
+            spacing
+            className={classes.contentText}
+          >
+            {t('error_page.unknown_error_description')}
+          </Paragraph>
+
+          <Paragraph
+            size='large'
+            spacing
+            className={classes.contentText}
+          >
+            {t('error_page.if_persistent_contact_service')}
+          </Paragraph>
+
+          <Paragraph
+            size='large'
+            spacing
+            className={classes.contentText}
+          >
+            <a
+              className={classes.link}
+              href='https://www.altinn.no/help'
+            >
+              {t('error_page.go_to_help')}
+            </a>
+          </Paragraph>
+        </div>
+        <div className={classes.rightContainer}>
+          <SeagullIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
