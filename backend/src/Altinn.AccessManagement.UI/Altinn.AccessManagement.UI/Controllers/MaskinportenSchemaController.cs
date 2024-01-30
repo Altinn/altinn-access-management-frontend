e042b58206963f6e01063626b1da1a580e2d6de5
Albert Ohrem Larsen <olarsenalbert@gmail.com>
2024-01-30 10:04:23 +0100

    Start on BFF implementation

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/MaskinportenSchemaController.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/MaskinportenSchemaController.cs
@@ -5,6 +5,8 @@ using System.Text.Json.Serialization;
 using Altinn.AccessManagement.UI.Core.Helpers;
 using Altinn.AccessManagement.UI.Core.Models;
 using Altinn.AccessManagement.UI.Core.Models.Delegation.Frontend;
+using Altinn.AccessManagement.UI.Core.Models.ResourceRegistry;
+using Altinn.AccessManagement.UI.Core.Models.ResourceRegistry.Frontend;
 using Altinn.AccessManagement.UI.Core.Models.SingleRight;
 using Altinn.AccessManagement.UI.Core.Services.Interfaces;
 using Altinn.AccessManagement.UI.Filters;
@@ -231,6 +233,35 @@ namespace Altinn.AccessManagement.UI.Controllers
             catch (Exception)
             {
                 return new ObjectResult(ProblemDetailsFactory.CreateProblemDetails(HttpContext));
+            }
+        }
+        
+        /// <summary>
+        ///     Search through all delegable service resources and returns matches
+        /// </summary>
+        /// <returns>Paginated search results</returns>
+        [HttpGet]
+        [Authorize]
+        [Route("paginatedSearch")]
+        public async Task<ActionResult<PaginatedList<ServiceResourceFE>>> PaginatedSearch([FromQuery] PaginatedSearchParams parameters)
+        {
+            int userId = AuthenticationHelper.GetUserId(_httpContextAccessor.HttpContext);
+            UserProfile userProfile = await _profileService.GetUserProfile(userId);
+            string languageCode = ProfileHelper.GetLanguageCodeForUser(userProfile);
+
+            try
+            {
+                return await _resourceService.GetPaginatedSearchResults(languageCode, parameters.ROFilters, parameters.SearchString, parameters.Page, parameters.ResultsPerPage);
+            }
+            catch (HttpStatusException ex)
+            {
+                if (ex.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return NoContent();
+                }
+
+                string responseContent = ex.Message;
+                return new ObjectResult(ProblemDetailsFactory.CreateProblemDetails(HttpContext, (int?)ex.StatusCode, "Unexpected HttpStatus response", detail: responseContent));
             }
         }
     }
--- src/rtk/features/apiDelegation/apiDelegationApi.ts
+++ src/rtk/features/apiDelegation/apiDelegationApi.ts
@@ -4,10 +4,25 @@ import type { IdValuePair } from '@/dataObjects/dtos/IdValuePair';
 import type { DelegationAccessResult } from '@/dataObjects/dtos/resourceDelegation';
 import { getCookie } from '@/resources/Cookie/CookieMethods';
 
+import type { DelegableApi } from './delegableApi/delegableApiSlice';
+
 export type ResourceReference = {
   resource: IdValuePair[];
   action?: string;
 };
+
+type PaginatedApiSearchResults = {
+  page: number;
+  numEntriesTotal: number;
+  results: DelegableApi[];
+};
+
+interface searchParams {
+  searchString: string;
+  ROfilters: string[];
+  page: number;
+  resultsPerPage: number;
+}
 
 const baseUrl = import.meta.env.BASE_URL + 'accessmanagement/api/v1';
 
@@ -22,7 +37,6 @@ export const apiDelegationApi = createApi({
     },
   }),
   tagTypes: ['APIs'],
-
   endpoints: (builder) => ({
     delegationCheck: builder.mutation<
       DelegationAccessResult,
@@ -38,6 +52,16 @@ export const apiDelegationApi = createApi({
       },
       transformErrorResponse: (response: { status: string | number }) => {
         return response.status;
+      },
+    }),
+    getPaginatedSearch: builder.query<PaginatedApiSearchResults, searchParams>({
+      query: (args) => {
+        const { searchString, ROfilters, page, resultsPerPage } = args;
+        let filterUrl = '';
+        for (const filter of ROfilters) {
+          filterUrl = filterUrl + `&ROFilters=${filter}`;
+        }
+        return `resources/paginatedSearch?Page=${page}&ResultsPerPage=${resultsPerPage}&SearchString=${searchString}${filterUrl}`;
       },
     }),
   }),
