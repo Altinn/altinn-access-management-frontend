dafbdc0582e859394124ef7ad547486ee4f82bda
albertlarsen <olarsenalbert@gmail.com>
2024-02-06 15:47:17 +0100

    add eslint ignore

--- src/resources/utils/debounce.ts
+++ src/resources/utils/debounce.ts
@@ -8,6 +8,8 @@
  * @example
  * debounce(() => console.log('Hello, world!'), 500);
  */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function debounce(func: (...args: any[]) => void, timeout = 300) {
   let timer: NodeJS.Timeout;
   return (...args: any[]) => {
