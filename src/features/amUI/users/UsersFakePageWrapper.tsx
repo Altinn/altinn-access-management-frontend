85c6a0c1ab211fc88f3da874216523e7092ace0e
Martin Gunnerud <mgunnerud@gmail.com>
2024-07-19 08:13:35 +0200

    add placeholder for header and left menu

---
+++ src/features/amUI/users/UsersFakePageWrapper.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Divider, Label, Search } from '@digdir/designsystemet-react';
+import {
+  ClockDashedIcon,
+  HandshakeIcon,
+  PersonGroupIcon,
+  RobotSmileIcon,
+} from '@navikt/aksel-icons';
+import AltinnTextLogo from '@/assets/AltinnTextLogo.svg?react';
+import classes from './UsersFakePageWrapper.module.css';
+
+interface UsersFakePageWrapperProps {
+  children?: React.ReactNode;
+}
+
+export const UsersFakePageWrapper = ({ children }: UsersFakePageWrapperProps): React.ReactNode => {
+  return (
+    <div className={classes.pageWrapper}>
+      <div className={classes.topBar}>
+        <AltinnTextLogo title='Altinn' />
+        <div>
+          <Search placeholder='Søk i Altinn' />
+        </div>
+        <div className={classes.menuButton}>
+          <span>Meny</span>
+          <div className={classes.companySquare} />
+        </div>
+      </div>
+      <div className={classes.contentWrapper}>
+        <div className={classes.sideMenu}>
+          <Label className={classes.companyTitle}>
+            <span className={classes.companySquare} />
+            Fysioterapautene
+          </Label>
+          <Divider />
+          {[
+            { title: 'Brukere og grupper', icon: <PersonGroupIcon /> },
+            { title: 'Fullmakter', icon: <HandshakeIcon /> },
+            { title: 'Maskintilganger', icon: <RobotSmileIcon /> },
+            { title: 'Aktivitetslogg', icon: <ClockDashedIcon /> },
+            { title: 'Virksomhetsprofil', icon: <HandshakeIcon /> },
+          ].map((item) => {
+            return (
+              <div
+                key={item.title}
+                className={classes.sideMenuItem}
+              >
+                {item.icon}
+                {item.title}
+              </div>
+            );
+          })}
+        </div>
+        <div className={classes.pageContent}>{children}</div>
+      </div>
+    </div>
+  );
+};
