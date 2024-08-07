538d2e628a95731843f027d3ebc348a9a5b28f5b
Sondre Wittek <sondrewittek@gmail.com>
2024-08-07 10:37:37 +0200

    Sort right holders list

--- src/features/amUI/users/useFilteredRightHolders.ts
+++ src/features/amUI/users/useFilteredRightHolders.ts
@@ -1,7 +1,7 @@
 import { getTotalNumOfPages, getArrayPage } from '@/resources/utils';
 import type { RightHolder } from '@/rtk/features/userInfo/userInfoApi';
 import { useGetRightHoldersQuery } from '@/rtk/features/userInfo/userInfoApi';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 const isSearchMatch = (searchString: string, rightHolder: RightHolder): boolean => {
   const isNameMatch = rightHolder.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1;
@@ -9,6 +9,34 @@ const isSearchMatch = (searchString: string, rightHolder: RightHolder): boolean 
   const isOrgNrMatch = rightHolder.organizationNumber === searchString;
   return isNameMatch || isPersonIdMatch || isOrgNrMatch;
 };
+
+// Custom sort function to prioritize registryRoles containing "DAGL"
+const sortRightHolders = (rightHolders: RightHolder[]): RightHolder[] => {
+  const sorted = [...rightHolders].sort((a, b) => {
+    const aHasDAGL = a.registryRoles.includes('DAGL');
+    const bHasDAGL = b.registryRoles.includes('DAGL');
+
+    if (aHasDAGL && !bHasDAGL) {
+      return -1;
+    } else if (!aHasDAGL && bHasDAGL) {
+      return 1;
+    } else {
+      return a.name.localeCompare(b.name);
+    }
+  });
+  return sorted;
+};
+
+export const sortRightholderList = (list: RightHolder[]): RightHolder[] =>
+  sortRightHolders(list).map((rightHolder) => {
+    if (rightHolder.inheritingRightHolders) {
+      return {
+        ...rightHolder,
+        inheritingRightHolders: sortRightHolders(rightHolder.inheritingRightHolders),
+      };
+    }
+    return rightHolder;
+  });
 
 const computePageEntries = (
   searchString: string,
@@ -56,18 +84,22 @@ export const useFilteredRightHolders = (
   pageSize: number,
 ) => {
   const { data: rightHolders } = useGetRightHoldersQuery();
+
   const [pageEntries, setPageEntries] = useState<RightHolder[]>([]);
   const [numOfPages, setNumOfPages] = useState<number>(1);
   const [searchResultLength, setSearchResultLength] = useState<number>(0);
+
+  // Sorting the list of rightHolders only when the list changes
+  const sortedRightHolders = useMemo(() => sortRightholderList(rightHolders || []), [rightHolders]);
 
   useEffect(() => {
     const { pageEntries, numOfPages, searchResultLength } = computePageEntries(
       searchString,
       currentPage,
       pageSize,
-      rightHolders,
+      sortedRightHolders,
     );
-    setPageEntries(pageEntries);
+    setPageEntries(pageEntries.filter((entry) => entry !== undefined));
     setNumOfPages(numOfPages);
     setSearchResultLength(searchResultLength);
   }, [searchString, currentPage, rightHolders]);
