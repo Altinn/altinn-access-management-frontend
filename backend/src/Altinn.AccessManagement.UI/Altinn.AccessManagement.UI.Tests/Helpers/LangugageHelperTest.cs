7ed090dddd968ff73d26a830c59199eb57b3f90a
Sondre Wittek <sondrewittek@gmail.com>
2024-08-12 13:15:34 +0200

    Add namespace

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Tests/Helpers/LangugageHelperTest.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Tests/Helpers/LangugageHelperTest.cs
@@ -2,123 +2,125 @@ using Altinn.AccessManagement.UI.Core.Helpers;
 using Microsoft.AspNetCore.Http;
 using Moq;
 
-
-[Collection("LanguageHelper Tests")]
-public class LanguageHelperTests
+namespace Altinn.AccessManagement.UI.Tests.Helpers
 {
-
-    [Fact]
-    public void GetAltinnPersistenceCookieValueFrontendStandard_ShouldReturnEmptyString_WhenCookieIsNull()
+    [Collection("LanguageHelper Tests")]
+    public class LanguageHelperTests
     {
-        // Arrange
-        var httpContextMock = new Mock<HttpContext>();
-        var requestMock = new Mock<HttpRequest>();
-        var cookiesMock = new Mock<IRequestCookieCollection>();
 
-        cookiesMock.Setup(c => c["altinnPersistentContext"]).Returns((string)null);
-        requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
-        httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
+        [Fact]
+        public void GetAltinnPersistenceCookieValueFrontendStandard_ShouldReturnEmptyString_WhenCookieIsNull()
+        {
+            // Arrange
+            var httpContextMock = new Mock<HttpContext>();
+            var requestMock = new Mock<HttpRequest>();
+            var cookiesMock = new Mock<IRequestCookieCollection>();
 
-        // Act
-        var result = LanguageHelper.GetAltinnPersistenceCookieValueFrontendStandard(httpContextMock.Object);
+            cookiesMock.Setup(c => c["altinnPersistentContext"]).Returns((string)null);
+            requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
+            httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
 
-        // Assert
-        Assert.Equal(string.Empty, result);
-    }
+            // Act
+            var result = LanguageHelper.GetAltinnPersistenceCookieValueFrontendStandard(httpContextMock.Object);
 
-    [Theory]
-    [InlineData("UL=1044", "no_nb")]
-    [InlineData("UL=2068", "no_nn")]
-    [InlineData("UL=1033", "en")]
-    public void GetAltinnPersistenceCookieValueFrontendStandard_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
-    {
-        // Arrange
-        var httpContextMock = new Mock<HttpContext>();
-        var requestMock = new Mock<HttpRequest>();
-        var cookiesMock = new Mock<IRequestCookieCollection>();
+            // Assert
+            Assert.Equal(string.Empty, result);
+        }
 
-        cookiesMock.Setup(c => c["altinnPersistentContext"]).Returns(cookieValue);
-        requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
-        httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
+        [Theory]
+        [InlineData("UL=1044", "no_nb")]
+        [InlineData("UL=2068", "no_nn")]
+        [InlineData("UL=1033", "en")]
+        public void GetAltinnPersistenceCookieValueFrontendStandard_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
+        {
+            // Arrange
+            var httpContextMock = new Mock<HttpContext>();
+            var requestMock = new Mock<HttpRequest>();
+            var cookiesMock = new Mock<IRequestCookieCollection>();
 
-        // Act
-        var result = LanguageHelper.GetAltinnPersistenceCookieValueFrontendStandard(httpContextMock.Object);
+            cookiesMock.Setup(c => c["altinnPersistentContext"]).Returns(cookieValue);
+            requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
+            httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
 
-        // Assert
-        Assert.Equal(expectedValue, result);
-    }
+            // Act
+            var result = LanguageHelper.GetAltinnPersistenceCookieValueFrontendStandard(httpContextMock.Object);
+
+            // Assert
+            Assert.Equal(expectedValue, result);
+        }
 
 
-    [Fact]
-    public void GetSelectedLanguageCookieValueBackendStandard_ShouldReturnEmptyString_WhenCookieIsNull()
-    {
-        // Arrange
-        var httpContextMock = new Mock<HttpContext>();
-        var requestMock = new Mock<HttpRequest>();
-        var cookiesMock = new Mock<IRequestCookieCollection>();
+        [Fact]
+        public void GetSelectedLanguageCookieValueBackendStandard_ShouldReturnEmptyString_WhenCookieIsNull()
+        {
+            // Arrange
+            var httpContextMock = new Mock<HttpContext>();
+            var requestMock = new Mock<HttpRequest>();
+            var cookiesMock = new Mock<IRequestCookieCollection>();
 
-        cookiesMock.Setup(c => c["selectedLanguage"]).Returns((string)null);
-        requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
-        httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
+            cookiesMock.Setup(c => c["selectedLanguage"]).Returns((string)null);
+            requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
+            httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
 
-        // Act
-        var result = LanguageHelper.GetSelectedLanguageCookieValueBackendStandard(httpContextMock.Object);
+            // Act
+            var result = LanguageHelper.GetSelectedLanguageCookieValueBackendStandard(httpContextMock.Object);
 
-        // Assert
-        Assert.Equal(string.Empty, result);
-    }
+            // Assert
+            Assert.Equal(string.Empty, result);
+        }
 
-    [Theory]
-    [InlineData("UL=1044", "nb")]
-    [InlineData("UL=2068", "nn")]
-    [InlineData("UL=1033", "en")]
-    public void GetSelectedLanguageCookieValueBackendStandard_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
-    {
-        // Arrange
-        var httpContextMock = new Mock<HttpContext>();
-        var requestMock = new Mock<HttpRequest>();
-        var cookiesMock = new Mock<IRequestCookieCollection>();
+        [Theory]
+        [InlineData("UL=1044", "nb")]
+        [InlineData("UL=2068", "nn")]
+        [InlineData("UL=1033", "en")]
+        public void GetSelectedLanguageCookieValueBackendStandard_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
+        {
+            // Arrange
+            var httpContextMock = new Mock<HttpContext>();
+            var requestMock = new Mock<HttpRequest>();
+            var cookiesMock = new Mock<IRequestCookieCollection>();
 
-        cookiesMock.Setup(c => c["selectedLanguage"]).Returns(cookieValue);
-        requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
-        httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
+            cookiesMock.Setup(c => c["selectedLanguage"]).Returns(cookieValue);
+            requestMock.Setup(r => r.Cookies).Returns(cookiesMock.Object);
+            httpContextMock.Setup(h => h.Request).Returns(requestMock.Object);
 
-        // Act
-        var result = LanguageHelper.GetSelectedLanguageCookieValueBackendStandard(httpContextMock.Object);
+            // Act
+            var result = LanguageHelper.GetSelectedLanguageCookieValueBackendStandard(httpContextMock.Object);
 
-        // Assert
-        Assert.Equal(expectedValue, result);
-    }
+            // Assert
+            Assert.Equal(expectedValue, result);
+        }
 
-    [Theory]
-    [InlineData("UL=1044", "nb")]
-    [InlineData("UL=2068", "nn")]
-    [InlineData("UL=1033", "en")]
-    [InlineData("no_nb", "nb")]
-    [InlineData("no_nn", "nn")]
-    [InlineData("en", "en")]
-    public void GetBackendStandardLanguage_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
-    {
-        // Act
-        var result = LanguageHelper.GetBackendStandardLanguage(cookieValue);
+        [Theory]
+        [InlineData("UL=1044", "nb")]
+        [InlineData("UL=2068", "nn")]
+        [InlineData("UL=1033", "en")]
+        [InlineData("no_nb", "nb")]
+        [InlineData("no_nn", "nn")]
+        [InlineData("en", "en")]
+        public void GetBackendStandardLanguage_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
+        {
+            // Act
+            var result = LanguageHelper.GetBackendStandardLanguage(cookieValue);
 
-        // Assert
-        Assert.Equal(expectedValue, result);
-    }
+            // Assert
+            Assert.Equal(expectedValue, result);
+        }
 
-    [Theory]
-    [InlineData("UL=1044", "no_nb")]
-    [InlineData("UL=2068", "no_nn")]
-    [InlineData("UL=1033", "en")]
-    [InlineData("nb", "no_nb")]
-    [InlineData("nn", "no_nn")]
-    [InlineData("en", "en")]
-    public void GetFrontendStandardLanguage_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
-    {
-        // Act
-        var result = LanguageHelper.GetFrontendStandardLanguage(cookieValue);
+        [Theory]
+        [InlineData("UL=1044", "no_nb")]
+        [InlineData("UL=2068", "no_nn")]
+        [InlineData("UL=1033", "en")]
+        [InlineData("nb", "no_nb")]
+        [InlineData("nn", "no_nn")]
+        [InlineData("en", "en")]
+        public void GetFrontendStandardLanguage_ShouldReturnExpectedValue(string cookieValue, string expectedValue)
+        {
+            // Act
+            var result = LanguageHelper.GetFrontendStandardLanguage(cookieValue);
 
-        // Assert
-        Assert.Equal(expectedValue, result);
+            // Assert
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
