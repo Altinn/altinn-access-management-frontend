95d440ddd7d304f3164985eaa9583acd944f7bea
Sondre Wittek <sondrewittek@gmail.com>
2024-09-12 14:08:39 +0200

    Refactor usePagination.ts to optimize pagination logic

--- src/resources/hooks/usePagination.ts
+++ src/resources/hooks/usePagination.ts
@@ -1,28 +1,19 @@
 import { useState } from 'react';
 
+import { getArrayPage, getTotalNumOfPages } from '../utils/arrayUtils';
+
 interface PaginationHook<T> {
   currentPage: number;
   totalPages: number;
   itemsPerPage: number;
   paginatedData: T[];
-  nextPage: () => void;
-  prevPage: () => void;
   goToPage: (page: number) => void;
 }
 
 function usePagination<T>(data: T[], itemsPerPage: number): PaginationHook<T> {
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(data.length / itemsPerPage);
-
-  const paginatedData = data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-
-  const nextPage = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-  };
-
-  const prevPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-  };
+  const totalPages = getTotalNumOfPages(data, itemsPerPage);
+  const paginatedData = getArrayPage(data, currentPage, itemsPerPage);
 
   const goToPage = (page: number) => {
     const pageNumber = Math.max(1, Math.min(page, totalPages));
@@ -34,8 +25,6 @@ function usePagination<T>(data: T[], itemsPerPage: number): PaginationHook<T> {
     totalPages,
     itemsPerPage,
     paginatedData,
-    nextPage,
-    prevPage,
     goToPage,
   };
 }
