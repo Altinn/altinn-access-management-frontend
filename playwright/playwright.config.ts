9e68f89cc910748d93db2d97105764e3191a5c3f
Vegard Nyeng <vegard.nyeng@gmail.com>
2024-09-23 10:31:43 +0200

    uppercase

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -40,8 +40,8 @@ const config: PlaywrightTestConfig = {
       'html',
       {
         open: 'on-failure',
-        //outputDir: `playwright-report/${process.env.environment ?? 'at24'}/run-${process.env.github_run_number ?? '1'}-${process.env.github_run_attempt ?? '1'}`,
-        outputFolder: `playwright-report/${process.env.environment ?? 'at24'}}`,
+        //outputDir: `playwright-report/${process.env.ENVIRONMENT ?? 'AT24'}/run-${process.env.GITHUB_RUN_NUMBER ?? '1'}-${process.env.GITHUB_RUN_ATTEMPT ?? '1'}`,
+        outputFolder: `playwright-report/${process.env.ENVIRONMENT?.toUpperCase() ?? 'AT24'}`,
       },
     ],
   ],
