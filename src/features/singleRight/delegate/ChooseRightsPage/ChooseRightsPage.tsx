26370858c77aaf234f79ef82634bfa9fe925f664
Sondre Wittek <sondrewittek@gmail.com>
2024-10-07 16:14:48 +0200

    fix btn color

--- src/features/singleRight/delegate/ChooseRightsPage/ChooseRightsPage.tsx
+++ src/features/singleRight/delegate/ChooseRightsPage/ChooseRightsPage.tsx
@@ -201,7 +201,7 @@ export const ChooseRightsPage = () => {
         <Popover.Context>
           <Popover.Trigger
             variant='primary'
-            color='neutral'
+            color='accent'
             disabled={delegationCount < 1}
             onClick={() => setPopoverOpen(!popoverOpen)}
           >
