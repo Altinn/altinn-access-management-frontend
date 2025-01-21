3bd6903bd9ec65b242236fe8ac120108f32cb6ef
Martin Gunnerud <mgunnerud@gmail.com>
2025-01-21 08:35:04 +0100

    whitespace changes

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Tests/Utils/AssertionUtil.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Tests/Utils/AssertionUtil.cs
@@ -425,18 +425,18 @@ namespace Altinn.AccessManagement.UI.Tests.Utils
             Assert.Equal(expected.System.SystemVendorOrgNumber, actual.System.SystemVendorOrgNumber);
             AssertCollections(expected.Resources, actual.Resources, AssertEqual);
         }
-
+        
         public static void AssertEqual(RoleAssignment expected, RoleAssignment actual)
         {
             Assert.NotNull(actual);
             Assert.NotNull(expected);
-          
+
             Assert.Equal(expected.Id, actual.Id);
             Assert.Equal(expected.RoleId, actual.RoleId);
             Assert.Equal(expected.ToId, actual.ToId);
             AssertEqual(expected.Role, actual.Role);
         }
-      
+
         static void AssertEqual(Role expected, Role actual)
         {
             Assert.NotNull(actual);
