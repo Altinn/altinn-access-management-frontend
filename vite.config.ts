3e0c92fe8882c815d63eaffe0b4920557cde6f0e
Sondre Wittek <sondrewittek@gmail.com>
2025-02-04 09:22:54 +0100

    Fix Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig(
+export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
