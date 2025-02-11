6d2ad3b2492f90c7f8e30a1bc356891b9cfbe6c4
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-02-11 11:09:56 +0100

    added missing test that was also deleted by accident

--- playwright/e2eTests/singleRightsDelegering.spec.ts
+++ playwright/e2eTests/singleRightsDelegering.spec.ts
@@ -1,7 +1,67 @@
 /* eslint-disable import/default */
 /* eslint-disable import/no-named-as-default-member */
 import { test } from './../fixture/pomFixture';
-test.describe('User with DAGL/HADM role without having resource access themselves', () => {
+test.describe('User with DAGL/HADM role without having resource access themselves debug', () => {
+  test('User A who is DAGL/HADM for org delegates resources/Altinn 3 app/Altinn 2 services to User B', async ({
+    login,
+    delegate,
+    delegateRights,
+    deleteRights,
+    logoutUser,
+    instantiateResources,
+    coverebyRights,
+    delegateRoles,
+    context,
+  }) => {
+    test.setTimeout(300000);
+
+    await login.loginWithUser('04885299593');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+
+    //To delete rights - setup
+    // GIVEN user without delegated rights to ROMANTISK ESKE
+    await deleteRights.revokeRightsSSN('SKYFRI GATE');
+
+    //To delegate rights
+    // WHEN user delegates to ROMANTISK ESKE
+    await delegate.delegateToSSN('19856097121', 'GATE');
+    await delegateRoles.delegateRoles(
+      'Tilgangsstyring',
+      'Begrenset signeringsrettighet',
+      'SKYFRI GATE',
+    );
+    await delegateRights.delegateRightsToSSN('Ressurs for enkeltrettigheter testing');
+    await delegateRights.delegateRightsToSSN('autorisasjon-automatisert-app');
+    await delegateRights.delegateRightsToSSN('Altinn2 reporting service for authorization tests');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+    // await context1.close();
+    await context.clearCookies();
+
+    //Login with covereby User
+
+    await login.loginWithUser('19856097121');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+    await coverebyRights.checkCoverebyRights();
+
+    //Instantiate app to which user had got rights
+    await instantiateResources.instantiateApp('ULIK FLAT TIGER AS');
+    // await context.close();
+
+    //To delegate rights
+    // WHEN user delegates to KLIPPFISK
+    await delegate.delegateToSSN('04880748144', 'KLIPPFISK');
+    await delegateRights.delegateRightsToSSN('autorisasjon-automatisert-app');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+    await context.clearCookies();
+
+    //Instantiate app to which user had got rights
+    await login.loginWithUser('04880748144');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+    await coverebyRights.checkCoverebyRights();
+    await instantiateResources.instantiateApp('ULIK FLAT TIGER AS');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+  });
+
   test('User A who is DAGL/HADM for org delegates resources/Altinn 3 app/Altinn 2 services to Org B', async ({
     login,
     delegate,
