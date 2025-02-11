22eefb398d4ea45e7d98c35a09441c97017c62f2
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-02-11 11:00:26 +0100

    added test that was removed by accident

--- playwright/e2eTests/singleRightsDelegering.spec.ts
+++ playwright/e2eTests/singleRightsDelegering.spec.ts
@@ -2,40 +2,128 @@
 /* eslint-disable import/no-named-as-default-member */
 import { test } from './../fixture/pomFixture';
 test.describe('User with DAGL/HADM role without having resource access themselves', () => {
-  // Testscenario:
-  // Logg inn med bruker på Altinn
-  // Gå til enkeltdelegering på profil
-  // Gå til andre med rettigheter til virksomheten
-  // Legg til ny person eller virksomhet: 24928099071: (OPPSTEMT) ABBED
-  // Gi nye enkelttjenester: Velg tjeneste som ikke kan delegeres: "Altinn2 non-delegable Reporting service"
-  // Du skal nå få feilmelding: Du kan ikke gi fullmakt til denne tjenesten
-  test('Singleright delegation - Non-delegable service', async ({
+  test('User A who is DAGL/HADM for org delegates resources/Altinn 3 app/Altinn 2 services to Org B', async ({
     login,
     delegate,
     delegateRights,
     deleteRights,
     logoutUser,
-    instantiateResources,
     coverebyRights,
+    instantiateResources,
     delegateRoles,
     context,
   }) => {
-    test.setTimeout(300000);
+    test.setTimeout(60000 * 3);
 
     await login.loginWithUser('04885299593');
     await login.chooseReportee('ULIK FLAT TIGER AS');
 
     //To delete rights - setup
-    // GIVEN user without delegated rights to ROMANTISK ESKE
-    //await deleteRights.revokeRightsSSN('SKYFRI GATE');
+    // GIVEN user without delegated rights to OPPKLARENDE OMKOMMEN TIGER AS
+    const buttonIndex = 1;
+    await deleteRights.revokeRightsOrg('GJESTFRI RESERVERT HUND DA', buttonIndex);
 
     //To delegate rights
-    // WHEN user delegates to ROMANTISK ESKE
+    // WHEN user delegates to
+    await delegate.delegateToOrg('310832170', 'GJESTFRI RESERVERT HUND DA');
+    await delegateRoles.delegateRoles(
+      'Tilgangsstyring',
+      'Begrenset signeringsrettighet',
+      'GJESTFRI RESERVERT HUND DA',
+    );
+    await delegateRights.delegateRightsToSSN('Ressurs for enkeltrettigheter testing');
+    await delegateRights.delegateRightsToSSN('autorisasjon-automatisert-app');
+    await delegateRights.delegateRightsToSSN('Altinn2 reporting service for authorization tests');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+    // await context1.close();
+    await context.clearCookies();
+
+    //Login with covereby User
+
+    await login.loginWithUser('21908498426');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+    await coverebyRights.checkCoverebyRights();
+
+    //Instantiate app to which user had got rights
+    await instantiateResources.instantiateApp('ULIK FLAT TIGER AS');
+    // await context.close();
+
+    //Delete rights
+    await deleteRights.revokeRightsOrg('UNDERFUNDIG TROFAST TIGER AS', buttonIndex);
+
+    //Delegate to another org
+    await delegate.delegateToOrg('313948579', 'UNDERFUNDIG TROFAST TIGER AS');
+    await delegateRights.delegateRightsToSSN('autorisasjon-automatisert-app');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+    await context.clearCookies();
+
+    //Instantiate app to which user had got rights
+    await login.loginWithUser('20860898609');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+    await coverebyRights.checkCoverebyRights();
+    await instantiateResources.instantiateApp('ULIK FLAT TIGER AS');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+  });
+
+  test('DAGL/HADM do not have rights to delegate Altinn2 to org Y , but has the rights to delegate same service after delegating sens role to himself', async ({
+    login,
+    delegate,
+    delegateRights,
+    deleteRights,
+    logoutUser,
+    coverebyRights,
+    context,
+    delegateRoles,
+  }) => {
+    test.setTimeout(60000 * 3);
+
+    //Login with user who has tilgangstyrring rolle
+    await login.loginWithUser('04885299593');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+
+    //To delete rights - setup
+    // GIVEN user without delegated rights to UINTERESSERT LØVEFLOKK
+    await deleteRights.revokeRightsSSN('UINTERESSERT LØVEFLOKK');
+
+    //To delegate rights
+    // WHEN user delegates rolls to UINTERESSERT LØVEFLOKK
+    await delegate.delegateToSSN('07922148605', 'LØVEFLOKK');
+    await delegateRoles.delegateRole('Taushetsbelagt post', 'Hovedadministrator');
+    await logoutUser.gotoLogoutPage('ULIK FLAT TIGER AS');
+    await context.clearCookies();
+
+    //Login with another user who has SENS role
+    await login.loginWithUser('07922148605');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+    await deleteRights.revokeRightsSSN('STORARTET KORGSTOL');
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(null);
+      }, 500),
+    );
+    await delegate.delegateToSSN('07885798378', 'KORGSTOL');
+    await delegateRights.delegateRightsToSSN(
+      'Altinn2 sensitive reporting service for Authorizaion tests',
+    );
+
+    // await context1.close();
+    await context.clearCookies();
+
+    //Login with covereby User
+    await login.loginWithUser('07885798378');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+    await coverebyRights.checkCoverebyRights();
+  });
+
+  test('Singleright delegation - Non-delegable service', async ({
+    login,
+    delegate,
+    delegateRights,
+  }) => {
+    await login.loginWithUser('04885299593');
+    await login.chooseReportee('ULIK FLAT TIGER AS');
+
     await delegate.delegateToSSN('19856097121', 'GATE');
     await delegateRights.nonDelegatebleRightsToSSN('Altinn2 non-delegable');
-
-    // Forsøk å deleger Altinn2 non-delegable Reporting service
-    // Lag ny metode som forsøker å delegere en enkelttjeneste (dublisere fra annen metode)
-    // Verifiser at det kommer opp feilmelding "Du kan ikke gi fullmakt til denne tjenesten"
   });
 });
