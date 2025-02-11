53abebe42c8892c2a0a3f413556aa45934de20eb
Sondre Wittek <sondrewittek@gmail.com>
2025-02-11 14:00:39 +0100

    fix broken file import

--- src/routes/Router/Router.tsx
+++ src/routes/Router/Router.tsx
@@ -13,7 +13,7 @@ import { ChooseRightsPage } from '@/features/singleRight/delegate/ChooseRightsPa
 import { ReceiptPage as SingleRightReceiptPage } from '@/features/singleRight/delegate/ReceiptPage/ReceiptPage';
 import { UserRightsPage } from '@/features/amUI/userRightsPage/UserRightsPage';
 import { UsersPage } from '@/features/amUI/users/UsersPage';
-import { ReporteesPage } from '@/features/amUI/reportees/reporteesPage';
+import { ReporteesPage } from '@/features/amUI/reportees/ReporteesPage';
 
 import { GeneralPath, SingleRightPath, ApiDelegationPath, amUIPath } from '../paths';
 
