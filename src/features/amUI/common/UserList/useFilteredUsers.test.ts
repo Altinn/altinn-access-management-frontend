eef06058c6367c172c7aaf88408b3215c37fb503
Sondre Wittek <sondrewittek@gmail.com>
2025-02-12 17:12:55 +0100

    improve search cases, and validate pagination logic

--- src/features/amUI/common/UserList/useFilteredUsers.test.ts
+++ src/features/amUI/common/UserList/useFilteredUsers.test.ts
@@ -22,6 +22,22 @@ const mockUsers: User[] = [
     registryRoles: [],
   },
   {
+    partyUuid: '2.5',
+    name: 'Ipsum AS',
+    organizationNumber: '456',
+    inheritingUsers: [
+      {
+        partyUuid: '1',
+        name: 'InheritBob',
+        inheritingUsers: [],
+        partyType: PartyType.Person,
+        registryRoles: [],
+      },
+    ],
+    partyType: PartyType.Organization,
+    registryRoles: [],
+  },
+  {
     partyUuid: '3',
     name: 'Lorem AS',
     organizationNumber: '789',
@@ -29,7 +45,6 @@ const mockUsers: User[] = [
       {
         partyUuid: '1',
         name: 'InheritAlice',
-        organizationNumber: '1234',
         inheritingUsers: [],
         partyType: PartyType.Person,
         registryRoles: [],
@@ -54,22 +69,58 @@ describe('useFilteredUsers', () => {
   it('should return all users when search string is empty', () => {
     const { result } = renderHook(() => useFilteredUsers({ users: mockUsers, searchString: '' }));
 
-    expect(result.current.users).toHaveLength(3);
+    expect(result.current.users).toHaveLength(4);
+  });
+
+  it('should handle organization number search correctly', () => {
+    const { result } = renderHook(() =>
+      useFilteredUsers({ users: mockUsers, searchString: '789' }),
+    );
+
+    expect(result.current.users).toHaveLength(1);
+    expect(result.current.users[0].name).toBe('Lorem AS');
+  });
+
+  it('should return hasNextPage: false when list contains less than 10 users', () => {
+    const { result } = renderHook(() => useFilteredUsers({ users: mockUsers, searchString: '' }));
+    expect(result.current.hasNextPage).toBe(false);
+  });
+
+  it('should return hasNextPage: true when list contains more than 10 users', () => {
+    const users = [...mockUsers, ...mockUsers, ...mockUsers];
+    const { result } = renderHook(() =>
+      useFilteredUsers({
+        users,
+        searchString: '',
+      }),
+    );
+    expect(result.current.hasNextPage).toBe(true);
+  });
+
+  it('should return all more users if goNextPage is called', () => {
+    const users = [...mockUsers, ...mockUsers, ...mockUsers];
+    const { result } = renderHook(() =>
+      useFilteredUsers({
+        users,
+        searchString: '',
+      }),
+    );
+    expect(result.current.hasNextPage).toBe(true);
+    result.current.goNextPage();
+    expect(result.current.hasNextPage).toBe(false);
+    expect(result.current.users).toHaveLength(12);
   });
 
   it('should return no users when search string does not match any user', () => {
     const { result } = renderHook(() =>
       useFilteredUsers({ users: mockUsers, searchString: 'NonExistent' }),
     );
-
     expect(result.current.users).toHaveLength(0);
   });
 
   it('should handle inheriting users correctly', () => {
-    const usersWithInheriting = [mockUsers[2]];
-
     const { result } = renderHook(() =>
-      useFilteredUsers({ users: usersWithInheriting, searchString: 'InheritAlice' }),
+      useFilteredUsers({ users: mockUsers, searchString: 'InheritAlice' }),
     );
 
     expect(result.current.users).toHaveLength(1);
