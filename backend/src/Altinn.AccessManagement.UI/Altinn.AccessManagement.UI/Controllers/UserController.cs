bd102af5c04e194db708efe88494d4899582e149
Sondre Wittek <sondrewittek@gmail.com>
2025-02-14 11:30:10 +0100

    Update backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/UserController.cs
    Co-authored-by: Alexandra Vedeler <arnex04@gmail.com>

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/UserController.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/UserController.cs
@@ -198,7 +198,7 @@ namespace Altinn.AccessManagement.UI.Controllers
         /// </summary>
         /// <param name="from">The uuid for the user which the right holder has access to</param>
         /// <param name="to">The uuid for the user whose accesses are to be returned</param>
-        /// <returns>All right holder's accesses</returns>
+        /// <returns>All accesses a user (to) has on behalf of a party (from)</returns>
         [HttpGet]
         [Authorize]
         [Route("from/{from}/to/{to}/accesses")]
