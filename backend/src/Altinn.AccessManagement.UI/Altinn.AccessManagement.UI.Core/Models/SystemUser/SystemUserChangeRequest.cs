51c3449840ab0cdf564222b4ce8fc3351340f3a9
Martin Gunnerud <mgunnerud@gmail.com>
2025-02-19 14:30:04 +0100

    use new accesspackage list item after change in backend (#1280)

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Models/SystemUser/SystemUserChangeRequest.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Models/SystemUser/SystemUserChangeRequest.cs
@@ -59,14 +59,14 @@ namespace Altinn.AccessManagement.UI.Core.Models.SystemUser
         /// </summary>
         [Required]
         [JsonPropertyName("requiredAccessPackages")]
-        public List<IdValuePair> RequiredAccessPackages { get; set; }
+        public List<RegisteredSystemAccessPackage> RequiredAccessPackages { get; set; }
 
         /// <summary>
         /// The set of Access Packages to be ensured are not delegeted to this system user.
         /// </summary>
         [Required]
         [JsonPropertyName("unwantedAccessPackages")]
-        public List<IdValuePair> UnwantedAccessPackages { get; set; }
+        public List<RegisteredSystemAccessPackage> UnwantedAccessPackages { get; set; }
 
         /// <summary>
         /// Initially the request is "new", 
