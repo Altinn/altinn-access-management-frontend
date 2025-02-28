8459bef548876493b749688973218009a70e0292
Sondre Wittek <sondrewittek@gmail.com>
2025-02-28 13:56:05 +0100

    fix: update AccessPackageInfoModal to use reportee data and right holder UUID

--- src/features/amUI/userRightsPage/AccessPackageSection/AccessPackageInfoModal.tsx
+++ src/features/amUI/userRightsPage/AccessPackageSection/AccessPackageInfoModal.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import type { AccessPackage } from '@/rtk/features/accessPackageApi';
 import type { Party } from '@/rtk/features/lookupApi';
-import { useGetUserInfoQuery } from '@/rtk/features/userInfoApi';
+import { useGetReporteeQuery, useGetUserInfoQuery } from '@/rtk/features/userInfoApi';
 
 import { EditModal } from '../DelegationModal/EditModal';
 import { ActionError } from '@/resources/hooks/useActionError';
@@ -23,11 +24,23 @@ export const AccessPackageInfoModal = ({
   onClose,
   openWithError,
 }: AccessPackageInfoModalProps) => {
+  useEffect(() => {
+    const handleClose = () => onClose?.();
+    modalRef.current?.addEventListener('close', handleClose);
+    return () => modalRef.current?.removeEventListener('close', handleClose);
+  }, [onClose, modalRef]);
+
+  const { data: reportee } = useGetReporteeQuery();
+  const { id: rightHolderUuid } = useParams();
+
+  const { data: currentUser } = useGetUserInfoQuery();
+  const isCurrentUser = currentUser?.uuid === toParty.partyUuid;
+
   return (
     <EditModal
       ref={modalRef}
-      toPartyUuid={toParty.partyUuid}
-      fromPartyUuid={currentUser?.uuid || ''}
+      toPartyUuid={rightHolderUuid ?? ''}
+      fromPartyUuid={reportee?.partyUuid ?? ''}
       accessPackage={modalItem}
       availableActions={[
         !isCurrentUser ? DelegationAction.DELEGATE : DelegationAction.REQUEST,
