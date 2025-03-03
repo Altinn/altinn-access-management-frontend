154d32c0142d555a0ba77f5eff5e3d820971b81a
Martin Gunnerud <mgunnerud@gmail.com>
2025-03-03 14:54:29 +0100

    fix

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/ClientInterfaces/ISystemUserClientRequestClient.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/ClientInterfaces/ISystemUserClientRequestClient.cs
@@ -12,10 +12,10 @@ namespace Altinn.AccessManagement.UI.Core.ClientInterfaces
         /// Get a system user client delegation request
         /// </summary>
         /// <param name="partyId">Used to identify the party the system user request is for.</param>
-        /// <param name="changeRequestId">The id of the system user request</param>
+        /// <param name="clientRequestId">The id of the system user request</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>The system user request</returns>
-        Task<Result<SystemUserClientRequest>> GetSystemUserClientRequest(int partyId, Guid changeRequestId, CancellationToken cancellationToken);
+        Task<Result<SystemUserClientRequest>> GetSystemUserClientRequest(int partyId, Guid clientRequestId, CancellationToken cancellationToken);
         
         /// <summary>
         /// Approve a system user client delegation request to create a new system user
