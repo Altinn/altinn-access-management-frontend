443a2f7795901664c6a28430a2d293d8e20f424d
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-03-13 10:00:00 +0100

    verify new url

--- playwright/e2eTests/systemuser/approveSystemUserChangeRequest.spec.ts
+++ playwright/e2eTests/systemuser/approveSystemUserChangeRequest.spec.ts
@@ -27,8 +27,7 @@ test.describe('Godkjenn og avvis Systembruker endringsforespørsel', () => {
 
     const confirmUrlChangeRequest = await api.postSystemuserChangeRequest(externalRef);
     const newUrl = Util.useAccessManagementUrlChangeRequest(confirmUrlChangeRequest);
-
-    await page.goto(newUrl);
+    console.log(newUrl);
     await page.getByRole('button', { name: 'Avvis' }).click();
 
     //Expect user to be logged out
