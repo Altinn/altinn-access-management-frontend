12221835da34d177e3cb472b1fe3d301fae8f99b
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-03-13 09:42:34 +0100

    new urls to be approved

--- playwright/util/Util.tsx
+++ playwright/util/Util.tsx
@@ -1,16 +1,19 @@
 export class Util {
+  private static swapDomain(oldUrl: string): string {
+    return oldUrl.replace(/authn\.ui\.(.*?)\.altinn\.(no|cloud)/, 'am.ui.$1.altinn.$2');
+  }
+
   public static useAccessManagementUrlChangeRequest(oldUrl: string): string {
-    return oldUrl
-      .replace('authn.ui.tt02', 'am.ui.tt02')
-      .replace(
-        '/authfront/ui/auth/vendorchangerequest',
-        '/accessmanagement/ui/systemuser/changerequest',
-      );
+    return this.swapDomain(oldUrl).replace(
+      '/authfront/ui/auth/vendorchangerequest',
+      '/accessmanagement/ui/systemuser/changerequest',
+    );
   }
 
   public static useAccessManagementUrlSystemUserRequest(oldUrl: string): string {
-    return oldUrl
-      .replace('authn.ui.tt02', 'am.ui.tt02')
-      .replace('/authfront/ui/auth/vendorrequest', '/accessmanagement/ui/systemuser/request');
+    return this.swapDomain(oldUrl).replace(
+      '/authfront/ui/auth/vendorrequest',
+      '/accessmanagement/ui/systemuser/request',
+    );
   }
 }
