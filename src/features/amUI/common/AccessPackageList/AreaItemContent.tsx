e779d84ccb3145901bd999e00d0e515a94be9abf
Sondre Wittek <sondrewittek@gmail.com>
2025-03-13 08:32:54 +0100

    Refactor delegation check logic to improve performance and state management

--- src/features/amUI/common/AccessPackageList/AreaItemContent.tsx
+++ src/features/amUI/common/AccessPackageList/AreaItemContent.tsx
@@ -2,6 +2,7 @@ import { Paragraph } from '@digdir/designsystemet-react';
 import { Button, ListBase } from '@altinn/altinn-components';
 import { MinusCircleIcon, PlusCircleIcon } from '@navikt/aksel-icons';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useMemo } from 'react';
 
 import { useDelegationCheckMutation, type AccessPackage } from '@/rtk/features/accessPackageApi';
 
@@ -34,96 +35,94 @@ export const AreaItemContent = ({
   const { packages } = area;
   const { t } = useTranslation();
 
-  const { canDelegatePackage: canDelegate } = useDelegationCheck(
-    packages.available.map((pkg) => pkg.id),
+  const availablePackageIds = useMemo(
+    () => packages.available.map((pkg) => pkg.id),
+    [packages.available],
   );
+
+  const canDelegate = useDelegationCheck(availablePackageIds);
 
   return (
     <div className={classes.accessAreaContent}>
       <Paragraph>{area.description}</Paragraph>
       {packages.assigned.length > 0 && (
         <ListBase>
-          {packages.assigned.map((pkg) => {
-            return (
-              <PackageItem
-                key={pkg.id}
-                pkg={pkg}
-                onSelect={onSelect}
-                hasAccess
-                controls={
-                  availableActions?.includes(DelegationAction.REVOKE) && useDeleteConfirm ? (
-                    <ButtonWithConfirmPopup
-                      triggerButtonContent={t('common.delete_poa')}
-                      triggerButtonProps={{
-                        icon: MinusCircleIcon,
-                        variant: 'text',
-                        size: 'sm',
-                        disabled: pkg.inherited,
-                      }}
-                      popoverProps={{
-                        color: 'neutral',
-                      }}
-                      message={t('user_rights_page.delete_confirm_message', {
-                        name: pkg.name,
-                      })}
-                      data-size='sm'
-                      onConfirm={() => onRevoke(pkg)}
-                    />
-                  ) : (
-                    <Button
-                      icon={MinusCircleIcon}
-                      variant='text'
-                      size='sm'
-                      disabled={pkg.inherited}
-                      onClick={() => onRevoke(pkg)}
-                    >
-                      {t('common.delete_poa')}
-                    </Button>
-                  )
-                }
-              />
-            );
-          })}
+          {packages.assigned.map((pkg) => (
+            <PackageItem
+              key={pkg.id}
+              pkg={pkg}
+              onSelect={onSelect}
+              hasAccess
+              controls={
+                availableActions?.includes(DelegationAction.REVOKE) && useDeleteConfirm ? (
+                  <ButtonWithConfirmPopup
+                    triggerButtonContent={t('common.delete_poa')}
+                    triggerButtonProps={{
+                      icon: MinusCircleIcon,
+                      variant: 'text',
+                      size: 'sm',
+                      disabled: pkg.inherited,
+                    }}
+                    popoverProps={{
+                      color: 'neutral',
+                    }}
+                    message={t('user_rights_page.delete_confirm_message', {
+                      name: pkg.name,
+                    })}
+                    data-size='sm'
+                    onConfirm={() => onRevoke(pkg)}
+                  />
+                ) : (
+                  <Button
+                    icon={MinusCircleIcon}
+                    variant='text'
+                    size='sm'
+                    disabled={pkg.inherited}
+                    onClick={() => onRevoke(pkg)}
+                  >
+                    {t('common.delete_poa')}
+                  </Button>
+                )
+              }
+            />
+          ))}
         </ListBase>
       )}
       {packages.available.length > 0 && (
         <ListBase>
-          {packages.available.map((pkg) => {
-            const delegationCheck = canDelegate(pkg.id);
-            return (
-              <PackageItem
-                key={pkg.id}
-                pkg={pkg}
-                onSelect={onSelect}
-                controls={
-                  <>
-                    {availableActions?.includes(DelegationAction.DELEGATE) && (
-                      <Button
-                        icon={PlusCircleIcon}
-                        variant='text'
-                        size='sm'
-                        disabled={delegationCheck}
-                        onClick={() => onDelegate(pkg)}
-                      >
-                        {t('common.give_poa')}
-                      </Button>
-                    )}
-                    {availableActions?.includes(DelegationAction.REQUEST) && (
-                      <Button
-                        icon={PlusCircleIcon}
-                        variant='text'
-                        size='sm'
-                        disabled
-                        onClick={() => onRequest(pkg)}
-                      >
-                        {t('common.request_poa')}
-                      </Button>
-                    )}
-                  </>
-                }
-              />
-            );
-          })}
+          {packages.available.map((pkg) => (
+            <PackageItem
+              key={pkg.id}
+              pkg={pkg}
+              onSelect={onSelect}
+              controls={
+                <>
+                  {availableActions?.includes(DelegationAction.DELEGATE) && (
+                    <Button
+                      icon={PlusCircleIcon}
+                      variant='text'
+                      size='sm'
+                      disabled={canDelegate(pkg.id)}
+                      onClick={() => onDelegate(pkg)}
+                    >
+                      {t('common.give_poa')}
+                    </Button>
+                  )}
+                  {availableActions?.includes(DelegationAction.REQUEST) && (
+                    <Button
+                      icon={PlusCircleIcon}
+                      variant='text'
+                      size='sm'
+                      disabled
+                      onClick={() => onRequest(pkg)}
+                    >
+                      {t('common.request_poa')}
+                    </Button>
+                  )}
+                </>
+              }
+            />
+          ))}
         </ListBase>
       )}
     </div>
@@ -131,14 +130,16 @@ export const AreaItemContent = ({
 };
 
 const useDelegationCheck = (accessPackageIds: string[]) => {
-  const [delegationCheck, { isLoading, data: delegationChecks }] = useDelegationCheckMutation();
+  const [delegationCheck, { isLoading, data }] = useDelegationCheckMutation();
 
-  const canDelegatePackage = (packageId: string) => {
-    const check = delegationChecks?.find((c) => c.packageId === packageId);
-    return isLoading || !!check?.canDelegate;
-  };
+  useEffect(() => {
+    if (accessPackageIds.length > 0) {
+      delegationCheck({ packageIds: accessPackageIds });
+    }
+  }, [accessPackageIds]);
 
-  delegationCheck({ packageIds: accessPackageIds });
+  const canDelegate = (id: string) =>
+    !isLoading && data?.find((d) => d.packageId === id)?.canDelegate;
 
-  return { canDelegatePackage, isLoading };
+  return canDelegate;
 };
