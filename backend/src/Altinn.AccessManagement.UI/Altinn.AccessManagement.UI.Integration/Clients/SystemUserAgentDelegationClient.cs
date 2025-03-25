994eee05949dae7f7dc808052877a8ac9844b778
Martin Gunnerud <mgunnerud@gmail.com>
2025-03-25 08:15:15 +0100

    fix delete delegation url

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/SystemUserAgentDelegationClient.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/SystemUserAgentDelegationClient.cs
@@ -106,7 +106,7 @@ namespace Altinn.AccessManagement.UI.Integration.Clients
             try
             {
                 string token = JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext, _platformSettings.JwtCookieName);
-                string endpointUrl = $"systemuser/agent/{partyId}/{facilitatorId}/{delegationId}";
+                string endpointUrl = $"systemuser/agent/{partyId}/delegation/{delegationId}?facilitatorId={facilitatorId}";
 
                 HttpResponseMessage response = await _client.DeleteAsync(token, endpointUrl);
                 string responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
