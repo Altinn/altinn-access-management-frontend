505ab78360cdb68939e2b510aa008fe5c28304a0
Alexandra Vedeler <arnex04@gmail.com>
2025-04-22 12:07:20 +0200

    Add new org with backend support (#1412)
    * new bff endpoint
    
    * add user org is working!
    
    * auto lint
    
    * added tests
    
    * fixes
    
    ---------
    
    Co-authored-by: Vedeler <acn-avede@ai-dev.no>

---
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/ClientInterfaces/IRightHolderClient.cs
@@ -0,0 +1,16 @@
+﻿namespace Altinn.AccessManagement.UI.Core.ClientInterfaces
+{
+    /// <summary>
+    /// Interface for client to integrate with access package metadata
+    /// </summary>
+    public interface IRightHolderClient
+    {
+        /// <summary>
+        /// Creates an assignment between the authenticated user's selected party and the specified target party.
+        /// </summary>
+        /// <param name="party">The GUID identifying the party the authenticated user is acting on behalf of.</param>
+        /// <param name="to">The GUID identifying the target party to which the assignment should be created.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
+        Task<HttpResponseMessage> PostNewRightHolder(Guid party, Guid to, CancellationToken cancellationToken = default);
+    }
+}
---
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Mocks/Mocks/RightHolderClientMock.cs
@@ -0,0 +1,41 @@
+﻿using Altinn.AccessManagement.UI.Core.ClientInterfaces;
+using Altinn.AccessManagement.UI.Core.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+
+namespace Altinn.AccessManagement.UI.Mocks.Mocks
+{
+    /// <summary>
+    ///     Mock class for <see cref="IRightHolderClient"></see> interface
+    /// </summary>
+    public class RightHolderClientMock : IRightHolderClient
+    {
+        private static readonly JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        private readonly string dataFolder;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AccessManagementClientMock" /> class
+        /// </summary>
+        public RightHolderClientMock(
+            HttpClient httpClient,
+            ILogger<AccessManagementClientMock> logger,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            dataFolder = Path.Combine(Path.GetDirectoryName(new Uri(typeof(AccessManagementClientMock).Assembly.Location).LocalPath), "Data");
+        }
+
+        /// <inheritdoc />
+        public Task<HttpResponseMessage> PostNewRightHolder(Guid party, Guid to, CancellationToken cancellationToken = default)
+        {
+            if (party == Guid.Empty)
+            {
+                throw new HttpStatusException("Test", "Testing unexpected status from backend", HttpStatusCode.BadRequest, null);
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            return Task.FromResult(response);
+        }
+    }
+}
