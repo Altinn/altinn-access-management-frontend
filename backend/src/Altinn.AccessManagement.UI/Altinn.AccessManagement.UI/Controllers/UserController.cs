fd276d9357c1f8910bc090e5889c41b3ca960ea6
Alexandra Vedeler <arnex04@gmail.com>
2025-04-24 15:04:22 +0200

    Update backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/UserController.cs
    Co-authored-by: Sondre Wittek <sondrewittek@gmail.com>

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/UserController.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI/Controllers/UserController.cs
@@ -157,7 +157,7 @@ namespace Altinn.AccessManagement.UI.Controllers
         /// <summary>
         ///     Endpoint for revoking all rights associated with a right holder by revoking their status as a right holder for another party.
         /// </summary>
-        /// <param name="partyUuid">The uuid pf the reportee party, from which the right holder is have their rights revoked</param>
+        /// <param name="partyUuid">The uuid of the reportee party, from which the right holder is to have their rights revoked</param>
         /// <param name="rightholderPartyUuid">The uuid of the party that is to lose their right holder status</param>
         /// <response code="400">Bad Request</response>
         /// <response code="500">Internal Server Error</response>
