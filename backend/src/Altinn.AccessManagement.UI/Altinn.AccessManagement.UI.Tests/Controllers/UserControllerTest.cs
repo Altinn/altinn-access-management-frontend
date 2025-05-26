bdec6a12d693f9167d5d4ad3676271098199c680
Sondre Wittek <sondrewittek@gmail.com>
2025-05-26 11:53:43 +0200

    fix name of test

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Tests/Controllers/UserControllerTest.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Tests/Controllers/UserControllerTest.cs
@@ -793,7 +793,7 @@ namespace Altinn.AccessManagement.UI.Tests.Controllers
         [InlineData(null)]
         [InlineData("")]
         [InlineData("not-a-guid")]
-        public async Task MethodName_InvalidInputs_ReturnsBadRequest(string invalid_party)
+        public async Task CheckAccess_InvalidInputs_ReturnsBadRequest(string invalid_party)
         {
             // Arrange
             var token = PrincipalUtil.GetToken(1234, 1234, 2);
