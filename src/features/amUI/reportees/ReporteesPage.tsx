90ac7d82e50deaad57e1781ddd4b14bac11e7958
Sondre Wittek <sondrewittek@gmail.com>
2025-05-30 10:03:19 +0200

    Feat/am UI/729 feilmelding dersom aktør er privatperson (#1454)
    * add check and feature-toggle to prevent PRIV users access to  amui
    
    * refactor and and improve
    
    * fix bug
    
    * move up to prevent not needed data-fetching
    
    * fix typos
    
    * flip feature-toggle

--- src/features/amUI/reportees/ReporteesPage.tsx
+++ src/features/amUI/reportees/ReporteesPage.tsx
@@ -1,40 +1,43 @@
 import { DsHeading } from '@altinn/altinn-components';
 import { useTranslation } from 'react-i18next';
 
+import { PageLayoutWrapper } from '../common/PageLayoutWrapper';
+import { PartyRepresentationProvider } from '../common/PartyRepresentationContext/PartyRepresentationContext';
+import { AlertIfNotAvailableForUserType } from '../common/alertIfNotAvailableForUserType/AlertIfNotAvailableForUserType';
+
+import classes from './ReporteePage.module.css';
+import { ReporteesList } from './ReporteesList';
+
 import { useDocumentTitle } from '@/resources/hooks/useDocumentTitle';
 import { PageWrapper } from '@/components';
 import { getCookie } from '@/resources/Cookie/CookieMethods';
 import { useGetReporteePartyQuery } from '@/rtk/features/lookupApi';
 
-import { PageLayoutWrapper } from '../common/PageLayoutWrapper';
-import { PartyRepresentationProvider } from '../common/PartyRepresentationContext/PartyRepresentationContext';
-
-import classes from './ReporteePage.module.css';
-import { ReporteesList } from './ReporteesList';
-
 export const ReporteesPage = () => {
   const { t } = useTranslation();
+  const { data: party } = useGetReporteePartyQuery();
 
   useDocumentTitle(t('reportees_page.page_title'));
-  const { data: party } = useGetReporteePartyQuery();
 
   return (
     <PageWrapper>
       <PageLayoutWrapper>
-        <PartyRepresentationProvider
-          fromPartyUuid={getCookie('AltinnPartyUuid')}
-          actingPartyUuid={getCookie('AltinnPartyUuid')}
-        >
-          <div className={classes.reporteeListHeading}>
-            <DsHeading
-              level={1}
-              data-size='md'
-            >
-              {t('reportees_page.main_page_heading', { name: party?.name || '' })}
-            </DsHeading>
-          </div>
-          <ReporteesList />
-        </PartyRepresentationProvider>
+        <AlertIfNotAvailableForUserType>
+          <PartyRepresentationProvider
+            fromPartyUuid={getCookie('AltinnPartyUuid')}
+            actingPartyUuid={getCookie('AltinnPartyUuid')}
+          >
+            <div className={classes.reporteeListHeading}>
+              <DsHeading
+                level={1}
+                data-size='md'
+              >
+                {t('reportees_page.main_page_heading', { name: party?.name || '' })}
+              </DsHeading>
+            </div>
+            <ReporteesList />
+          </PartyRepresentationProvider>
+        </AlertIfNotAvailableForUserType>
       </PageLayoutWrapper>
     </PageWrapper>
   );
--- src/features/amUI/userRightsPage/UserRightsPage.tsx
+++ src/features/amUI/userRightsPage/UserRightsPage.tsx
@@ -2,23 +2,24 @@ import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router';
 
-import { useDocumentTitle } from '@/resources/hooks/useDocumentTitle';
-import { PageWrapper } from '@/components';
-import { amUIPath } from '@/routes/paths';
-import { getCookie } from '@/resources/Cookie/CookieMethods';
-import { rerouteIfNotConfetti } from '@/resources/utils/featureFlagUtils';
-
 import { PageContainer } from '../common/PageContainer/PageContainer';
 import { PageLayoutWrapper } from '../common/PageLayoutWrapper';
 import { UserPageHeader } from '../common/UserPageHeader/UserPageHeader';
 import { RightsTabs } from '../common/RightsTabs/RightsTabs';
 import { DelegationModalProvider } from '../common/DelegationModal/DelegationModalContext';
 import { PartyRepresentationProvider } from '../common/PartyRepresentationContext/PartyRepresentationContext';
+import { AlertIfNotAvailableForUserType } from '../common/alertIfNotAvailableForUserType/AlertIfNotAvailableForUserType';
 
 import { AccessPackageSection } from './AccessPackageSection/AccessPackageSection';
 import { SingleRightsSection } from './SingleRightsSection/SingleRightsSection';
 import { RoleSection } from './RoleSection/RoleSection';
 import { DeleteUserModal } from './DeleteUserModal';
+
+import { rerouteIfNotConfetti } from '@/resources/utils/featureFlagUtils';
+import { getCookie } from '@/resources/Cookie/CookieMethods';
+import { amUIPath } from '@/routes/paths';
+import { PageWrapper } from '@/components';
+import { useDocumentTitle } from '@/resources/hooks/useDocumentTitle';
 
 export const UserRightsPage = () => {
   const { t } = useTranslation();
@@ -32,30 +33,32 @@ export const UserRightsPage = () => {
 
   return (
     <PageWrapper>
-      <PartyRepresentationProvider
-        actingPartyUuid={getCookie('AltinnPartyUuid')}
-        fromPartyUuid={getCookie('AltinnPartyUuid')}
-        toPartyUuid={id ?? undefined}
-      >
-        <DelegationModalProvider>
-          <PageLayoutWrapper>
-            <PageContainer
-              backUrl={`/${amUIPath.Users}`}
-              contentActions={<DeleteUserModal direction='to' />}
-            >
-              <UserPageHeader
-                direction='to'
-                displayRoles={!displayLimitedPreviewLaunch}
-              />
-              <RightsTabs
-                packagesPanel={<AccessPackageSection numberOfAccesses={0} />}
-                singleRightsPanel={<SingleRightsSection />}
-                roleAssignmentsPanel={<RoleSection numberOfAccesses={0} />}
-              />
-            </PageContainer>
-          </PageLayoutWrapper>
-        </DelegationModalProvider>
-      </PartyRepresentationProvider>
+      <AlertIfNotAvailableForUserType>
+        <PartyRepresentationProvider
+          actingPartyUuid={getCookie('AltinnPartyUuid')}
+          fromPartyUuid={getCookie('AltinnPartyUuid')}
+          toPartyUuid={id ?? undefined}
+        >
+          <DelegationModalProvider>
+            <PageLayoutWrapper>
+              <PageContainer
+                backUrl={`/${amUIPath.Users}`}
+                contentActions={<DeleteUserModal direction='to' />}
+              >
+                <UserPageHeader
+                  direction='to'
+                  displayRoles={!displayLimitedPreviewLaunch}
+                />
+                <RightsTabs
+                  packagesPanel={<AccessPackageSection numberOfAccesses={0} />}
+                  singleRightsPanel={<SingleRightsSection />}
+                  roleAssignmentsPanel={<RoleSection numberOfAccesses={0} />}
+                />
+              </PageContainer>
+            </PageLayoutWrapper>
+          </DelegationModalProvider>
+        </PartyRepresentationProvider>
+      </AlertIfNotAvailableForUserType>
     </PageWrapper>
   );
 };
