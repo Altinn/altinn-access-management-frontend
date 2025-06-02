8cf1d1fa24dbeb4713e6c09a0191afa6b6498664
Martin Gunnerud <mgunnerud@gmail.com>
2025-06-02 14:17:32 +0200

    add comments

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Models/Consent/ConsentRequestStatusType.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Models/Consent/ConsentRequestStatusType.cs
@@ -9,18 +9,33 @@ namespace Altinn.AccessManagement.UI.Core.Models.Consent
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ConsentRequestStatusType
     {
+        /// <summary>
+        /// Consent request created
+        /// </summary>
         [EnumMember(Value = "created")]
         Created = 0,
 
+        /// <summary>
+        /// Consent request rejected
+        /// </summary>
         [EnumMember(Value = "rejected")]
         Rejected = 1,
 
+        /// <summary>
+        /// Consent request approved
+        /// </summary>
         [EnumMember(Value = "accepted")]
         Accepted = 2,
 
+        /// <summary>
+        /// Consent request revoked
+        /// </summary>
         [EnumMember(Value = "revoked")]
         Revoked = 3,
 
+        /// <summary>
+        /// Consent request deleted
+        /// </summary>
         [EnumMember(Value = "deleted")]
         Deleted = 4
     }
