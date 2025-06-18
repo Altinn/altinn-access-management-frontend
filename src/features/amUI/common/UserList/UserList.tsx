a5db947ff46c39f904b13eb865648208e06b5012
Alexandra Vedeler <arnex04@gmail.com>
2025-06-18 12:36:33 +0200

    do not prompt for adding new users on reportee page (#1486)
    Co-authored-by: Vedeler <acn-avede@ai-dev.no>

--- src/features/amUI/common/UserList/UserList.tsx
+++ src/features/amUI/common/UserList/UserList.tsx
@@ -1,12 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import { Button, DsParagraph } from '@altinn/altinn-components';
 
+import type { User } from '@/rtk/features/userInfoApi';
+
+import NewUserModal, { NewUserButton } from '../../users/NewUserModal/NewUserModal';
+
 import { ListWrapper } from './ListWrapper';
 import { useFilteredUsers } from './useFilteredUsers';
 import classes from './UserList.module.css';
-
-import type { User } from '@/rtk/features/userInfoApi';
-import NewUserModal, { NewUserButton } from '../../users/NewUserModal/NewUserModal';
 
 export interface UserListProps {
   userList?: User[];
@@ -14,6 +15,7 @@ export interface UserListProps {
   isLoading?: boolean;
   listItemTitleAs?: 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   interactive?: boolean;
+  canAdd?: boolean;
 }
 
 export const UserList = ({
@@ -22,6 +24,7 @@ export const UserList = ({
   isLoading,
   listItemTitleAs,
   interactive,
+  canAdd = true,
 }: UserListProps) => {
   const { t } = useTranslation();
   const { users, hasNextPage, goNextPage } = useFilteredUsers({
@@ -29,7 +32,7 @@ export const UserList = ({
     searchString,
   });
 
-  const promptForNoResults = !isLoading && users?.length === 0;
+  const promptForNoResults = !isLoading && users?.length === 0 && canAdd;
 
   return (
     <>
