44c00282eb2cb4b962a1a8498cea41f6bf5acfee
Alexandra Vedeler <arnex04@gmail.com>
2025-06-23 13:39:13 +0200

    fixed so number of packages is displayed on reportee page as well (#1495)
    Co-authored-by: Vedeler <acn-avede@ai-dev.no>

--- src/features/amUI/reporteeRightsPage/ReporteeAccessPackageSection.tsx
+++ src/features/amUI/reporteeRightsPage/ReporteeAccessPackageSection.tsx
@@ -2,31 +2,39 @@ import { useTranslation } from 'react-i18next';
 import { useEffect, useRef, useState } from 'react';
 import { DsHeading } from '@altinn/altinn-components';
 
-import type { AccessPackage } from '@/rtk/features/accessPackageApi';
+import { useGetUserDelegationsQuery, type AccessPackage } from '@/rtk/features/accessPackageApi';
 
 import { AccessPackageList } from '../common/AccessPackageList/AccessPackageList';
 import { DelegationAction } from '../common/DelegationModal/EditModal';
 import { AccessPackageInfoModal } from '../userRightsPage/AccessPackageSection/AccessPackageInfoModal';
 import { useDelegationModalContext } from '../common/DelegationModal/DelegationModalContext';
 import { AccessPackageInfoAlert } from '../userRightsPage/AccessPackageSection/AccessPackageInfoAlert';
+import { usePartyRepresentation } from '../common/PartyRepresentationContext/PartyRepresentationContext';
 
-interface ReporteeAccessPackageSectionProps {
-  numberOfAccesses?: number;
-}
-
-export const ReporteeAccessPackageSection = ({
-  numberOfAccesses,
-}: ReporteeAccessPackageSectionProps) => {
+export const ReporteeAccessPackageSection = () => {
   const { t } = useTranslation();
   const modalRef = useRef<HTMLDialogElement>(null);
   const [modalItem, setModalItem] = useState<AccessPackage | undefined>(undefined);
   const { setActionError } = useDelegationModalContext();
+
+  const { toParty, fromParty, actingParty } = usePartyRepresentation();
 
   useEffect(() => {
     const handleClose = () => setModalItem(undefined);
     modalRef.current?.addEventListener('close', handleClose);
     return () => modalRef.current?.removeEventListener('close', handleClose);
   }, []);
+
+  const { data: accesses } = useGetUserDelegationsQuery(
+    {
+      from: fromParty?.partyUuid ?? '',
+      to: toParty?.partyUuid ?? '',
+      party: actingParty?.partyUuid ?? '',
+    },
+    { skip: !toParty?.partyUuid || !fromParty?.partyUuid || !actingParty?.partyUuid },
+  );
+
+  const numberOfAccesses = accesses ? Object.values(accesses).flat().length : 0;
 
   return (
     <>
