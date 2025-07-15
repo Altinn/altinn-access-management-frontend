10a290a930f5e4ff6d799adb838be09a5edfe962
Sondre Wittek <sondrewittek@gmail.com>
2025-07-15 11:17:28 +0200

    refactor: simplify AltinnCdnClient constructor by removing unused parameters

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/AltinnCdnClient.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/AltinnCdnClient.cs
@@ -1,11 +1,7 @@
 ﻿using System.Text.Json;
 using Altinn.AccessManagement.UI.Core.ClientInterfaces;
 using Altinn.AccessManagement.UI.Core.Models.Common;
-using Altinn.AccessManagement.UI.Core.Services.Interfaces;
-using Altinn.AccessManagement.UI.Integration.Configuration;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Altinn.AccessManagement.UI.Integration.Clients
 {
@@ -16,32 +12,18 @@ namespace Altinn.AccessManagement.UI.Integration.Clients
     {
         private readonly ILogger _logger;
         private readonly HttpClient _client;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly PlatformSettings _platformSettings;
-        private readonly IAccessTokenProvider _accessTokenProvider;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AltinnCdnClient"/> class
         /// </summary>
         /// <param name="httpClient">the handler for httpclient service</param>
         /// <param name="logger">the handler for logger service</param>
-        /// <param name="httpContextAccessor">the handler for httpcontextaccessor service</param>
-        /// <param name="platformSettings"> platform settings configuration</param>
-        /// <param name="accessTokenProvider">the handler for access token generator</param>
         public AltinnCdnClient(
             HttpClient httpClient,
-            ILogger<AltinnCdnClient> logger,
-            IHttpContextAccessor httpContextAccessor,
-            IOptions<PlatformSettings> platformSettings,
-            IAccessTokenProvider accessTokenProvider)
+            ILogger<AltinnCdnClient> logger)
         {
             _logger = logger;
-            _platformSettings = platformSettings.Value;
-            httpClient.BaseAddress = new Uri(_platformSettings.ApiProfileEndpoint);
-            httpClient.DefaultRequestHeaders.Add(_platformSettings.SubscriptionKeyHeaderName, _platformSettings.SubscriptionKey);
             _client = httpClient;
-            _httpContextAccessor = httpContextAccessor;
-            _accessTokenProvider = accessTokenProvider;
         }
 
         private readonly string altinnCdnUrl = "https://altinncdn.no/";
