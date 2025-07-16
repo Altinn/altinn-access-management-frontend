7fdcffaefc4ac162177a6f7e265e1aaf7147b45b
Sondre Wittek <sondrewittek@gmail.com>
2025-07-16 13:05:06 +0200

    Scripted upgrade for storybook v.9.x (#1528)
    * scripted update to  storybook@latest
    
    * fix package-list story and add feature-toggles to storybook config
    
    * fix: update storybook addon dependencies to use caret versioning
    
    * lock file

--- src/features/singleRight/components/ChooseServiceSkeleton/ChooseServiceSkeleton.stories.tsx
+++ src/features/singleRight/components/ChooseServiceSkeleton/ChooseServiceSkeleton.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { MugIcon } from '@navikt/aksel-icons';
 
 import { Page, PageContent, PageHeader } from '@/components';
