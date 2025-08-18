46957889ca9c35a8941d1ec57388b192f856f7e7
Sondre Wittek <sondrewittek@gmail.com>
2025-08-18 12:31:40 +0200

    update search handling in AccessPackagePermissions component to use debounced value

--- src/features/amUI/poaOverview/AccessPackagePermissions.tsx
+++ src/features/amUI/poaOverview/AccessPackagePermissions.tsx
@@ -10,34 +10,37 @@ export const AccessPackagePermissions = () => {
   const { t } = useTranslation();
 
   const [searchString, setSearchString] = useState('');
+  const [debouncedSearchString, setDebouncedSearchString] = useState(searchString ?? '');
 
   const debouncedSearch = useCallback(
     debounce((value: string) => {
-      setSearchString(value);
+      setDebouncedSearchString(value);
     }, 300),
     [],
   );
-
   return (
     <>
       <search className={classes.searchInput}>
         <DsSearch data-size='sm'>
           <DsSearch.Input
+            value={searchString}
             aria-label={t('access_packages.search_label')}
             placeholder={t('access_packages.search_label')}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setSearchString(event.target.value);
               debouncedSearch(event.target.value);
             }}
           />
           <DsSearch.Clear
             onClick={() => {
               setSearchString('');
+              debouncedSearch('');
             }}
           />
         </DsSearch>
       </search>
       <AccessPackageList
-        searchString={searchString}
+        searchString={debouncedSearchString}
         minimizeAvailablePackages={false}
         showAvailableToggle={false}
         showPermissions
