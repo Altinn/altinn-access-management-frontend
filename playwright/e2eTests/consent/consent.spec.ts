bfbaaaa865b0e4a5a2a7dfb51f5386e56e917a48
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-09-02 08:39:06 +0200

    added tests, now refactor

--- playwright/e2eTests/consent/consent.spec.ts
+++ playwright/e2eTests/consent/consent.spec.ts
@@ -67,7 +67,7 @@ test.describe('Consent', () => {
       page.getByText(new RegExp(`Samtykket er tidsavgrenset og vil gå ut ${expectedDateTime}`)),
     ).toBeVisible();
 
-    await ConsentSayYes(page);
+    await ApproveConsent(page);
   });
 
   test('Godta samtykke - krav-template', async ({ page }) => {
@@ -85,7 +85,22 @@ test.describe('Consent', () => {
     });
 
     await LoginUser({ page, consentResponse, fromPerson });
-    await ConsentSayYes(page);
+
+    await expect(page.getByRole('heading', { name: 'Godkjenne deling med banken' })).toBeVisible();
+    await expect(page.getByText('Playwright integrasjonstest')).toBeVisible();
+    await expect(
+      page.getByText('Skatteetaten har utviklet en løsning for å dele informasjon med banker '),
+    ).toBeVisible();
+    await expect(page.getByText('Brukerstyrt tilgang')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Altinn.no' })).toBeVisible();
+    await expect(page.getByText('Samtykket er tidsavgrenset og')).toBeVisible();
+
+    const expectedDateTime = formatUiDateTime(validToTimestamp);
+    await expect(
+      page.getByText(new RegExp(`Samtykket er tidsavgrenset og vil gå ut ${expectedDateTime}`)),
+    ).toBeVisible();
+
+    await ApproveConsent(page);
   });
 
   test('Godta samtykke - Fullmakt utføre tjeneste', async ({ page }) => {
@@ -103,6 +118,25 @@ test.describe('Consent', () => {
     });
 
     await LoginUser({ page, consentResponse, fromPerson });
+
+    await expect(
+      page.getByRole('heading', { name: 'Fullmakt til å handle på dine vegne' }),
+    ).toBeVisible();
+    await expect(page.getByText('Playwright integrasjonstest')).toBeVisible();
+    await expect(
+      page.getByText(/Ved at du gir fullmakt, får .* tilgang til følgende tjenester på dine vegne/),
+    ).toBeVisible();
+    await expect(page.getByText('Samtykke fullmakt utføre tjeneste')).toBeVisible();
+    await expect(
+      page.getByText('Du gir fullmakt til at en annen kan utføre denne tjenesten for deg'),
+    ).toBeVisible();
+    await expect(page.getByText('Tiltak: 2024')).toBeVisible();
+
+    const expectedDateTime = formatUiDateTime(validToTimestamp);
+    await expect(
+      page.getByText(new RegExp(`Fullmakten er tidsavgrenset og vil gå ut ${expectedDateTime}`)),
+    ).toBeVisible();
+
     await page.getByRole('button', samtykkeButtonFullmakt).click();
     await waitForRedirects(page, [/login\.test\.idporten\.no\/logout\/success/i, /example\.com/i]);
   });
@@ -122,7 +156,32 @@ test.describe('Consent', () => {
     });
 
     await LoginUser({ page, consentResponse, fromPerson });
-    await ConsentSayYes(page);
+
+    await expect(
+      page.getByRole('heading', { name: 'Samtykke til bruk av dine data' }),
+    ).toBeVisible();
+    await expect(page.getByText(/ønsker å hente opplysninger om deg/i)).toBeVisible();
+    await expect(page.getByText('Playwright integrasjonstest')).toBeVisible();
+    await expect(
+      page.getByText(/Ved at du samtykker, får .* tilgang til følgende opplysninger om deg/),
+    ).toBeVisible();
+
+    await expect(
+      page.getByText('Du samtykker til at søknadsdata kan brukes i forbindelse med låneprosessen'),
+    ).toBeVisible();
+    await expect(page.getByText('Rente: 4.2')).toBeVisible();
+    await expect(page.getByText('utloepsar: 2027')).toBeVisible();
+    await expect(page.getByText('Bank: Testbanken E2E')).toBeVisible();
+
+    await expect(
+      page.getByText('Samtykket gjelder én gangs utlevering av opplysningene'),
+    ).toBeVisible();
+
+    // Approve consent
+    await page.getByRole('button', samtykkeButtonStandard).click();
+
+    //Verify logout
+    await waitForRedirects(page, [/login\.test\.idporten\.no\/logout\/success/i, /example\.com/i]);
   });
 
   test('Approve consent for template: Enkelt samtykke', async ({ page }) => {
@@ -138,11 +197,34 @@ test.describe('Consent', () => {
       validToIsoUtc: validToTimestamp,
       resourceValue: 'enkelt-samtykke',
       redirectUrl,
-      metaData: { simpletag: 'E2E enkelt samtykke test' },
+      metaData: { simpletag: 'E2E Playwright metadata for simpletag' },
     });
 
     await LoginUser({ page, consentResponse, fromPerson });
-    await ConsentSayYes(page);
+
+    await expect(page.getByText(/.* ber om ditt samtykke/)).toBeVisible();
+    await expect(page.getByText('Playwright integrasjonstest')).toBeVisible();
+    await expect(
+      page.getByText('Ved å godkjenne denne forespørselen samtykker du til følgende:'),
+    ).toBeVisible();
+
+    await expect(page.getByText('Enkelt samtykke')).toBeVisible();
+    await expect(
+      page.getByText('Du samtykker til at dine data kan brukes i denne tjenesten'),
+    ).toBeVisible();
+    await expect(page.getByText('vi tar vare på dine data')).toBeVisible();
+    await expect(page.getByText('metadata: E2E Playwright metadata for simpletag')).toBeVisible();
+
+    const expectedDateTime = formatUiDateTime(validToTimestamp);
+    await expect(
+      page.getByText(new RegExp(`Samtykket er tidsavgrenset og vil gå ut ${expectedDateTime}`)),
+    ).toBeVisible();
+
+    // Approve consent
+    await page.getByRole('button', samtykkeButtonStandard).click();
+
+    //Verify logout
+    await waitForRedirects(page, [/login\.test\.idporten\.no\/logout\/success/i, /example\.com/i]);
   });
 
   test('Reject consent', async ({ page }) => {
@@ -161,7 +243,11 @@ test.describe('Consent', () => {
       metaData: { simpletag: 'E2E reject test' },
     });
 
-    await runConsentRejectionTest({ page, consentResponse, fromPerson });
+    await LoginUser({ page, consentResponse, fromPerson });
+
+    await expect(page.getByRole('heading', { name: 'Forespørsel om samtykke' })).toBeVisible();
+    await page.getByRole('button', { name: 'Nei, jeg gir ikke samtykke' }).click();
+    await waitForRedirects(page, [/login\.test\.idporten\.no\/logout\/success/i, /example\.com/i]);
   });
 
   async function LoginUser({
@@ -175,33 +261,15 @@ test.describe('Consent', () => {
   }) {
     loginPage = new LoginPage(page);
     expect(consentResponse.viewUri).toBeTruthy();
+
+    // https://github.com/Altinn/altinn-authorization-tmp/issues/1115
     consentUrl = consentResponse.viewUri + '&DONTCHOOSEREPORTEE=TRUE';
     await page.goto(consentUrl);
     await loginPage.loginAsActorPid(fromPerson);
   }
 
-  async function ConsentSayYes(page: Page) {
+  async function ApproveConsent(page: Page) {
     await page.getByRole('button', samtykkeButtonStandard).click();
-    await waitForRedirects(page, [/login\.test\.idporten\.no\/logout\/success/i, /example\.com/i]);
-  }
-
-  async function runConsentRejectionTest({
-    page,
-    consentResponse,
-    fromPerson,
-  }: {
-    page: Page;
-    consentResponse: { viewUri: string };
-    fromPerson: string;
-  }) {
-    loginPage = new LoginPage(page);
-    expect(consentResponse.viewUri).toBeTruthy();
-    consentUrl = consentResponse.viewUri + '&DONTCHOOSEREPORTEE=TRUE';
-    await page.goto(consentUrl);
-    await loginPage.loginAsActorPid(fromPerson);
-    const header = await page.getByRole('heading', { name: 'Forespørsel om samtykke' });
-    await expect(header).toBeVisible();
-    await page.getByRole('button', { name: 'Nei, jeg gir ikke samtykke' }).click();
     await waitForRedirects(page, [/login\.test\.idporten\.no\/logout\/success/i, /example\.com/i]);
   }
 });
