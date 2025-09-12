10da3f727cc90be63d9f09e61db3d134347ecc39
Sondre Wittek <sondrewittek@gmail.com>
2025-09-12 09:30:58 +0200

    fix(localization): update info alert text with correct link for access packages

--- src/features/amUI/userRightsPage/AccessPackageSection/AccessPackageInfoAlert.tsx
+++ src/features/amUI/userRightsPage/AccessPackageSection/AccessPackageInfoAlert.tsx
@@ -1,18 +1,7 @@
 import { DsAlert, DsParagraph } from '@altinn/altinn-components';
-import { Trans, useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
 
 export const AccessPackageInfoAlert = () => {
-  const { i18n } = useTranslation();
-
-  // Resolve locale-specific link target
-  const lang = i18n.resolvedLanguage || i18n.language;
-  const linkMap: Record<string, string> = {
-    en: 'https://info.altinn.no/en/help/profile/access-packages/',
-    no_nb: 'https://info.altinn.no/hjelp/profil/tilgangspakker/',
-    no_nn: 'https://info.altinn.no/nn/hjelp/profil/tilgangspakker/',
-  };
-  const href = linkMap[lang] ?? linkMap.no_nb;
-
   return (
     <DsAlert
       data-color='info'
@@ -24,7 +13,6 @@ export const AccessPackageInfoAlert = () => {
           components={{
             a: (
               <a
-                href={href}
                 target='_blank'
                 rel='noopener noreferrer'
               />
