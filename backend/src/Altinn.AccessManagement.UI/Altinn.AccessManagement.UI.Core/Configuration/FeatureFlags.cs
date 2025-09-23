268b24d0fc0709043a8ffd9ea39bd61caadd1b0f
Martin Gunnerud <mgunnerud@gmail.com>
2025-09-23 12:41:04 +0200

    add feature toggle for consent gui

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Configuration/FeatureFlags.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Configuration/FeatureFlags.cs
@@ -27,6 +27,11 @@ namespace Altinn.AccessManagement.UI.Core.Configuration
         public bool DisplayResourceDelegation { get; set; }
 
         /// <summary>
+        /// Whether or not to display the consent gui feature in the UI
+        /// </summary>
+        public bool DisplayConsentGui { get; set; }
+
+        /// <summary>
         /// Whether to show the new AMUI to PRIV users
         /// </summary>
         public bool RestrictPrivUse { get; set; }
