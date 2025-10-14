8039c9180d99fdc31b984d0cd4770b60136df969
Sondre Wittek <sondrewittek@gmail.com>
2025-10-14 16:27:06 +0200

    Refactor UserRightsPageSkeleton to simplify tab structure

--- src/features/amUI/userRightsPage/UserRightsPageSkeleton.tsx
+++ src/features/amUI/userRightsPage/UserRightsPageSkeleton.tsx
@@ -7,52 +7,15 @@ import { UserPageHeaderSkeleton } from '../common/UserPageHeader/UserPageHeaderS
 import { SkeletonAccessPackageList } from '../common/AccessPackageList/SkeletonAccessPackageList';
 
 import classes from '../common/RightsTabs/RightsTabs.module.css';
+import { TabContentSkeleton } from '../common/RightsTabs/TabContentSkeleton';
 
 export const UserRightsPageSkeleton = () => {
-  const { t } = useTranslation();
-  const { displayLimitedPreviewLaunch, displayResourceDelegation } = window.featureFlags;
-
   return (
     <PageContainer backUrl={`/${amUIPath.Users}`}>
       <UserPageHeaderSkeleton />
-      <DsTabs
-        defaultValue='packages'
-        data-size='sm'
-      >
-        <DsTabs.List>
-          <DsTabs.Tab value='packages'>{t('user_rights_page.access_packages_title')}</DsTabs.Tab>
-          {displayResourceDelegation && (
-            <DsTabs.Tab value='singleRights'>
-              {t('user_rights_page.single_rights_title')}
-            </DsTabs.Tab>
-          )}
-          {!displayLimitedPreviewLaunch && (
-            <DsTabs.Tab value='roleAssignments'>{t('user_rights_page.roles_title')}</DsTabs.Tab>
-          )}
-        </DsTabs.List>
-        <DsTabs.Panel
-          className={classes.tabContent}
-          value='packages'
-        >
-          <SkeletonAccessPackageList />
-        </DsTabs.Panel>
-        {displayResourceDelegation && (
-          <DsTabs.Panel
-            className={classes.tabContent}
-            value='singleRights'
-          >
-            <SkeletonAccessPackageList />
-          </DsTabs.Panel>
-        )}
-        {!displayLimitedPreviewLaunch && (
-          <DsTabs.Panel
-            className={classes.tabContent}
-            value='roleAssignments'
-          >
-            <SkeletonAccessPackageList />
-          </DsTabs.Panel>
-        )}
-      </DsTabs>
+      <div className={classes.tabContent}>
+        <TabContentSkeleton />
+      </div>
     </PageContainer>
   );
 };
