c425600ac7cf490488dd5b4f06aa45fa622dd45c
Sondre Wittek <sondrewittek@gmail.com>
2025-11-04 19:57:22 +0100

    Add error handling for 'person' user type in PartyRepresentationProvider (#1695)
    * Add error handling for 'person' user type in PartyRepresentationProvider
    
    * Update renderWithProvider to include loadingComponent prop in tests

--- src/features/amUI/poaOverview/PoaOverviewPage.tsx
+++ src/features/amUI/poaOverview/PoaOverviewPage.tsx
@@ -31,6 +31,7 @@ export const PoaOverviewPage = () => {
         <PartyRepresentationProvider
           fromPartyUuid={partyUuid}
           actingPartyUuid={partyUuid ?? ''}
+          errorOnPriv={true}
         >
           <ReporteePageHeading
             title={t('poa_overview_page.heading', { name: reportee?.name || '' })}
