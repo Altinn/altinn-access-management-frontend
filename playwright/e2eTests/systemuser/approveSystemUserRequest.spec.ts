cb983f51a0253287e40562dbeecc2cd763a2ce93
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-11-12 20:38:02 +0100

    support new actor - system user

--- playwright/e2eTests/systemuser/approveSystemUserRequest.spec.ts
+++ playwright/e2eTests/systemuser/approveSystemUserRequest.spec.ts
@@ -8,17 +8,16 @@ test.describe('Godkjenn og avvis Systembrukerforespørsel', () => {
   const orgNumber = '310547891'; // Hardcoded org ID for testing
   const systemId = '310547891_E2E-Playwright-Authentication'; // Hardcoded system ID for testing
 
-  test.beforeEach(async ({ page, login }) => {
+  test.beforeEach(async () => {
     api = new ApiRequests(orgNumber);
-    await login.LoginWithUserFromFrontpage('14824497789');
-    await login.chooseReportee('AKTVERDIG RETORISK APE');
   });
 
-  test('Avvis Systembrukerforespørsel', async ({ page }): Promise<void> => {
+  test('Avvis Systembrukerforespørsel', async ({ page, login }): Promise<void> => {
     const externalRef = TestdataApi.generateExternalRef();
     const response = await api.postSystemuserRequest(externalRef, systemId);
 
     await page.goto(response.confirmUrl);
+    await login.LoginWithUserFromFrontpage('14824497789');
 
     await page.getByRole('button', { name: 'Avvis' }).click();
 
@@ -32,11 +31,13 @@ test.describe('Godkjenn og avvis Systembrukerforespørsel', () => {
     expect(statusApiRequest.status).toBe('Rejected');
   });
 
-  test('Godkjenn Systembrukerforespørsel', async ({ page }): Promise<void> => {
+  test('Godkjenn Systembrukerforespørsel', async ({ page, login }): Promise<void> => {
     const externalRef = TestdataApi.generateExternalRef();
     const response = await api.postSystemuserRequest(externalRef, systemId);
 
     await page.goto(response.confirmUrl);
+    await login.LoginWithUserFromFrontpage('14824497789');
+
     await page.getByRole('button', { name: 'Godkjenn' }).click();
 
     //Expect user to be logged out
