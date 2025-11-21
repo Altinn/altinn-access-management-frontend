572bcbf5b66bc7538de73d32a25e13b8133e2076
Martin Gunnerud <mgunnerud@gmail.com>
2025-11-21 09:19:16 +0100

    change floating help button according to design

--- src/features/amUI/common/PageLayoutWrapper/InfoModal.tsx
+++ src/features/amUI/common/PageLayoutWrapper/InfoModal.tsx
@@ -1,73 +1,40 @@
-import { DsButton, DsDialog, DsHeading, DsLink, DsParagraph } from '@altinn/altinn-components';
-import { useEffect, useState } from 'react';
-import { InformationSquareIcon } from '@navikt/aksel-icons';
+import { FloatingDropdown } from '@altinn/altinn-components';
+import { ExternalLinkIcon, LeaveIcon, QuestionmarkIcon } from '@navikt/aksel-icons';
 
-import classes from './InfoModal.module.css';
-import { getHostUrl } from '@/resources/utils/pathUtils';
-import { amUIPath } from '@/routes/paths';
-import { Link } from 'react-router';
-import { t } from 'i18next';
+import { getAltinnStartPageUrl, getHostUrl } from '@/resources/utils/pathUtils';
+import { useTranslation } from 'react-i18next';
 
 export const InfoModal = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { t, i18n } = useTranslation();
 
-  useEffect(() => {
-    // Check if the modal has been displayed before
-    const hasBeenDisplayed = localStorage.getItem('am:beta-modal-displayed');
+  const goToOldSolution = () => {
+    window.location.assign(getHostUrl() + 'ui/profile');
+  };
 
-    // If not displayed or value is 'false', open the modal
-    if (!hasBeenDisplayed || hasBeenDisplayed === 'false') {
-      setIsOpen(true);
-    }
-
-    localStorage.setItem('am:beta-modal-displayed', 'true');
-  }, []);
+  const goToHelpPages = () => {
+    const route = i18n.language === 'en' ? 'help' : 'hjelp';
+    window.location.assign(getAltinnStartPageUrl() + route);
+  };
 
   return (
     <>
-      <DsButton
-        onClick={() => setIsOpen(true)}
-        className={classes.infoModalButton}
-        icon
-        data-size='lg'
-        aria-label={t('info_modal.info_button')}
-      >
-        <InformationSquareIcon title='Info' />
-      </DsButton>
-      <DsDialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        closeButton={false}
-        aria-labelledby='info_modal-title'
-      >
-        <div className={classes.modalContent}>
-          <DsHeading
-            id='info_modal-title'
-            data-size='sm'
-          >
-            {t('info_modal.title')}
-          </DsHeading>
-          <DsParagraph>{t('info_modal.info')}</DsParagraph>
-          <div className={classes.modalActions}>
-            <DsButton onClick={() => setIsOpen(false)}>{t('info_modal.close_button')}</DsButton>
-            <DsButton
-              variant='secondary'
-              asChild
-            >
-              <a href={getHostUrl() + 'ui/profile'}>{t('info_modal.back_button')}</a>
-            </DsButton>
-          </div>
-          <DsLink asChild>
-            <Link
-              to={`/${amUIPath.Info}`}
-              target='_blank'
-              rel='noreferrer'
-            >
-              {t('info_modal.info_link')}
-            </Link>
-          </DsLink>
-        </div>
-      </DsDialog>
+      <FloatingDropdown
+        icon={QuestionmarkIcon}
+        iconAltText={t('info_modal.info_button')}
+        color='company'
+        items={[
+          {
+            icon: ExternalLinkIcon,
+            title: t('info_modal.help_pages'),
+            onClick: goToHelpPages,
+          },
+          {
+            icon: LeaveIcon,
+            title: t('info_modal.back_button'),
+            onClick: goToOldSolution,
+          },
+        ]}
+      />
     </>
   );
 };
