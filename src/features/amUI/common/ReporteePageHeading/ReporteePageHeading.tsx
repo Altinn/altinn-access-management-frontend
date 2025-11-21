82aaf786d3167c82750268cfbdf64b2bd34db42b
Martin Gunnerud <mgunnerud@gmail.com>
2025-11-21 13:07:41 +0100

    show 'underenhet' in header for subunits + fix according to design

--- src/features/amUI/common/ReporteePageHeading/ReporteePageHeading.tsx
+++ src/features/amUI/common/ReporteePageHeading/ReporteePageHeading.tsx
@@ -11,6 +11,8 @@ type Props = {
   className?: string;
   level?: 1 | 2 | 3 | 4 | 5 | 6;
   dataSize?: 'sm' | 'md' | 'lg';
+  subHeadingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
+  subHeadingDataSize: '2xs' | 'xs' | 'sm' | 'md' | 'lg';
   isLoading?: boolean;
 };
 
@@ -20,11 +22,14 @@ export const ReporteePageHeading: React.FC<Props> = ({
   className,
   level = 1,
   dataSize = 'sm',
+  subHeadingLevel = 2,
+  subHeadingDataSize = '2xs',
   isLoading = false,
 }) => {
   const { t } = useTranslation();
   const orgNumber = reportee?.organizationNumber ?? '';
   const isMainUnit = (reportee?.subunits?.length ?? 0) > 0;
+  const isSubUnit = reportee?.type === 'BEDR' || reportee?.type === 'AAFY';
 
   if (isLoading) {
     return (
@@ -45,16 +50,22 @@ export const ReporteePageHeading: React.FC<Props> = ({
     );
   }
   return (
-    <DsHeading
-      level={level}
-      data-size={dataSize}
-      className={className || styles.pageHeading}
-    >
-      {title}
-      <br />
-      {t('common.org_nr_lowercase', { org_number: orgNumber })}{' '}
-      {isMainUnit ? `(${t('common.mainunit_lowercase')})` : ''}
-    </DsHeading>
+    <div className={className || styles.pageHeading}>
+      <DsHeading
+        level={level}
+        data-size={dataSize}
+      >
+        {title}
+      </DsHeading>
+      <DsHeading
+        level={subHeadingLevel}
+        data-size={subHeadingDataSize}
+      >
+        {t('common.org_nr_lowercase', { org_number: orgNumber.match(/.{1,3}/g)?.join(' ') })}{' '}
+        {isMainUnit ? `(${t('common.mainunit_lowercase')})` : ''}
+        {isSubUnit ? `(${t('common.subunit_lowercase')})` : ''}
+      </DsHeading>
+    </div>
   );
 };
 
