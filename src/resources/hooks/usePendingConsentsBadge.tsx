538f9edfbd7830bffd5cc67feb5641744e9ff389
Martin Gunnerud <mgunnerud@gmail.com>
2025-12-03 13:33:40 +0100

    add loading state

--- src/resources/hooks/usePendingConsentsBadge.tsx
+++ src/resources/hooks/usePendingConsentsBadge.tsx
@@ -1,8 +1,9 @@
+import { ReactElement } from 'react';
 import { getCookie } from '@/resources/Cookie/CookieMethods';
 import { useGetActiveConsentsQuery } from '@/rtk/features/consentApi';
 import { hasConsentPermission } from '../utils/permissionUtils';
 import { useGetIsAdminQuery, useGetReporteeQuery } from '@/rtk/features/userInfoApi';
-import { BadgeProps } from '@altinn/altinn-components';
+import { BadgeProps, DsSpinner } from '@altinn/altinn-components';
 
 export const usePendingConsentsBadge = () => {
   const partyUuid = getCookie('AltinnPartyUuid');
@@ -10,19 +11,27 @@ export const usePendingConsentsBadge = () => {
   const { data: reportee } = useGetReporteeQuery();
   const { data: isAdmin } = useGetIsAdminQuery();
   const hasPermissionToAcceptConsent = hasConsentPermission(reportee, isAdmin);
-  const { data: activeConsents } = useGetActiveConsentsQuery(
+  const { data: activeConsents, isLoading: isLoadingPendingConsents } = useGetActiveConsentsQuery(
     { partyId: partyUuid },
     { skip: !partyUuid || !hasPermissionToAcceptConsent },
   );
-
   const pendingConsentsCount = activeConsents?.filter((consent) => consent.isPendingConsent);
-  const consentBadge: BadgeProps | undefined = pendingConsentsCount?.length
-    ? {
-        label: pendingConsentsCount.length,
-        color: 'danger',
-        variant: 'base',
-      }
-    : undefined;
+
+  let consentBadge: BadgeProps | ReactElement | undefined = undefined;
+  if (isLoadingPendingConsents) {
+    consentBadge = (
+      <DsSpinner
+        data-size='xs'
+        aria-label='laster'
+      />
+    );
+  } else if (pendingConsentsCount?.length) {
+    consentBadge = {
+      label: pendingConsentsCount.length,
+      color: 'danger',
+      variant: 'base',
+    };
+  }
 
   return { consentBadge };
 };
