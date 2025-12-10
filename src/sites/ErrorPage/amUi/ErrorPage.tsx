4624bd8d737bfd10dcbf7f9c8ab9c5139a479b41
Vedeler <acn-avede@ai-dev.no>
2025-12-10 13:29:56 +0100

    new error page

--- src/sites/ErrorPage/amUi/ErrorPage.tsx
+++ src/sites/ErrorPage/amUi/ErrorPage.tsx
@@ -4,11 +4,10 @@ import { useRouteError } from 'react-router';
 import * as React from 'react';
 import { DsParagraph } from '@altinn/altinn-components';
 
-import { Page, PageContent, PageContainer } from '@/components';
-
 import { PageNotFound } from './contents/PageNotFound';
 import { UnknownError } from './contents/UnknownError';
 import classes from './ErrorPage.module.css';
+import { ErrorLayoutWrapper } from './ErrorLayoutWrapper';
 
 export const ErrorPage = () => {
   const { t } = useTranslation();
@@ -26,34 +25,27 @@ export const ErrorPage = () => {
 
   return (
     <div>
-      <PageContainer>
-        <Page>
-          <PageContent>
-            <div className={classes.errorPageWrapper}>
-              <DsParagraph
-                data-size='sm'
-                className={classes.errorMessage}
-              >
-                {t('common.time')}: {timestamp.toLocaleString('no-NO')}
-              </DsParagraph>
-              <DsParagraph
-                data-size='sm'
-                className={classes.errorMessage}
-              >
-                {t('common.error_status_code')}: {error.status}
-              </DsParagraph>
+      <ErrorLayoutWrapper>
+        <div className={classes.errorPageWrapper}>
+          <div className={classes.errorStatus}>
+            <DsParagraph data-size='sm'>
+              {t('common.error_status_code')}: {error.status}
+            </DsParagraph>
+            <DsParagraph data-size='sm'>
+              {t('common.time')}: {timestamp.toLocaleString('no-NO')}
+            </DsParagraph>
+            {error.status !== 404 && (
               <DsParagraph
                 data-size='sm'
                 variant='long'
-                className={classes.errorMessage}
               >
                 {t('common.error_message')}: {error.error?.message ?? error?.message}
               </DsParagraph>
-              {renderContent()}
-            </div>
-          </PageContent>
-        </Page>
-      </PageContainer>
+            )}
+          </div>
+          {renderContent()}
+        </div>
+      </ErrorLayoutWrapper>
     </div>
   );
 };
