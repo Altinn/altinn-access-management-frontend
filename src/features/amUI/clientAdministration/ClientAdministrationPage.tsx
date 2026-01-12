7634a12ba8ed10ae13c329b6f86ff8ddb4eed9f1
Sondre Wittek <sondrewittek@gmail.com>
2026-01-12 15:45:03 +0100

    Add tabs and agent management functionality to Client Administration page

--- src/features/amUI/clientAdministration/ClientAdministrationPage.tsx
+++ src/features/amUI/clientAdministration/ClientAdministrationPage.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Navigate } from 'react-router';
-import { DsAlert, DsHeading, DsParagraph, DsSkeleton } from '@altinn/altinn-components';
+import { DsAlert, DsHeading, DsParagraph, DsSkeleton, DsTabs } from '@altinn/altinn-components';
 
 import { PageWrapper } from '@/components/PageWrapper/PageWrapper';
 import { PageLayoutWrapper } from '../common/PageLayoutWrapper';
 import { clientAdministrationPageEnabled } from '@/resources/utils/featureFlagUtils';
 import { useGetIsClientAdminQuery } from '@/rtk/features/userInfoApi';
+import { AddAgentButton } from '../users/NewUserModal/AddAgentModal';
 
 export const ClientAdministrationPage = () => {
   const { t } = useTranslation();
   const pageIsEnabled = clientAdministrationPageEnabled();
+  const [activeTab, setActiveTab] = useState('users');
   const {
     data: isClientAdmin,
     isLoading: isLoadingIsClientAdmin,
@@ -59,6 +61,25 @@ export const ClientAdministrationPage = () => {
     <PageWrapper>
       <PageLayoutWrapper>
         <DsHeading data-size='lg'>{t('client_administration_page.page_heading')}</DsHeading>
+        <DsTabs
+          defaultValue='users'
+          data-size='sm'
+          value={activeTab}
+          onChange={setActiveTab}
+        >
+          <DsTabs.List>
+            <DsTabs.Tab value='users'>
+              {t('client_administration_page.agents_tab_title')}
+            </DsTabs.Tab>
+            <DsTabs.Tab value='clients'>
+              {t('client_administration_page.clients_tab_title')}
+            </DsTabs.Tab>
+          </DsTabs.List>
+          <DsTabs.Panel value='users'>
+            <AddAgentButton />
+          </DsTabs.Panel>
+          <DsTabs.Panel value='clients' />
+        </DsTabs>
       </PageLayoutWrapper>
     </PageWrapper>
   );
