1cdcb77df4a59d0fddb62aa7be2c8bee655161fa
Sondre Wittek <sondrewittek@gmail.com>
2026-01-21 16:00:02 +0100

    Add tabs for agent client delegation views and update localization strings

--- src/features/amUI/clientAdministrationAgentDetails/ClientAdministrationAgentDetails.tsx
+++ src/features/amUI/clientAdministrationAgentDetails/ClientAdministrationAgentDetails.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   DsAlert,
   DsHeading,
   DsParagraph,
   DsSkeleton,
+  DsTabs,
   formatDisplayName,
 } from '@altinn/altinn-components';
 import { useParams } from 'react-router';
@@ -27,6 +28,7 @@ export const ClientAdministrationAgentDetails = () => {
   const { t } = useTranslation();
   const { id } = useParams();
   const { toParty, actingParty } = usePartyRepresentation();
+  const [activeTab, setActiveTab] = useState('has-clients');
   const { data: isClientAdmin, isLoading: isLoadingIsClientAdmin } = useGetIsClientAdminQuery();
   const {
     data: agentAccessPackages,
@@ -99,119 +101,133 @@ export const ClientAdministrationAgentDetails = () => {
         )}
 
         {id && (
-          <>
-            <DsHeading data-size='sm'>
-              {t('client_administration_page.delegations_heading')}
-            </DsHeading>
-            {isLoadingAgentAccessPackages && (
-              <DsParagraph data-size='sm'>
-                <DsSkeleton variant='text' />
-              </DsParagraph>
-            )}
-            {isErrorAgentAccessPackages && (
-              <DsAlert data-color='warning'>
-                <DsParagraph>{t('client_administration_page.load_delegations_error')}</DsParagraph>
-              </DsAlert>
-            )}
-            {!isLoadingAgentAccessPackages &&
-              !isErrorAgentAccessPackages &&
-              agentAccessPackages &&
-              agentAccessPackages.length > 0 && (
-                <ul>
-                  {agentAccessPackages.map((client) => (
-                    <li key={client.client.id}>{client.client.name}</li>
-                  ))}
-                </ul>
+          <DsTabs
+            defaultValue='has-clients'
+            data-size='sm'
+            value={activeTab}
+            onChange={setActiveTab}
+          >
+            <DsTabs.List>
+              <DsTabs.Tab value='has-clients'>
+                {t('client_administration_page.agent_has_clients_tab')}
+              </DsTabs.Tab>
+              <DsTabs.Tab value='can-get-clients'>
+                {t('client_administration_page.agent_can_get_clients_tab')}
+              </DsTabs.Tab>
+            </DsTabs.List>
+            <DsTabs.Panel value='has-clients'>
+              <DsHeading data-size='sm'>
+                {t('client_administration_page.delegations_heading')}
+              </DsHeading>
+              {isLoadingAgentAccessPackages && (
+                <DsParagraph data-size='sm'>
+                  <DsSkeleton variant='text' />
+                </DsParagraph>
               )}
-            {!isLoadingAgentAccessPackages &&
-              !isErrorAgentAccessPackages &&
-              agentAccessPackages &&
-              agentAccessPackages.length === 0 && (
-                <DsParagraph>{t('client_administration_page.no_delegations')}</DsParagraph>
+              {isErrorAgentAccessPackages && (
+                <DsAlert data-color='warning'>
+                  <DsParagraph>
+                    {t('client_administration_page.load_delegations_error')}
+                  </DsParagraph>
+                </DsAlert>
               )}
-          </>
-        )}
-
-        {id && (
-          <>
-            <DsHeading data-size='sm'>
-              {t('client_administration_page.clients_tab_title')}
-            </DsHeading>
-            {isLoadingClients && (
-              <DsParagraph data-size='sm'>
-                <DsSkeleton variant='text' />
-              </DsParagraph>
-            )}
-            {isErrorClients && (
-              <DsAlert data-color='warning'>
-                <DsParagraph>{t('common.general_error_paragraph')}</DsParagraph>
-              </DsAlert>
-            )}
-            {!isLoadingClients && !isErrorClients && clients && clients.length === 0 && (
-              <DsParagraph>{t('client_administration_page.no_clients')}</DsParagraph>
-            )}
-            {!isLoadingClients && !isErrorClients && clients && clients.length > 0 && (
-              <div>
-                {clients.map((client) => (
-                  <details key={client.client.id}>
-                    <summary>{client.client.name}</summary>
-                    {client.access.length === 0 && (
-                      <DsParagraph data-size='sm'>
-                        {t('client_administration_page.no_delegations')}
-                      </DsParagraph>
-                    )}
-                    {client.access.length > 0 && (
-                      <ul>
-                        {client.access.map((access) => (
-                          <li key={`${client.client.id}-${access.role.id}`}>
-                            <span>{access.role.code}</span>
-                            {access.packages.length > 0 && (
-                              <ul>
-                                {access.packages.map((pkg) => (
-                                  <li key={`${client.client.id}-${access.role.id}-${pkg.id}`}>
-                                    <span>{pkg.urn ?? pkg.id}</span>
-                                    <button
-                                      type='button'
-                                      disabled={
-                                        isAddingAgentAccessPackages ||
-                                        !toPartyUuid ||
-                                        !actingPartyUuid
-                                      }
-                                      onClick={() => {
-                                        if (!toPartyUuid || !actingPartyUuid) {
-                                          return;
+              {!isLoadingAgentAccessPackages &&
+                !isErrorAgentAccessPackages &&
+                agentAccessPackages &&
+                agentAccessPackages.length > 0 && (
+                  <ul>
+                    {agentAccessPackages.map((client) => (
+                      <li key={client.client.id}>{client.client.name}</li>
+                    ))}
+                  </ul>
+                )}
+              {!isLoadingAgentAccessPackages &&
+                !isErrorAgentAccessPackages &&
+                agentAccessPackages &&
+                agentAccessPackages.length === 0 && (
+                  <DsParagraph>{t('client_administration_page.no_delegations')}</DsParagraph>
+                )}
+            </DsTabs.Panel>
+            <DsTabs.Panel value='can-get-clients'>
+              <DsHeading data-size='sm'>
+                {t('client_administration_page.clients_tab_title')}
+              </DsHeading>
+              {isLoadingClients && (
+                <DsParagraph data-size='sm'>
+                  <DsSkeleton variant='text' />
+                </DsParagraph>
+              )}
+              {isErrorClients && (
+                <DsAlert data-color='warning'>
+                  <DsParagraph>{t('common.general_error_paragraph')}</DsParagraph>
+                </DsAlert>
+              )}
+              {!isLoadingClients && !isErrorClients && clients && clients.length === 0 && (
+                <DsParagraph>{t('client_administration_page.no_clients')}</DsParagraph>
+              )}
+              {!isLoadingClients && !isErrorClients && clients && clients.length > 0 && (
+                <div>
+                  {clients.map((client) => (
+                    <details key={client.client.id}>
+                      <summary>{client.client.name}</summary>
+                      {client.access.length === 0 && (
+                        <DsParagraph data-size='sm'>
+                          {t('client_administration_page.no_delegations')}
+                        </DsParagraph>
+                      )}
+                      {client.access.length > 0 && (
+                        <ul>
+                          {client.access.map((access) => (
+                            <li key={`${client.client.id}-${access.role.id}`}>
+                              <span>{access.role.code}</span>
+                              {access.packages.length > 0 && (
+                                <ul>
+                                  {access.packages.map((pkg) => (
+                                    <li key={`${client.client.id}-${access.role.id}-${pkg.id}`}>
+                                      <span>{pkg.urn ?? pkg.id}</span>
+                                      <button
+                                        type='button'
+                                        disabled={
+                                          isAddingAgentAccessPackages ||
+                                          !toPartyUuid ||
+                                          !actingPartyUuid
                                         }
+                                        onClick={() => {
+                                          if (!toPartyUuid || !actingPartyUuid) {
+                                            return;
+                                          }
 
-                                        void addAgentAccessPackages({
-                                          from: client.client.id,
-                                          to: toPartyUuid,
-                                          party: actingPartyUuid,
-                                          payload: {
-                                            values: [
-                                              {
-                                                role: access.role.code,
-                                                packages: [pkg.urn ?? ''],
-                                              },
-                                            ],
-                                          },
-                                        });
-                                      }}
-                                    >
-                                      {t('client_administration_page.delegate_package_button')}
-                                    </button>
-                                  </li>
-                                ))}
-                              </ul>
-                            )}
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </details>
-                ))}
-              </div>
-            )}
-          </>
+                                          void addAgentAccessPackages({
+                                            from: client.client.id,
+                                            to: toPartyUuid,
+                                            party: actingPartyUuid,
+                                            payload: {
+                                              values: [
+                                                {
+                                                  role: access.role.code,
+                                                  packages: [pkg.id],
+                                                },
+                                              ],
+                                            },
+                                          });
+                                        }}
+                                      >
+                                        {t('client_administration_page.delegate_package_button')}
+                                      </button>
+                                    </li>
+                                  ))}
+                                </ul>
+                              )}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </details>
+                  ))}
+                </div>
+              )}
+            </DsTabs.Panel>
+          </DsTabs>
         )}
       </PageContainer>
     </>
