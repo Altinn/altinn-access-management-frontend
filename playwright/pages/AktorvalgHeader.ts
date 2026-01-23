874b6f1ce56a77354b413cc71a03ada001c7f334
Ivar Olav Tryti <aas-iotryti@ai-dev.no>
2026-01-23 11:08:58 +0100

    Gjorde favoritter-biten mindre flaky i Aktørvalg-testen.

--- playwright/pages/AktorvalgHeader.ts
+++ playwright/pages/AktorvalgHeader.ts
@@ -121,6 +121,13 @@ export class AktorvalgHeader {
     ).toBeVisible();
   }
 
+  async removeAllFavorites() {
+    var thing = await this.page.getByRole('button', { name: 'Fjern frå favorittar' }).all();
+    for (const item of thing) {
+      item.click();
+    }
+  }
+
   async unfavoriteFirstActor() {
     await this.page.getByRole('button', { name: 'Fjern frå favorittar' }).first().click();
     await expect(this.page.getByRole('button', { name: 'Fjern frå favorittar' })).toHaveCount(0);
