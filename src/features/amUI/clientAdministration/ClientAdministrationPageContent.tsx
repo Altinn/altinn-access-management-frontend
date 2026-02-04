9a31b9f8d38a5d277a1e955c538944172a712b5d
Sondre Wittek <sondrewittek@gmail.com>
2026-02-04 16:34:45 +0100

    Client administration header and page description.

--- src/features/amUI/clientAdministration/ClientAdministrationPageContent.tsx
+++ src/features/amUI/clientAdministration/ClientAdministrationPageContent.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { Navigate, useSearchParams } from 'react-router';
 import { DsAlert, DsHeading, DsParagraph, DsSkeleton, DsTabs } from '@altinn/altinn-components';
 
 import { clientAdministrationPageEnabled } from '@/resources/utils/featureFlagUtils';
 import { useGetIsClientAdminQuery } from '@/rtk/features/userInfoApi';
+import { usePartyRepresentation } from '../common/PartyRepresentationContext/PartyRepresentationContext';
 import { ClientAdministrationAgentsTab } from './ClientAdministrationAgentsTab';
 import { ClientAdministrationClientsTab } from './ClientAdministrationClientsTab';
+import classes from './ClientAdministrationPageContent.module.css';
 
 export const ClientAdministrationPageContent = () => {
   const { t } = useTranslation();
   const pageIsEnabled = clientAdministrationPageEnabled();
+  const { actingParty } = usePartyRepresentation();
 
   const [params, setParams] = useSearchParams();
   const activeTab = params.get('tab') === 'clients' ? 'clients' : 'users';
@@ -33,8 +36,10 @@ export const ClientAdministrationPageContent = () => {
   if (isLoadingIsClientAdmin) {
     return (
       <>
-        <DsHeading data-size='lg'>
-          <DsSkeleton variant='text'>{t('client_administration_page.page_heading')}</DsSkeleton>
+        <DsHeading data-size='md'>
+          <DsSkeleton variant='text'>
+            {t('client_administration_page.page_heading', { actingparty: '' })}
+          </DsSkeleton>
         </DsHeading>
         <DsParagraph data-size='lg'>
           <DsSkeleton
@@ -54,7 +59,28 @@ export const ClientAdministrationPageContent = () => {
 
   return (
     <>
-      <DsHeading data-size='lg'>{t('client_administration_page.page_heading')}</DsHeading>
+      <div className={classes.headerSection}>
+        <div className={classes.pageHeader}>
+          <DsHeading data-size='sm'>
+            <Trans
+              i18nKey='client_administration_page.page_heading'
+              values={{ actingparty: actingParty?.name ?? '' }}
+            />
+          </DsHeading>
+          <DsParagraph data-size='md'>
+            <Trans
+              i18nKey='client_administration_page.page_description'
+              values={{ 'actingparty.organisationidentifier': actingParty?.orgNumber ?? '' }}
+            />
+          </DsParagraph>
+        </div>
+        <DsParagraph
+          data-size='md'
+          className={classes.pageDescription}
+        >
+          {t('client_administration_page.forwarding_accesses_info')}
+        </DsParagraph>
+      </div>
       <DsTabs
         defaultValue='users'
         data-size='sm'
