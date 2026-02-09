350b5faf2750bb824817c7f06fef140c4d866728
Sondre Wittek <sondrewittek@gmail.com>
2026-02-09 16:09:30 +0100

    Improve user messages for agent search placeholders in localization files

--- src/features/amUI/clientAdministration/ClientAdministrationClientsTab.tsx
+++ src/features/amUI/clientAdministration/ClientAdministrationClientsTab.tsx
@@ -70,6 +70,7 @@ export const ClientAdministrationClientsTab = () => {
         connections={clientConnections}
         isLoading={isClientsLoading}
         canDelegate={false}
+        searchPlaceholder={t('client_administration_page.client_search_placeholder')}
         noUsersText={t('client_administration_page.no_clients')}
         getUserLink={(user) => `/clientadministration/client/${user.id}`}
         hasActiveAdditionalFilters={roleFilter.length > 0 || !showDeleted}
