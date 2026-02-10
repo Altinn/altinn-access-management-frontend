dc8572c5d40823c65fff48f6b5e43147b72579c1
Martin Gunnerud <mgunnerud@gmail.com>
2026-02-10 10:02:42 +0100

    use new toolbar to search and filter resources in delegation modal

--- src/features/amUI/common/DelegationModal/SingleRights/ResourceSearch.tsx
+++ src/features/amUI/common/DelegationModal/SingleRights/ResourceSearch.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { FilterIcon } from '@navikt/aksel-icons';
 import { useParams } from 'react-router';
-import { DsHeading, DsSearch } from '@altinn/altinn-components';
+import { DsHeading } from '@altinn/altinn-components';
 import { useCallback } from 'react';
 
 import type { ServiceResource } from '@/rtk/features/singleRights/singleRightsApi';
@@ -12,15 +11,14 @@ import {
 } from '@/rtk/features/singleRights/singleRightsApi';
 import { useGetResourceOwnersQuery } from '@/rtk/features/resourceApi';
 import { arraysEqual, debounce } from '@/resources/utils';
-import { Filter } from '@/components';
 import { getCookie } from '@/resources/Cookie/CookieMethods';
 import type { Party } from '@/rtk/features/lookupApi';
 
 import { useDelegationModalContext } from '../DelegationModalContext';
 import { SearchResults } from './SearchResults';
-import { FilterChips } from './FilterChips';
 
 import classes from './ResourceSearch.module.css';
+import { ResourceFilterToolbar } from '../../ResourceFilterToolbar/ResourceFilterToolbar';
 
 export interface ResourceSearchProps {
   onSelect: (resource: ServiceResource) => void;
@@ -93,67 +91,38 @@ export const ResourceSearch = ({ onSelect, toParty }: ResourceSearchProps) => {
           components={{ strong: <strong /> }}
         />
       </DsHeading>
-      <search className={classes.searchSection}>
-        <div className={classes.searchInputs}>
-          <div className={classes.searchField}>
-            <DsSearch aria-label={t('single_rights.search_label')}>
-              <DsSearch.Input
-                aria-label={t('single_rights.search_label')}
-                placeholder={t('single_rights.search_label')}
-                value={searchString}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  debouncedSearch(event.target.value);
-                  setSearchString(event.target.value);
-                }}
-              />
-              <DsSearch.Clear
-                onClick={() => {
-                  setDebouncedSearchString('');
-                  setSearchString('');
-                  setCurrentPage(1);
-                }}
-              />
-            </DsSearch>
-          </div>
-          <Filter
-            className={classes.filter}
-            icon={<FilterIcon />}
-            label={t('single_rights.filter_label')}
-            options={filterOptions}
-            applyButtonLabel={t('common.apply')}
-            resetButtonLabel={t('common.reset_choices')}
-            closeButtonAriaLabel={t('common.close')}
-            searchable
-            values={filters}
-            onApply={(filtersToApply: string[]) => {
-              if (!arraysEqual(filtersToApply, filters)) {
-                setFilters(filtersToApply);
-                setCurrentPage(1);
-              }
-            }}
-          />
-        </div>
-        <FilterChips
-          filters={filters}
-          filterOptions={filterOptions}
-          onRemoveFilter={unCheckFilter}
+      <div className={classes.toolbarContainer}>
+        <ResourceFilterToolbar
+          search={searchString}
+          setSearch={(searchString: string) => {
+            debouncedSearch(searchString);
+            setSearchString(searchString);
+          }}
+          filterState={filters}
+          setFilterState={(filtersToApply: string[]) => {
+            if (!arraysEqual(filtersToApply, filters)) {
+              setFilters(filtersToApply ?? []);
+              setCurrentPage(1);
+            }
+          }}
+          serviceOwnerOptions={filterOptions}
         />
-        <div className={classes.searchResults}>
-          <SearchResults
-            isFetching={isFetching}
-            error={error}
-            resources={resources}
-            searchString={searchString}
-            delegatedResources={delegatedResources}
-            totalNumberOfResults={totalNumberOfResults}
-            displayPopularResources={displayPopularResources}
-            currentPage={currentPage}
-            searchResultsPerPage={searchResultsPerPage}
-            onSelect={onSelect}
-            setCurrentPage={setCurrentPage}
-          />
-        </div>
-      </search>
+      </div>
+      <div className={classes.searchResults}>
+        <SearchResults
+          isFetching={isFetching}
+          error={error}
+          resources={resources}
+          searchString={searchString}
+          delegatedResources={delegatedResources}
+          totalNumberOfResults={totalNumberOfResults}
+          displayPopularResources={displayPopularResources}
+          currentPage={currentPage}
+          searchResultsPerPage={searchResultsPerPage}
+          onSelect={onSelect}
+          setCurrentPage={setCurrentPage}
+        />
+      </div>
     </>
   );
 };
