8f8d54fc4175235ce1d053ccb7641832e485a6c3
Sondre Wittek <sondrewittek@gmail.com>
2026-02-16 14:02:12 +0100

    restore

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -4,14 +4,8 @@ import classes from './PageWrapper.module.css';
 
 export interface PageWrapperProps {
   children: React.ReactNode;
-  pageAction?: React.ReactNode;
 }
 
-export const PageWrapper = ({ children, pageAction }: PageWrapperProps) => {
-  return (
-    <div className={classes.background}>
-      {pageAction ? <div className={classes.pageAction}>{pageAction}</div> : null}
-      {children}
-    </div>
-  );
+export const PageWrapper = ({ children }: PageWrapperProps) => {
+  return <div className={classes.background}>{children}</div>;
 };
