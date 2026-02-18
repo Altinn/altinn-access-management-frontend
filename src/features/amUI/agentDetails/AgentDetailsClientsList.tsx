4c52d1257d51b0bc63035e5a76cfcabf4700e5b9
Sondre Wittek <sondrewittek@gmail.com>
2026-02-18 15:45:03 +0100

    rename

---
+++ src/features/amUI/agentDetails/AgentDetailsClientsList.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { formatDisplayName } from '@altinn/altinn-components';
+
+import type {
+  AddAgentAccessPackagesFn,
+  Client,
+  RemoveAgentAccessPackagesFn,
+} from '@/rtk/features/clientApi';
+import { ClientAccessList } from '../common/ClientAccessList/ClientAccessList';
+
+import { useAgentDetailsAccessPackageActions } from './useAgentDetailsAccessPackageActions';
+import { usePartyRepresentation } from '../common/PartyRepresentationContext/PartyRepresentationContext';
+import { PartyType } from '@/rtk/features/userInfoApi';
+
+type AgentDetailsClientsListProps = {
+  clients: Client[];
+  agentAccessPackages: Client[];
+  isLoading: boolean;
+  toPartyUuid?: string;
+  actingPartyUuid?: string;
+  addAgentAccessPackages: AddAgentAccessPackagesFn;
+  removeAgentAccessPackages: RemoveAgentAccessPackagesFn;
+};
+
+export const AgentDetailsClientsList = ({
+  clients,
+  agentAccessPackages,
+  isLoading,
+  toPartyUuid,
+  actingPartyUuid,
+  addAgentAccessPackages,
+  removeAgentAccessPackages,
+}: AgentDetailsClientsListProps) => {
+  const { t } = useTranslation();
+
+  const delegateDisabled = isLoading || !toPartyUuid || !actingPartyUuid;
+  const removeDisabled = isLoading || !toPartyUuid || !actingPartyUuid;
+
+  const { addAgentAccessPackage, removeAgentAccessPackage } = useAgentDetailsAccessPackageActions({
+    toPartyUuid,
+    actingPartyUuid,
+    addAgentAccessPackages,
+    removeAgentAccessPackages,
+  });
+  const { toParty, actingParty } = usePartyRepresentation();
+  const agentName = formatDisplayName({
+    fullName: toParty?.name || '',
+    type: toParty?.partyTypeName === PartyType.Person ? 'person' : 'company',
+  });
+  const actingPartyName = formatDisplayName({
+    fullName: actingParty?.name || '',
+    type: actingParty?.partyTypeName === PartyType.Person ? 'person' : 'company',
+  });
+
+  return (
+    <ClientAccessList
+      clients={clients}
+      accessStateClients={agentAccessPackages}
+      addDisabled={delegateDisabled}
+      removeDisabled={removeDisabled}
+      onAddAccessPackage={({ clientId, roleCode, packageId, accessPackageName }) =>
+        addAgentAccessPackage(clientId, roleCode, packageId, agentName, accessPackageName)
+      }
+      onRemoveAccessPackage={({ clientId, roleCode, packageId, accessPackageName }) =>
+        removeAgentAccessPackage(clientId, roleCode, packageId, agentName, accessPackageName)
+      }
+      searchPlaceholder={t('client_administration_page.client_search_placeholder')}
+      emptyAccessText={t('client_administration_page.no_access_to_delegate', {
+        name: actingPartyName,
+      })}
+    />
+  );
+};
