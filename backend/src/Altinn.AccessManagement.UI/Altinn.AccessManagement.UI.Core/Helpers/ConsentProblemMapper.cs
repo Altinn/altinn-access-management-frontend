30cc7770a19986df1760d94b99f21674ad1ef74e
Martin Gunnerud <mgunnerud@gmail.com>
2026-02-24 14:42:04 +0100

    return unknown error with correct HTTP status code if consentClient response return error without ProblemDetails body

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Helpers/ConsentProblemMapper.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Core/Helpers/ConsentProblemMapper.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
+using System.Text.Json;
 using Altinn.AccessManagement.UI.Core.Constants;
 using Altinn.Authorization.ProblemDetails;
 
@@ -14,29 +15,40 @@ namespace Altinn.AccessManagement.UI.Core.Helpers
         /// <summary>
         /// Map error codes from AM to CTUI error codes
         /// </summary>
-        public static ProblemDescriptor MapToConsentUiError(AltinnProblemDetails problemDetails, HttpStatusCode statusCode)
+        public static ProblemDescriptor MapToConsentUiError(string responseContent, HttpStatusCode statusCode)
         {
-            if (!string.IsNullOrEmpty(problemDetails?.ErrorCode.ToString()))
+            try
             {
-                return problemDetails.ErrorCode.ToString() switch
-                {
-                    "AM-00000" => ConsentProblem.NotAuthorizedForConsentRequest,
-                    "AM-00001" => ConsentProblem.ConsentNotFound,
-                    "AM-00002" => ConsentProblem.ConsentCantBeAccepted,
-                    "AM-00010" => ConsentProblem.ConsentCantBeRevoked,
-                    "AM-00011" => ConsentProblem.ConsentRevoked,
-                    "AM-00013" => ConsentProblem.ConsentExpired,
-                    "AM-00014" => ConsentProblem.ConsentNotAccepted,
-                    "AM-00015" => ConsentProblem.ConsentCantBeRejected,
-                    _ => ConsentProblem.UnknownError
-                };
-            }
-            else if (statusCode == HttpStatusCode.Unauthorized)
-            {
-                return ConsentProblem.UnauthorizedUser;
-            }
+                AltinnProblemDetails problemDetails = JsonSerializer.Deserialize<AltinnProblemDetails>(responseContent);
+                string authErrorCode = problemDetails?.ErrorCode.ToString();
 
-            return ConsentProblem.UnknownError;
+                if (!string.IsNullOrEmpty(authErrorCode))
+                {
+                    return authErrorCode switch
+                    {
+                        "AM-00000" => ConsentProblem.NotAuthorizedForConsentRequest,
+                        "AM-00001" => ConsentProblem.ConsentNotFound,
+                        "AM-00002" => ConsentProblem.ConsentCantBeAccepted,
+                        "AM-00010" => ConsentProblem.ConsentCantBeRevoked,
+                        "AM-00011" => ConsentProblem.ConsentRevoked,
+                        "AM-00013" => ConsentProblem.ConsentExpired,
+                        "AM-00014" => ConsentProblem.ConsentNotAccepted,
+                        "AM-00015" => ConsentProblem.ConsentCantBeRejected,
+                        _ => ConsentProblem.UnknownError
+                    };
+                }
+                else if (statusCode == HttpStatusCode.Unauthorized)
+                {
+                    return ConsentProblem.UnauthorizedUser;
+                }
+
+                return ConsentProblem.UnknownError;
+            }
+            catch
+            {
+                // In case of deserialization failure or any other exception, return a generic problem descriptor
+                return ConsentProblem.CreateGenericProblem(statusCode, "Error without problem code");
+            }
         }
     }
 }
