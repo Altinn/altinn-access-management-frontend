62664399b4df7f4e1cd43b994c29887d6a483262
Sondre Wittek <sondrewittek@gmail.com>
2026-02-26 10:06:30 +0100

    Refactor rights access checks and missing messages

--- src/features/amUI/common/DelegationModal/SingleRights/hooks/useRightsSection.tsx
+++ src/features/amUI/common/DelegationModal/SingleRights/hooks/useRightsSection.tsx
@@ -19,6 +19,7 @@ import { useRightChips } from './useRightChips';
 import { useUpdateResource } from '@/resources/hooks/useUpdateResource';
 import { useRevokeResource } from '@/resources/hooks/useRevokeResource';
 import { useHasResourceCheck } from './useHasResourceCheck';
+import { getInheritedStatus } from '../../../useInheritedStatus';
 
 export const useRightsSection = ({
   resource,
@@ -100,46 +101,7 @@ export const useRightsSection = ({
         setCurrentRights([]);
       }
     }
-  }, [resourceRights, isResourceRightsFetching, resource.identifier, hasResourceAccess]);
-
-  // Instantiate/reset rights and missing access message states
-  useEffect(() => {
-    if (delegationCheckedActions) {
-      setMissingAccess(getMissingAccessMessage(delegationCheckedActions));
-
-      if (hasAccess && resourceRights) {
-        const chipRights: ChipRight[] = mapRightsToChipRights(
-          delegationCheckedActions,
-          (right) => currentRights.some((key) => key === right.right.key),
-          (rightKey) => resourceRights.indirectRights.some((r) => r.right.key === rightKey),
-        );
-        setRights(chipRights);
-      } else {
-        const chipRights: ChipRight[] = mapRightsToChipRights(
-          delegationCheckedActions,
-          (right) => right.result === true,
-          () => false, // If the user doesn't have access to the resource, none of the rights can be inherited
-        );
-        setRights(chipRights);
-      }
-    }
-  }, [delegationCheckedActions, resource.identifier, hasAccess, currentRights, resourceRights]);
-
-  /// Functions
-
-  const onSuccess = () => {
-    setIsActionLoading(false);
-    setIsActionSuccess(true);
-    setTimeout(() => setIsActionSuccess(false), 2000);
-    onDelegate?.();
-  };
-
-  const applyActionStates = () => {
-    setIsActionLoading(true);
-    setIsActionSuccess(false);
-    setDelegationError(null);
-    setMissingAccess(null);
-  };
+  }, [resourceRights, isResourceRightsFetching, hasResourceAccess]);
 
   const getMissingAccessMessage = useCallback(
     (response: DelegationCheckedRight[]) => {
@@ -173,6 +135,68 @@ export const useRightsSection = ({
     },
     [t, resource?.resourceOwnerName, reportee?.name],
   );
+
+  // Instantiate/reset rights and missing access message states
+  useEffect(() => {
+    if (delegationCheckedActions) {
+      setMissingAccess(getMissingAccessMessage(delegationCheckedActions));
+
+      if (hasAccess && resourceRights) {
+        const allRightAccesses = [...resourceRights.directRights, ...resourceRights.indirectRights];
+        const isRightInherited = (rightKey: string) =>
+          allRightAccesses
+            .filter((rightAccess) => rightAccess.right.key === rightKey)
+            .some(
+              (rightAccess) =>
+                getInheritedStatus({
+                  permissions: rightAccess.permissions,
+                  toParty,
+                  fromParty,
+                  actingParty,
+                }).length > 0,
+            );
+
+        const chipRights: ChipRight[] = mapRightsToChipRights(
+          delegationCheckedActions,
+          (right) => currentRights.some((key) => key === right.right.key),
+          isRightInherited,
+        );
+        setRights(chipRights);
+      } else {
+        const chipRights: ChipRight[] = mapRightsToChipRights(
+          delegationCheckedActions,
+          (right) => right.result === true,
+          () => false, // If the user doesn't have access to the resource, none of the rights can be inherited
+        );
+        setRights(chipRights);
+      }
+    }
+  }, [
+    actingParty,
+    currentRights,
+    delegationCheckedActions,
+    fromParty,
+    getMissingAccessMessage,
+    hasAccess,
+    resourceRights,
+    toParty,
+  ]);
+
+  /// Functions
+
+  const onSuccess = () => {
+    setIsActionLoading(false);
+    setIsActionSuccess(true);
+    setTimeout(() => setIsActionSuccess(false), 2000);
+    onDelegate?.();
+  };
+
+  const applyActionStates = () => {
+    setIsActionLoading(true);
+    setIsActionSuccess(false);
+    setDelegationError(null);
+    setMissingAccess(null);
+  };
 
   const saveEditedRights = () => {
     const actionKeysToDelegate = rights
