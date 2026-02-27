5695eb816ac65ee2e4e98313e7ed78b7b25f58b1
Martin Gunnerud <mgunnerud@gmail.com>
2026-02-27 09:13:33 +0100

    refactor mapping of system user errors; handle delegation check errors when loading request (#1989)
    * refactor mapping of system user errors; handle delegation check errors when loading request
    
    * fix

--- src/features/amUI/systemUser/components/DelegationCheckError/DelegationCheckError.tsx
+++ src/features/amUI/systemUser/components/DelegationCheckError/DelegationCheckError.tsx
@@ -5,6 +5,7 @@ import { DsAlert } from '@altinn/altinn-components';
 import type { ProblemDetail } from '../../types';
 
 import classes from './DelegationCheckError.module.css';
+import { mapErrorCodeToErrorMessage } from '../../errorHandling';
 
 interface DelegationCheckErrorProps {
   defaultError: string;
@@ -20,43 +21,7 @@ export const DelegationCheckError = ({
   const { t } = useTranslation();
 
   const getErrorMessage = (): string => {
-    switch (error?.data?.code) {
-      case 'AMUI-00001':
-        return t('systemuser_delegation_errors.01_rights_not_found_or_not_delegable');
-      case 'AMUI-00002':
-        return t('systemuser_delegation_errors.02_rights_failed_to_delegate');
-      case 'AMUI-00003':
-        return t('systemuser_delegation_errors.03_systemuser_failed_to_create');
-      case 'AMUI-00004':
-        return t('systemuser_delegation_errors.04_system_user_already_exists');
-      case 'AMUI-00011':
-        return t('systemuser_delegation_errors.11_system_not_found');
-      case 'AMUI-00014':
-        return t('systemuser_delegation_errors.14_unable_to_do_delegation_check');
-      case 'AMUI-00016':
-        return t('systemuser_delegation_errors.16_delegation_right_missing_role_access');
-      case 'AMUI-00018':
-        return t('systemuser_delegation_errors.18_delegation_right_missing_delegation_access');
-      case 'AMUI-00019':
-        return t('systemuser_delegation_errors.19_delegation_right_missing_srr_right_access');
-      case 'AMUI-00020':
-        return t(
-          'systemuser_delegation_errors.20_delegation_right_insufficient_authentication_level',
-        );
-      case 'AMUI-00051':
-        return t('systemuser_delegation_errors.51_accesspackage_delegation_failed');
-      case 'AMUI-00053':
-        return t(
-          'systemuser_delegation_errors.53_accesspackage_delegation_missing_required_access',
-        );
-      case 'AMUI-00055':
-        return t('systemuser_delegation_errors.55_accesspackage_failed_to_get_delegated_packages');
-      case 'AMUI-00057':
-        return t('systemuser_delegation_errors.57_systemuser_failed_to_delete_accesspackage');
-
-      default:
-        return t(defaultError);
-    }
+    return t(mapErrorCodeToErrorMessage(error?.data.code)) || t(defaultError);
   };
 
   return (
--- src/features/amUI/systemUser/components/SystemUserRequestLoadError/SystemUserRequestLoadError.tsx
+++ src/features/amUI/systemUser/components/SystemUserRequestLoadError/SystemUserRequestLoadError.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ProblemDetail } from '../../types';
 import { DsAlert } from '@altinn/altinn-components';
 import { useTranslation } from 'react-i18next';
+import { mapErrorCodeToErrorMessage } from '../../errorHandling';
 
 interface SystemUserRequestLoadErrorProps {
   error?: ProblemDetail;
@@ -9,11 +10,14 @@ interface SystemUserRequestLoadErrorProps {
 
 export const SystemUserRequestLoadError = ({ error }: SystemUserRequestLoadErrorProps) => {
   const { t } = useTranslation();
-  let errorText = 'systemuser_request.load_request_error';
-  if (error?.code === 'AMUI-00010' || error?.status === 404) {
+
+  let errorText = mapErrorCodeToErrorMessage(error?.code);
+  if (!errorText && error?.status === 404) {
     errorText = 'systemuser_request.load_request_error_notfound';
-  } else if (error?.code === 'AMUI-00066' || error?.status === 403) {
+  } else if (!errorText && error?.status === 403) {
     errorText = 'systemuser_request.load_request_missing_permissions';
+  } else if (!errorText) {
+    errorText = 'systemuser_request.load_request_error';
   }
 
   return <DsAlert data-color='danger'>{t(errorText)}</DsAlert>;
