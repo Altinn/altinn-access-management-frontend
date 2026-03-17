a7616584196cd58fe315fc9bd3314ab9563dd904
Sondre Wittek <sondrewittek@gmail.com>
2026-03-17 09:39:16 +0100

    Add instance delegation admin check and UI (#2042)
    * Add instance delegation admin check and UI
    
    * Refactor error handling in InstanceDetailPageContent to remove alert and use paragraph for error message
    
    * Fix loading state for ReporteePageHeading to include admin check
    
    * Refactor PageDivider component to support additional class names
    
    * active loading state return skeleton
    
    * Refactor InstanceDetailPageContent to enhance error handling and improve admin checks
    
    * qa
    
    * imports fix
    
    * qa

---
+++ src/features/amUI/InstanceDetailPage/InstanceDetailHeader.tsx
@@ -0,0 +1,54 @@
+import { DsHeading, DsParagraph, Icon, formatDisplayName } from '@altinn/altinn-components';
+import { useTranslation } from 'react-i18next';
+
+import { PartyType } from '@/rtk/features/userInfoApi';
+import type { ServiceResource } from '@/rtk/features/singleRights/singleRightsApi';
+
+import classes from './InstanceDetailHeader.module.css';
+
+interface InstanceDetailHeaderProps {
+  resource: ServiceResource;
+  resourceId: string;
+  providerLogoUrl?: string;
+  fromPartyName?: string;
+  fromPartyTypeName?: PartyType;
+}
+
+export const InstanceDetailHeader = ({
+  resource,
+  resourceId,
+  providerLogoUrl,
+  fromPartyName,
+  fromPartyTypeName,
+}: InstanceDetailHeaderProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classes.infoHeading}>
+      <DsHeading
+        level={1}
+        data-size='sm'
+      >
+        {resource.title ?? resourceId}
+      </DsHeading>
+      <div className={classes.resourceOwner}>
+        <Icon
+          iconUrl={providerLogoUrl ?? resource.resourceOwnerLogoUrl}
+          size='sm'
+        />
+        <DsParagraph data-size='sm'>
+          {resource.resourceOwnerName}{' '}
+          <span className={classes.providerName}>
+            {t('instance_detail_page.provider_name', {
+              name: formatDisplayName({
+                fullName: fromPartyName ?? '',
+                type: fromPartyTypeName === PartyType.Person ? 'person' : 'company',
+              }),
+            })}
+          </span>
+        </DsParagraph>
+      </div>
+      {resource.description && <DsParagraph data-size='sm'>{resource.description}</DsParagraph>}
+    </div>
+  );
+};
