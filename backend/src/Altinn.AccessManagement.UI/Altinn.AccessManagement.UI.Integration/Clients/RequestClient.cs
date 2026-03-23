8414a0c053d7aea779cb90145ffde1c9767fa19a
Martin Gunnerud <mgunnerud@gmail.com>
2026-03-23 20:14:59 +0100

    remove type filter (for now)

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/RequestClient.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/RequestClient.cs
@@ -60,10 +60,12 @@ namespace Altinn.AccessManagement.UI.Integration.Clients
                 }
 
                 var statusParams = status?.Select(s => new KeyValuePair<string, string>("status", s.ToString())) ?? [];
+                /*
                 if (!string.IsNullOrEmpty(type))
                 {
                     queryParams.Add("type", type);
                 }
+                */
                 
                 string endpointUrl = QueryHelpers.AddQueryString("enduser/request/sent", queryParams.Concat(statusParams));
                 string token = JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext, _platformSettings.JwtCookieName);
@@ -94,10 +96,12 @@ namespace Altinn.AccessManagement.UI.Integration.Clients
                 }
 
                 var statusParams = status?.Select(s => new KeyValuePair<string, string>("status", s.ToString())) ?? [];
+                /*
                 if (!string.IsNullOrEmpty(type))
                 {
                     queryParams.Add("type", type);
                 }
+                */
                 
                 string endpointUrl = QueryHelpers.AddQueryString("enduser/request/received", queryParams.Concat(statusParams));
                 string token = JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext, _platformSettings.JwtCookieName);
