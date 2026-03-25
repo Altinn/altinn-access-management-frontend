00eff810070a7f72cca4c1701fda1a9a284965cb
Sondre Wittek <sondrewittek@gmail.com>
2026-03-25 13:49:36 +0100

    Add query parameters for sent and received request count endpoints

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/RequestClient.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Integration/Clients/RequestClient.cs
@@ -227,7 +227,7 @@ namespace Altinn.AccessManagement.UI.Integration.Clients
         {
             try
             {
-                string endpointUrl = $"enduser/request/sent/count?party={party}";
+                string endpointUrl = $"enduser/request/sent/count?party={party}&to={to}";
                 string token = JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext, _platformSettings.JwtCookieName);
 
                 var httpResponse = await _client.GetAsync(token, endpointUrl);
@@ -245,7 +245,7 @@ namespace Altinn.AccessManagement.UI.Integration.Clients
         {
             try
             {
-                string endpointUrl = $"enduser/request/received/count?party={party}";
+                string endpointUrl = $"enduser/request/received/count?party={party}&from={from}";
                 string token = JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext, _platformSettings.JwtCookieName);
 
                 var httpResponse = await _client.GetAsync(token, endpointUrl);
