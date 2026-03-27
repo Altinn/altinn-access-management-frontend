a83a0a7b1bf578dadced78bceb765e4d0d8899d7
Sondre Wittek <sondrewittek@gmail.com>
2026-03-27 10:29:33 +0100

    Force "nb_no" on fetching package metadata

--- src/rtk/features/accessPackageApi.ts
+++ src/rtk/features/accessPackageApi.ts
@@ -76,7 +76,12 @@ export const accessPackageApi = createApi({
   endpoints: (builder) => ({
     search: builder.query<
       AccessArea[],
-      { searchString: string; language: string; typeName?: string }
+      {
+        searchString: string;
+        // Temporarily disable refetch on language change until backend has a fix for wrong translations of package names and descriptions.
+        // language: string;
+        typeName?: string;
+      }
     >({
       query: ({ searchString, typeName }) => {
         const typeNameParam = typeName ? `&typeName=${typeName}` : '';
@@ -95,7 +100,14 @@ export const accessPackageApi = createApi({
     }),
     getPackagePermissionDetails: builder.query<
       AccessPackage,
-      { from?: string; to?: string; party?: string; packageId: string; language: string }
+      {
+        from?: string;
+        to?: string;
+        party?: string;
+        packageId: string;
+        // Temporarily disable refetch on language change until backend has a fix for wrong translations of package names and descriptions.
+        // language: string
+      }
     >({
       query: ({ from, to, party = getCookie('AltinnPartyUuid'), packageId }) => {
         return `permission/${packageId}?from=${from ?? ''}&to=${to ?? ''}&party=${party}`;
