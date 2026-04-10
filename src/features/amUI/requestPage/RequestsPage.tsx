25d89eacc6dd1dc88c5de6f029b4b0037139365c
Alexandra Vedeler <arnex04@gmail.com>
2026-04-10 09:30:14 +0200

    Use same counter as sidebar on received tab (#2106)
    * Use same counter as sidebar on received tab
    
    * Disable page when user is not admin
    
    ---------
    
    Co-authored-by: Vedeler <acn-avede@ai-dev.no>

--- src/features/amUI/requestPage/RequestsPage.tsx
+++ src/features/amUI/requestPage/RequestsPage.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Badge, BadgeVariant, Color, DsTabs, formatDisplayName } from '@altinn/altinn-components';
+import {
+  Badge,
+  BadgeVariant,
+  Color,
+  DsAlert,
+  DsTabs,
+  formatDisplayName,
+} from '@altinn/altinn-components';
 import { PageWrapper } from '@/components';
 import { PageLayoutWrapper } from '../common/PageLayoutWrapper';
 import {
@@ -18,6 +25,7 @@ import classes from './RequestPage.module.css';
 import { SentRequestsTabPanel } from './SentRequestsTabPanel';
 import { PartyRepresentationProvider } from '../common/PartyRepresentationContext/PartyRepresentationContext';
 import { getCookie } from '@/resources/Cookie/CookieMethods';
+import { useSidebarRequestCount } from '@/resources/hooks/useSidebarRequestCount';
 
 const selectedTabProps = {
   'data-size': 'sm',
@@ -40,7 +48,7 @@ export const RequestPage = () => {
   useDocumentTitle(t('request_page.page_title'));
 
   const { data: reportee, isLoading: isLoadingReportee } = useGetReporteeQuery();
-  const { data: isAdmin } = useGetIsAdminQuery();
+  const { data: isAdmin, isLoading: isLoadingAdmin } = useGetIsAdminQuery();
   const {
     pendingRequests,
     isLoadingSentRequests,
@@ -50,11 +58,16 @@ export const RequestPage = () => {
   } = useRequests();
 
   const partyUuid = getCookie('AltinnPartyUuid');
-  const { data: sentRequestCount, isLoading: isLoadingSentRequestCount } =
-    useGetSentRequestsCountQuery(
-      { party: partyUuid || '', status: ['Pending'] },
-      { skip: !partyUuid || !isAdmin || !enableRequestSingleRight() },
-    );
+  const { data: sentRequestCount } = useGetSentRequestsCountQuery(
+    { party: partyUuid || '', status: ['Pending'] },
+    { skip: !partyUuid || !isAdmin || !enableRequestSingleRight() },
+  );
+  const { requestsBadgeCount: receivedRequestCount } = useSidebarRequestCount({
+    displayRequestsPage: true,
+    isAdmin,
+    reportee,
+    isLoadingPermissions: isLoadingAdmin,
+  });
 
   const getBadgeProps = (tabValue: string) =>
     selectedTab === tabValue ? selectedTabProps : unselectedTabProps;
@@ -64,74 +77,80 @@ export const RequestPage = () => {
     type: reportee?.type === 'Person' ? 'person' : 'company',
   });
 
-  const receivedRequestsCount = pendingRequests ? pendingRequests.received.length : 0;
+  const receivedRequestsCount = receivedRequestCount ?? 0;
   const resolvedSentRequestCount = sentRequestCount ?? 0;
 
   return (
     <PageWrapper>
       <PageLayoutWrapper>
-        <Breadcrumbs items={['root', 'requests']} />
-        <PartyRepresentationProvider
-          fromPartyUuid={getCookie('AltinnPartyUuid')}
-          actingPartyUuid={getCookie('AltinnPartyUuid')}
-          isLoading={isLoadingReportee}
-        >
-          <ReporteePageHeading
-            title={t('request_page.heading', { name })}
-            reportee={reportee}
-            isLoading={isLoadingReportee}
-          />
-          <DsTabs
-            value={selectedTab}
-            onChange={setSelectedTab}
-            data-size='sm'
-          >
-            <DsTabs.List className={classes.requestPageTabs}>
-              <DsTabs.Tab
-                value={INCOMING_REQUESTS_TAB}
-                className={classes.requestTab}
+        {!isAdmin && !isLoadingAdmin ? (
+          <DsAlert data-color='warning'>{t('common.no_access_to_page')}</DsAlert>
+        ) : (
+          <>
+            <Breadcrumbs items={['root', 'requests']} />
+            <PartyRepresentationProvider
+              fromPartyUuid={getCookie('AltinnPartyUuid')}
+              actingPartyUuid={getCookie('AltinnPartyUuid')}
+              isLoading={isLoadingReportee}
+            >
+              <ReporteePageHeading
+                title={t('request_page.heading', { name })}
+                reportee={reportee}
+                isLoading={isLoadingReportee}
+              />
+              <DsTabs
+                value={selectedTab}
+                onChange={setSelectedTab}
+                data-size='sm'
               >
-                {!!receivedRequestsCount && (
-                  <Badge
-                    {...getBadgeProps(INCOMING_REQUESTS_TAB)}
-                    label={receivedRequestsCount}
-                  />
-                )}
-                {t('request_page.incoming_requests')}
-              </DsTabs.Tab>
-              <DsTabs.Tab
-                value={SENT_REQUESTS_TAB}
-                className={classes.requestTab}
-              >
-                <Badge
-                  {...getBadgeProps(SENT_REQUESTS_TAB)}
-                  label={String(resolvedSentRequestCount)}
-                />
-                {t('request_page.sent_requests')}
-              </DsTabs.Tab>
-            </DsTabs.List>
-            <DsTabs.Panel value={INCOMING_REQUESTS_TAB}>
-              <RequestsTabPanel
-                count={receivedRequestsCount}
-                isLoading={isLoadingReceivedRequests}
-                isError={isReceivedRequestsError}
-                emptyMessageKey='request_page.no_received_requests'
-              >
-                <PendingRequests pendingRequests={pendingRequests.received} />
-              </RequestsTabPanel>
-            </DsTabs.Panel>
-            <DsTabs.Panel value={SENT_REQUESTS_TAB}>
-              <RequestsTabPanel
-                count={sentRequestCount ?? 0}
-                isLoading={isLoadingSentRequests}
-                isError={isSentRequestsError}
-                emptyMessageKey='request_page.no_sent_requests'
-              >
-                <SentRequestsTabPanel pendingRequests={pendingRequests.sent} />
-              </RequestsTabPanel>
-            </DsTabs.Panel>
-          </DsTabs>
-        </PartyRepresentationProvider>
+                <DsTabs.List className={classes.requestPageTabs}>
+                  <DsTabs.Tab
+                    value={INCOMING_REQUESTS_TAB}
+                    className={classes.requestTab}
+                  >
+                    {!!receivedRequestsCount && (
+                      <Badge
+                        {...getBadgeProps(INCOMING_REQUESTS_TAB)}
+                        label={receivedRequestsCount}
+                      />
+                    )}
+                    {t('request_page.incoming_requests')}
+                  </DsTabs.Tab>
+                  <DsTabs.Tab
+                    value={SENT_REQUESTS_TAB}
+                    className={classes.requestTab}
+                  >
+                    <Badge
+                      {...getBadgeProps(SENT_REQUESTS_TAB)}
+                      label={String(resolvedSentRequestCount)}
+                    />
+                    {t('request_page.sent_requests')}
+                  </DsTabs.Tab>
+                </DsTabs.List>
+                <DsTabs.Panel value={INCOMING_REQUESTS_TAB}>
+                  <RequestsTabPanel
+                    count={receivedRequestsCount}
+                    isLoading={isLoadingReceivedRequests}
+                    isError={isReceivedRequestsError}
+                    emptyMessageKey='request_page.no_received_requests'
+                  >
+                    <PendingRequests pendingRequests={pendingRequests.received} />
+                  </RequestsTabPanel>
+                </DsTabs.Panel>
+                <DsTabs.Panel value={SENT_REQUESTS_TAB}>
+                  <RequestsTabPanel
+                    count={sentRequestCount ?? 0}
+                    isLoading={isLoadingSentRequests}
+                    isError={isSentRequestsError}
+                    emptyMessageKey='request_page.no_sent_requests'
+                  >
+                    <SentRequestsTabPanel pendingRequests={pendingRequests.sent} />
+                  </RequestsTabPanel>
+                </DsTabs.Panel>
+              </DsTabs>
+            </PartyRepresentationProvider>
+          </>
+        )}
       </PageLayoutWrapper>
     </PageWrapper>
   );
