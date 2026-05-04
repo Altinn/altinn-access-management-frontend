56345d898c21f0ac9aa5ad9395e2125745bc22ef
Vedeler <acn-avede@ai-dev.no>
2026-05-04 12:17:35 +0200

    Tweak language on error page

--- src/sites/ErrorPage/amUi/contents/ReporteeChangeError.tsx
+++ src/sites/ErrorPage/amUi/contents/ReporteeChangeError.tsx
@@ -15,12 +15,16 @@ export const ReporteeChangeError = () => {
         level={1}
         data-size='sm'
       >
-        {t('reportee_change_error.header')}
+        {t('error_page.reportee_change_error.header')}
       </DsHeading>
-      <DsParagraph>{t('reportee_change_error.description')}</DsParagraph>
-      <DsParagraph>{t('reportee_change_error.if_persistent_contact_service')}</DsParagraph>
+      <DsParagraph>{t('error_page.reportee_change_error.description')}</DsParagraph>
       <DsParagraph>
-        <DsLink href={getAltinnStartPageUrl()}>{t('reportee_change_error.go_to_altinn')}</DsLink>
+        {t('error_page.reportee_change_error.if_persistent_contact_service')}
+      </DsParagraph>
+      <DsParagraph>
+        <DsLink href={getAltinnStartPageUrl()}>
+          {t('error_page.reportee_change_error.go_to_altinn')}
+        </DsLink>
       </DsParagraph>
     </div>
   );
