4985d0f84e05921ece06e474443a58206556af9a
Vedeler <acn-avede@ai-dev.no>
2026-05-15 15:01:47 +0200

    Added info box to poa overview for access packages

--- src/features/amUI/poaOverview/AccessPackagePermissions.tsx
+++ src/features/amUI/poaOverview/AccessPackagePermissions.tsx
@@ -4,14 +4,44 @@ import { useTranslation } from 'react-i18next';
 import { AccessPackageList } from '../common/AccessPackageList/AccessPackageList';
 import { Link } from 'react-router';
 import { DebouncedSearchField } from '../common/DebouncedSearchField/DebouncedSearchField';
+import { DsLink, DsParagraph, DsPopover } from '@altinn/altinn-components';
+import { QuestionmarkCircleIcon } from '@navikt/aksel-icons';
+import classes from './AccessPackagePermissions.module.css';
 
 export const AccessPackagePermissions = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const [debouncedSearchString, setDebouncedSearchString] = useState('');
 
   return (
     <>
+      <DsParagraph className={classes.description}>
+        {t('poa_overview_page.packages_tab.description')}
+        <span>
+          <DsPopover.TriggerContext>
+            <DsPopover.Trigger
+              icon
+              variant='tertiary'
+              data-size={'xs'}
+            >
+              <QuestionmarkCircleIcon
+                aria-label={t('poa_overview_page.packages_tab.info_box_icon_label')}
+              />
+            </DsPopover.Trigger>
+            <DsPopover className={classes.popover}>
+              <div className={classes.infoBox}>
+                <DsParagraph>{t('poa_overview_page.packages_tab.info_box_p1')}</DsParagraph>
+                <DsLink
+                  href={getHelpPagesUrl(i18n.language)}
+                  target='_blank'
+                >
+                  {t('poa_overview_page.packages_tab.info_box_link')}
+                </DsLink>
+              </div>
+            </DsPopover>
+          </DsPopover.TriggerContext>
+        </span>
+      </DsParagraph>
       <DebouncedSearchField
         placeholder={t('access_packages.search_label')}
         setDebouncedSearchString={setDebouncedSearchString}
@@ -34,4 +64,15 @@ export const AccessPackagePermissions = () => {
       />
     </>
   );
+};
+
+const getHelpPagesUrl = (languageKey: string) => {
+  switch (languageKey) {
+    case 'en':
+      return `https://info.altinn.no/en/news/altinn-rollene-erstattes-av-tilgangspakker--slik-handterer-du-overgangen/`;
+    case 'no_nn':
+      return `https://info.altinn.no/nn/nyheiter/altinn-rollene-blir-erstatta-av-tilgangspakker--sann-handterer-du-overgangen/`;
+    default:
+      return `https://info.altinn.no/nyheter/altinn-rollene-erstattes-av-tilgangspakker--slik-handterer-du-overgangen/`;
+  }
 };
