73c19af7d2f35bc3b21cf2a2a7f299b39deff71f
Martin Gunnerud <mgunnerud@gmail.com>
2026-05-18 15:47:02 +0200

    remove unused import

--- src/features/amUI/common/RequestPageLayout/RequestPageLayout.tsx
+++ src/features/amUI/common/RequestPageLayout/RequestPageLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useSearchParams } from 'react-router';
 import cn from 'classnames';
