c4b028f392deb860f586d1ef0ad6209f07733df2
Sondre Wittek <sondrewittek@gmail.com>
2026-05-26 11:17:07 +0200

    hide A2 links conditionally in OldRolesAlert component

--- src/features/amUI/common/OldRolesAlert/OldRolesAlert.tsx
+++ src/features/amUI/common/OldRolesAlert/OldRolesAlert.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { DsAlert, DsParagraph, DsLink } from '@altinn/altinn-components';
 
 import { getRedirectToA2UsersListSectionUrl } from '@/resources/utils';
+import { hideA2Links } from '@/resources/utils/featureFlagUtils';
 
 import { usePartyRepresentation } from '../PartyRepresentationContext/PartyRepresentationContext';
 
@@ -19,15 +20,17 @@ export const OldRolesAlert = () => {
     <DsAlert data-color='info'>
       <div className={styles.container}>
         <DsParagraph>{t('a2Alerts.oldRolesContent')}</DsParagraph>
-        <DsLink
-          asChild
-          className={styles.link}
-        >
-          <Link to={url}>
-            {t('a2Alerts.oldRolesLinkText')}
-            <ExternalLinkIcon aria-hidden='true' />
-          </Link>
-        </DsLink>
+        {!hideA2Links() && (
+          <DsLink
+            asChild
+            className={styles.link}
+          >
+            <Link to={url}>
+              {t('a2Alerts.oldRolesLinkText')}
+              <ExternalLinkIcon aria-hidden='true' />
+            </Link>
+          </DsLink>
+        )}
       </div>
     </DsAlert>
   );
