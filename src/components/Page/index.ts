78fdc243cd448c167e0c85f8c38b4ea2c45f4e7d
Martin Gunnerud <mgunnerud@gmail.com>
2026-06-24 12:34:36 +0200

    delete components for Altinn 2 solution

--- src/components/Page/index.ts
+++
@@ -1,4 +0,0 @@
-export { Page } from './Page';
-export { PageHeader } from './PageHeader';
-export { PageContent } from './PageContent';
-export { type PageColor, type PageSize } from './Context';
--- src/dataObjects/CustomError/CustomError.ts
+++
@@ -1,4 +0,0 @@
-export interface CustomError {
-  message?: string;
-  statusCode?: string;
-}
