8f53e38b51909cc008f67883b62e01a0dd252267
Martin Gunnerud <mgunnerud@gmail.com>
2026-07-13 08:47:00 +0200

    fix: change size of poaDetails header + hide icon on small screens (#2375)
    * fix: change size of poaDetails header + hide icon on small screens
    
    * fix poaDetailsHeader skeleton styling

--- src/features/amUI/packagePoaDetailsPage/PackagePoaDetailsHeader.tsx
+++ src/features/amUI/packagePoaDetailsPage/PackagePoaDetailsHeader.tsx
@@ -3,6 +3,7 @@ import classes from './PackagePoaDetailsHeader.module.css';
 import { DsHeading, DsParagraph } from '@altinn/altinn-components';
 import { PackageIcon } from '@navikt/aksel-icons';
 import { PackagePoaDetailsHeaderSkeleton } from './PackagePoaDetailsHeaderSkeleton';
+import { useIsMobileOrSmaller } from '@/resources/utils/screensizeUtils';
 
 interface PackagePoaDetailsHeaderProps {
   packageName?: string;
@@ -17,22 +18,26 @@ export const PackagePoaDetailsHeader: React.FC<PackagePoaDetailsHeaderProps> = (
   isLoading = false,
   statusSection,
 }) => {
+  const isSmall = useIsMobileOrSmaller();
+
   return isLoading ? (
     <PackagePoaDetailsHeaderSkeleton />
   ) : (
     <>
-      <PackageIcon
-        className={classes.packageIcon}
-        aria-hidden='true'
-      />
-      <DsHeading
-        level={1}
-        data-size='lg'
-        className={classes.pageHeading}
-      >
-        {packageName}
-      </DsHeading>
-      {statusSection && <div className={classes.statusSection}>{statusSection}</div>}
+      <div className={classes.headingContainerText}>
+        <PackageIcon
+          className={classes.packageIcon}
+          aria-hidden='true'
+        />
+        <DsHeading
+          level={1}
+          data-size={isSmall ? '2xs' : 'sm'}
+          className={classes.pageHeading}
+        >
+          {packageName}
+        </DsHeading>
+      </div>
+      {statusSection}
       <DsParagraph
         variant='long'
         className={classes.pageDescription}
