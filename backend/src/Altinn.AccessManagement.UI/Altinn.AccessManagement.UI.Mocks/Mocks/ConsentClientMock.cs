5400748dccd30ddd8cae164c035e6cc023e8bd17
Martin Gunnerud <mgunnerud@gmail.com>
2026-07-27 11:17:09 +0200

    Bruk nytt endepunkt for å hente antall samtykke-forespørsler (#2103)
    * add backend code for consent count
    
    * get pending consent count instead of full list of consents
    
    * fixes
    
    * add negative test

--- backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Mocks/Mocks/ConsentClientMock.cs
+++ backend/src/Altinn.AccessManagement.UI/Altinn.AccessManagement.UI.Mocks/Mocks/ConsentClientMock.cs
@@ -153,5 +153,29 @@ namespace Altinn.AccessManagement.UI.Mocks.Mocks
 
             return Task.FromResult(new Result<bool>(true));
         }
+
+        public Task<Result<int>> GetConsentRequestCount(Guid partyId, ConsentRequestStatusType status, CancellationToken cancellationToken)
+        {
+            List<ConsentRequestDetails> consentList = [];
+
+            if (partyId != ORG_PARTY_ID && partyId != PERSON_PARTY_ID)
+            {
+                return Task.FromResult(new Result<int>(ConsentProblem.ConsentNotFound));
+            }
+
+            if (partyId == ORG_PARTY_ID)
+            {
+                consentList = Util.GetMockData<List<ConsentRequestDetails>>($"{dataFolder}/Consent/consents_org.json");
+            }
+            else if (partyId == PERSON_PARTY_ID)
+            {
+                consentList = Util.GetMockData<List<ConsentRequestDetails>>($"{dataFolder}/Consent/consents_person.json");
+            }
+
+            int count = consentList.Count(c => c.ConsentRequestEvents.Any(e =>
+                string.Equals(e.EventType, status.ToString(), StringComparison.OrdinalIgnoreCase)));
+
+            return Task.FromResult(new Result<int>(count));
+        }
     }
 }
