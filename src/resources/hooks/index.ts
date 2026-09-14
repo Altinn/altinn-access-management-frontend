25f6842cadee2985d0464bf6cfb18ec674e9a8f0
Martin Gunnerud <mgunnerud@gmail.com>
2026-09-14 08:38:24 +0200

    refactor: rename and move usePackagePermissionOverview to usePermissionOverview (#2501)
    * refactor: rename and move usePackagePermissionOverview to usePermissionOverview
    
    * forgot file

--- src/resources/hooks/index.ts
+++ src/resources/hooks/index.ts
@@ -2,5 +2,6 @@ export { useMediaQuery } from './useMediaQuery';
 export { useProviderLogoUrl } from './useProviderLogoUrl';
 export { useAccessPackageLookup } from './useAccessPackageLookup';
 export { useDebouncedValue } from './useDebouncedValue';
+export { usePermissionOverview } from './usePermissionOverview';
 export { useTabState } from './useTabState';
 export { useLanguageCode } from './useLanguageCode';
