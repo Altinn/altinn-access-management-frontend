f0604fdc3a22c715adfc38a152a62e5fc204a32a
Martin Gunnerud <mgunnerud@gmail.com>
2026-09-22 13:06:09 +0200

    consistent searchbar size + heading level

--- src/features/amUI/common/UserSearch/UserSearch.tsx
+++ src/features/amUI/common/UserSearch/UserSearch.tsx
@@ -117,7 +117,10 @@ export const UserSearch: React.FC<UserSearchProps> = ({
     <div className={classes.container}>
       <div className={classes.controls}>
         <div className={classes.searchAndFilters}>
-          <DsSearch className={classes.searchBar}>
+          <DsSearch
+            className={classes.searchBar}
+            data-size='sm'
+          >
             <DsSearch.Input
               id={restoreFocusFallbackId}
               aria-label={t('common.search')}
@@ -151,9 +154,9 @@ export const UserSearch: React.FC<UserSearchProps> = ({
             </DsParagraph>
           )}
           {isQuery && showIndirectList && (
-            <h3 className={classes.subHeader}>
+            <h2 className={classes.subHeader}>
               {directConnectionsHeading ?? t('advanced_user_search.direct_connections')}
-            </h3>
+            </h2>
           )}
           <UserSearchResults
             users={filteredDirectUsers}
@@ -181,9 +184,9 @@ export const UserSearch: React.FC<UserSearchProps> = ({
 
         {showIndirectList && (
           <>
-            <h3 className={classes.subHeader}>
+            <h2 className={classes.subHeader}>
               {indirectConnectionsHeading ?? t('advanced_user_search.indirect_connections')}
-            </h3>
+            </h2>
             <UserSearchResults
               users={filteredIndirectUsers}
               hasNextPage={!!hasNextIndirectPage}
